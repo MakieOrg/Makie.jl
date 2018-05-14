335685dab5cace3cbef19b4aca91ec11aa12eeaa
SimonDanisch <sdanisch@gmail.com>
2018-05-14 18:37:43 +0200

    refactor into abstractplots
    Former-commit-id: 2fcf936a04efd8cbfcf4bff48cc2420f8841ea63

--- src/basic_recipes.jl
+++ src/basic_recipes.jl
@@ -9,23 +9,6 @@ function default_theme(scene, ::Type{Contour})
     )
 end
 
-to_vector(x::AbstractVector, len, T) = convert(Vector{T}, x)
-to_vector(x::ClosedInterval, len, T) = linspace(T.(extrema(x))..., len)
-
-function resample(x::AbstractVector, len)
-    length(x) == len && return x
-    interpolated_getindex.((x,), linspace(0.0, 1.0, len))
-end
-
-function resampled_colors(attributes, levels)
-    cols = if haskey(attributes, :color)
-        c = get_attribute(attributes, :color)
-        repeated(c, levels)
-    else
-        c = get_attribute(attributes, :colormap)
-        resample(c, levels)
-    end
-end
 
 function contourlines(::Type{Contour}, contours, cols)
     result = Point2f0[]
@@ -39,6 +22,7 @@ function contourlines(::Type{Contour}, contours, cols)
     end
     result, colors
 end
+
 function contourlines(::Type{Contour3d}, contours, cols)
     result = Point3f0[]
     colors = RGBA{Float32}[]
@@ -152,15 +136,8 @@ function plot!(scene::Scenelike, ::Type{Poly}, attributes::Attributes, args...)
     )
     return plot!(scene, poly, rest)
 end
-# function poly(scene::makie, points::AbstractVector{Point2f0}, attributes::Dict)
-#     attributes[:positions] = points
-#     _poly(scene, attributes)
-# end
-# function poly(scene::makie, x::AbstractVector{<: Number}, y::AbstractVector{<: Number}, attributes::Dict)
-#     attributes[:x] = x
-#     attributes[:y] = y
-#     _poly(scene, attributes)
-# end
+
+
 function plot!(scene::Scenelike, ::Type{Poly}, attributes::Attributes, x::AbstractVector{T}) where T <: Union{Circle, Rectangle}
     position = map(node(:positions, x)) do rects
         map(rects) do rect
@@ -255,192 +232,6 @@ end
 
 
 
-function plot!(scene::Scenelike, attributes::Attributes, matrix::AbstractMatrix{<: AbstractFloat})
-    attributes, rest = merged_get!(:series, scene, attributes) do
-        Theme(
-            seriescolors = :Set1,
-            seriestype = :lines
-        )
-    end
-    A = node(:series, matrix)
-    sub = Combined{:Series}(scene, attributes, A)
-    colors = map_once(attributes[:seriescolors], A) do colors, A
-        cmap = to_colormap(colors)
-        if size(A, 2) > length(cmap)
-            warn("Colormap doesn't have enough distinctive values. Please consider using another value for seriescolors")
-            cmap = interpolated_getindex.((cmap,), linspace(0, 1, M))
-        end
-        cmap
-    end
-    plots = map_once(A, attributes[:seriestype]) do A, stype
-        empty!(sub.plots)
-        N, M = size(A)
-        map(1:M) do i
-            c = map(getindex, colors, Node(i))
-            attributes = Theme(color = c)
-            subsub = Combined{:LineScatter}(sub, attributes, A)
-            if stype in (:lines, :scatter_lines)
-                lines!(subsub, attributes, 1:N, A[:, i])
-            end
-            if stype in (:scatter, :scatter_lines)
-                scatter!(subsub, attributes, 1:N, A[:, i])
-            end
-            subsub
-        end
-    end
-    labels = get(attributes, :labels) do
-        map(i-> "y $i", 1:size(matrix, 2))
-    end
-    l = legend(scene, plots[], labels, rest)
-    plot!(scene, sub, rest)
-end
-
-
-
-
-arrow_head(::Type{<: Point{3}}) = Pyramid(Point3f0(0, 0, -0.5), 1f0, 1f0)
-arrow_head(::Type{<: Point{2}}) = '▲'
-
-scatterfun(::Type{<: Point{2}}) = scatter!
-scatterfun(::Type{<: Point{3}}) = meshscatter!
-
-function arrows(
-        scene, points::AbstractVector{T}, directions::AbstractVector{<: VecTypes};
-        kw_args...
-    ) where T <: VecTypes
-    attributes, rest = merged_get!(:arrows, scene, Attributes(kw_args)) do
-        color = :black
-        Theme(
-            arrowhead = Pyramid(Point3f0(0, 0, -0.5), 1f0, 1f0),
-            arrowtail = nothing,
-            linecolor = color,
-            arrowcolor = color,
-            linewidth = 1,
-            arrowsize = 0.3,
-            linestyle = nothing,
-            scale = Vec3f0(1),
-            normalize = false,
-            lengthscale = 1.0f0
-        )
-    end
-    points_n = node(:arrow_origins, points)
-    directions_n = node(:arrow_dir, directions)
-
-    arrows = Combined{:Arrows}(scene, attributes, points_n, directions_n)
-    headstart = map(points_n, directions_n, attributes[:lengthscale]) do points, directions, s
-        map(points, directions) do p1, dir
-            dir = attributes[:normalize][] ? StaticArrays.normalize(dir) : dir
-            p1 => p1 .+ (dir .* Float32(s))
-        end
-    end
-
-    ls = linesegments!(
-        arrows,
-        map(reinterpret, Signal(Point3f0), headstart),
-        color = arrows[:linecolor],
-        linewidth = arrows[:linewidth],
-        linestyle = arrows[:linestyle]
-    )
-    heads = map(x-> last.(x), headstart)
-    scatterfun(T)(
-        arrows,
-        heads, marker = arrows[:arrowhead],
-         markersize = attributes[:arrowsize],
-        color = attributes[:arrowcolor],
-        rotations = directions_n
-    )
-    plot!(scene, arrows, rest)
-end
-
-
-function wireframe(scene::Scenelike, x::AbstractVector, y::AbstractVector, z::AbstractMatrix, attributes::Dict)
-    wireframe(ngrid(x, y)..., z, attributes)
-end
-
-function wireframe!(scene::Scenelike, x::AbstractMatrix, y::AbstractMatrix, z::AbstractMatrix, attributes::Dict)
-    if (length(x) != length(y)) || (length(y) != length(z))
-        error("x, y and z must have the same length. Found: $(length(x)), $(length(y)), $(length(z))")
-    end
-    points = lift_node(to_node(x), to_node(y), to_node(z)) do x, y, z
-        Point3f0.(vec(x), vec(y), vec(z))
-    end
-    NF = (length(z) * 4) - ((size(z, 1) + size(z, 2)) * 2)
-    faces = Vector{Int}(NF)
-    idx = (i, j) -> sub2ind(size(z), i, j)
-    li = 1
-    for i = 1:size(z, 1), j = 1:size(z, 2)
-        if i < size(z, 1)
-            faces[li] = idx(i, j);
-            faces[li + 1] = idx(i + 1, j)
-            li += 2
-        end
-        if j < size(z, 2)
-            faces[li] = idx(i, j)
-            faces[li + 1] = idx(i, j + 1)
-            li += 2
-        end
-    end
-    linesegment!(scene, view(points, faces), attributes)
-end
-
-
-function wireframe(scene::Scenelike, mesh, attributes::Dict)
-    mesh = to_node(mesh, x-> to_mesh(scene, x))
-    points = lift_node(mesh) do g
-        decompose(Point3f0, g) # get the point representation of the geometry
-    end
-    indices = lift_node(mesh) do g
-        idx = decompose(Face{2, GLIndex}, g) # get the point representation of the geometry
-    end
-    linesegment(scene, view(points, indices), attributes)
-end
-
-
-function sphere_streamline(linebuffer, ∇ˢf, pt, h = 0.01f0, n = 5)
-    push!(linebuffer, pt)
-    df = normalize(∇ˢf(pt[1], pt[2], pt[3]))
-    push!(linebuffer, normalize(pt .+ h*df))
-    for k=2:n
-        cur_pt = last(linebuffer)
-        push!(linebuffer, cur_pt)
-        df = normalize(∇ˢf(cur_pt...))
-        push!(linebuffer, normalize(cur_pt .+ h*df))
-    end
-    return
-end
-
-
-function streamlines!(
-        scene::Scenelike,
-        origins::AbstractVector{T},
-        directions;
-        kw_args...
-    ) where T
-    attributes, rest = merged_get!(:streamlines, scene, kw_args) do
-        Theme(
-            h = 0.01f0,
-            n = 5,
-            color = :black,
-            linewidth = 1
-        )
-    end
-    dirs_n, pts_n = node(:dirs, directions), node(:origins, origins)
-    linebuffer = T[]
-    sub = Combined{:Streamlines}(
-        scene,
-        attributes,
-        pts_n, dirs_n
-    )
-    lines = map(dirs_n, pts_n, sub[:h], sub[:n]) do ∇ˢf, origins, h, n
-        empty!(linebuffer)
-        for point in origins
-            sphere_streamline(linebuffer, ∇ˢf, point, h, n)
-        end
-        linebuffer
-    end
-    linesegments!(sub, lines, color = sub[:color], linewidth = sub[:linewidth])
-    plot!(scene, sub, rest)
-end
 
 function mergekeys(keys::NTuple{N, Symbol}, target::Attributes, source::Attributes) where N
     result = copy(target)
@@ -448,43 +239,4 @@ function mergekeys(keys::NTuple{N, Symbol}, target::Attributes, source::Attribut
         get!(result, key, source[key])
     end
     result
-end
-
-
-function volumeslices(scene, x, y, z, volume; kw_args...)
-    attributes, rest = merged_get!(:volumeslices, scene, kw_args) do
-        Theme(
-            colormap = theme(scene, :colormap),
-            colorrange = nothing,
-            alpha = 0.1,
-            contour = Attributes(),
-            heatmap = Attributes(),
-        )
-    end
-    xyz_volume = convert_arguments(Contour, x, y, z, volume)
-    x, y, z, volume = node.((:x, :y, :z, :volume), xyz_volume)
-    replace_nothing!(attributes, :colorrange) do
-        map(extrema, volume)
-    end
-    vs = Combined{:VolumeSlices}(scene, attributes, x, y, z, volume)
-    keys = (:colormap, :alpha, :colorrange)
-    cattributes = mergekeys(keys, attributes[:contour][], attributes)
-    plot!(vs, Contour, cattributes, x, y, z, volume)
-    planes = (:xy, :xz, :yz)
-    hattributes = mergekeys(keys, attributes[:heatmap][], attributes)
-    sliders = map(zip(planes, (x, y, z))) do plane_r
-        plane, r = plane_r
-        idx = node(plane, Signal(1))
-        attributes[plane] = idx
-        hmap = heatmap!(vs, hattributes, x, y, zeros(length(x[]), length(y[])))
-        foreach(idx) do i
-            transform!(hmap, (plane, r[][i]))
-            indices = ntuple(Val{3}) do j
-                planes[j] == plane ? i : (:)
-            end
-            hmap[3][] = view(volume[], indices...)
-        end
-        idx
-    end
-    plot!(scene, vs, rest)
 end
