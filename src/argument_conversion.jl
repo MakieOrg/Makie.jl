bdc69f34654fa6c419623b0df7e2b80028fe9626
Anthony Wang <anthony81212@gmail.com>
2018-05-22 22:27:32 +0200

    Documenting the argument conversion functions in argument_conversion.jl
    Former-commit-id: e01b10da95c3f24e96dc7b2f539766c128b8e030

--- src/argument_conversion.jl
+++ src/argument_conversion.jl
@@ -6,11 +6,55 @@ function convert_arguments(P::Type, args::Vararg{Signal, N}) where N
         map(x-> x[i], args_c)
     end
 end
-convert_arguments(P, y::RealVector) = convert_arguments(0 .. length(y), y)
+
+"""
+    convert_arguments(P, y)::(Vector)
+
+Takes vector y and generates a range from 1 to the length of y, for plotting on
+an arbitrary x axis.
+P is the plot Type (it is optional).
+"""
+convert_arguments(P, y::RealVector) = convert_arguments(1 .. length(y), y)
+
+"""
+    convert_arguments(P, x, y)::(Vector)
+
+Takes vectors x and y and turns it into a vector of 2D points of the values
+from x and y.
+P is the plot Type (it is optional).
+"""
 convert_arguments(P, x::RealVector, y::RealVector) = (Point2f0.(x, y),)
+
+"""
+    convert_arguments(P, x, y, z)::(Vector)
+
+Takes vectors x, y, and z and turns it into a vector of 3D points of the values
+from x, y, and z.
+P is the plot Type (it is optional).
+"""
 convert_arguments(P, x::RealVector, y::RealVector, z::RealVector) = (Point3f0.(x, y, z),)
+
+"""
+    convert_arguments(x)::(String)
+
+Takes an input AbstractString x and converts it to a string.
+"""
 convert_arguments(::Type{Text}, x::AbstractString) = (String(x),)
+
+"""
+    convert_arguments(P, x)::(Vector)
+
+Accepts a vector x of the types in VecTypes.
+P is the plot Type (it is optional).
+"""
 convert_arguments(P, x::AbstractVector{<: VecTypes}) = (x,)
+
+"""
+    convert_arguments(P, x)::(Vector)
+
+Takes an input GeometryPrimitive x and decomposes it to points.
+P is the plot Type (it is optional).
+"""
 convert_arguments(P, x::GeometryPrimitive) = (decompose(Point, x),)
 
 
@@ -18,33 +62,99 @@ function convert_arguments(P, x::AbstractVector{Pair{Point{N, T}, Point{N, T}}})
     (reinterpret(Point{N, T}, x),)
 end
 
+"""
+    convert_arguments(P, x, y, z)::Tuple{Matrix, Matrix, Matrix}
+
+Takes 3 inputs of AbstractMatrix x, y, and z, converts them to Float32 and
+outputs them in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::AbstractMatrix, y::AbstractMatrix, z::AbstractMatrix)
     (Float32.(x), Float32.(y), Float32.(z))
 end
+
+"""
+    convert_arguments(P, x, y, z)::Tuple{Vector, Vector, Matrix}
+
+Takes 2 AbstractVector's x, y, and an AbstractMatrix z, and puts them in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::AbstractVector, y::AbstractVector, z::AbstractMatrix)
     (x, y, z)
 end
+
+"""
+    convert_arguments(P, x, y, z)::Tuple{ClosedInterval, ClosedInterval, Matrix}
+
+Takes 2 ClosedIntervals's x, y, and an AbstractMatrix z, and puts them in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::ClosedInterval, y::ClosedInterval, z::AbstractMatrix)
     (x, y, z)
 end
+
+"""
+    convert_arguments(P, x, y, z)::(ClosedInterval, ClosedInterval, Matrix)
+
+Takes 2 ClosedIntervals's x, y, and z, converts the intervals x and y into a range,
+and and puts everything in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::ClosedInterval, y::ClosedInterval, z)
     convert_arguments(P, to_range(x), to_range(y), z)
 end
+
+"""
+    convert_arguments(P, Matrix)::Tuple{ClosedInterval, ClosedInterval, Matrix}
+
+Takes a matrix, converts the dimesions n and m into closed intervals,
+and stores the closed intervals to n and m, plus the original matrix in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, data::AbstractMatrix)
     n, m = Float64.(size(data))
     (0.0 .. n, 0.0 .. m, data)
 end
 
+"""
+    convert_arguments(P, Matrix)::Tuple{ClosedInterval, ClosedInterval, Matrix}
+
+Takes an array of {T, 3}, converts the dimesions n, m and k into closed intervals,
+and stores the closed intervals to n, m and k, plus the original array in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, data::Array{T, 3}) where T
     n, m, k = Float64.(size(data))
     (0.0 .. n, 0.0 .. m, 0.0 .. k, data)
 end
+
+"""
+    convert_arguments(P, x, y, z, i)::(Vector, Vector, Vector, Matrix)
+
+Takes vectors x, y, and z and the matrix i, and puts everything in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::AbstractVector, y::AbstractVector, z::AbstractVector, i::AbstractArray{T, 3}) where T
     (x, y, z, i)
 end
+
+"""
+    convert_arguments(P, x, y, z, i)::(ClosedInterval, ClosedInterval, ClosedInterval, Matrix)
+
+Takes closed intervals x, y, and z and the matrix i, and puts everything in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::ClosedInterval, y::ClosedInterval, z::ClosedInterval, i::AbstractArray{T, 3}) where T
     (x, y, z, i)
 end
+
+"""
+    convert_arguments(P, x, y, z, f)::(Vector, Vector, Vector, Matrix)
+
+Takes vectors x, y, and z and the function f, evaluates the function on the volume
+spanned by x, y and z, and puts x, y, z and f(x,y,z) in a Tuple.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::AbstractVector, y::AbstractVector, z::AbstractVector, f::Function)
     _x, _y, _z = ntuple(Val{3}) do i
         A = (x, y, z)[i]
@@ -53,6 +163,14 @@ function convert_arguments(P, x::AbstractVector, y::AbstractVector, z::AbstractV
     (x, y, z, f.(_x, _y, _z))
 end
 
+
+"""
+    convert_arguments(P, x, y, f)::(Vector, Vector, Matrix)
+
+Takes vectors x and y and the function f, and applies f on the grid that x and y span.
+This is equivalent to f.(x, y').
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::AbstractVector{T1}, y::AbstractVector{T2}, f::Function) where {T1, T2}
     if !applicable(f, x[1], y[1])
         error("You need to pass a function with signature f(x::$T1, y::$T2). Found: $f")
@@ -63,12 +181,31 @@ function convert_arguments(P, x::AbstractVector{T1}, y::AbstractVector{T2}, f::F
     (x, y, z)
 end
 
+"""
+    convert_arguments(P, x)::(Vector)
+
+Takes an input HyperRectangle x and decomposes it to points.
+P is the plot Type (it is optional).
+"""
 function convert_arguments(P, x::Rect)
     # TODO fix the order of decompose
     convert_arguments(P, decompose(Point2f0, x)[[1, 2, 4, 3, 1]])
 end
 
+"""
+    convert_arguments(x)::(String)
+
+Takes an input Mesh x and stores it in a Tuple.
+"""
 convert_arguments(::Type{Mesh}, m::AbstractMesh) = (m,)
+
+"""
+    convert_arguments(T, x, y, z, indices)::Tuple{Type, Matrix, Vector}
+
+Takes an input mesh, RealVector's x, y and z, and an AbstractVector indices,
+and puts it in a Tuple with the Type, the 3D points of the values from x, y and z,
+and the indices.
+"""
 function convert_arguments(
         T::Type{Mesh},
         x::RealVector, y::RealVector, z::RealVector,
@@ -87,6 +224,12 @@ function convert_arguments(
     (m,)
 end
 
+"""
+    convert_arguments(MT, x, y, z)::Tuple{Type, Matrix}
+
+Takes an input mesh, RealVector's x, y and z, and puts them in a Tuple with
+the type and the 3D points of the values from x, y and z.
+"""
 function convert_arguments(
         MT::Type{Mesh},
         x::RealVector, y::RealVector, z::RealVector
@@ -100,6 +243,13 @@ function convert_arguments(
     faces = reinterpret(GLTriangle, UInt32[0:(length(xyz)-1);])
     convert_arguments(MT, xyz, faces)
 end
+
+"""
+    convert_arguments(MT, xy)::Tuple{Type, Matrix}
+
+Takes an input mesh, AbstractVector xy, and puts them in a Tuple with
+the type and the 3D points of the values from x, y and z = 0.0.
+"""
 function convert_arguments(MT::Type{Mesh}, xy::AbstractVector{<: VecTypes{2, T}}) where T
     convert_arguments(MT, Point3f0.(first.(xy), last.(xy), 0.0))
 end
