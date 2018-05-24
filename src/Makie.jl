4d7c2e2006b5dbbce6749690c9d9a8970d21dbc6
SimonDanisch <sdanisch@gmail.com>
2018-05-24 15:07:40 +0200

    further refactor
    Former-commit-id: 26dfa3fd70c12f44a7427abf00ddf6328f60805b

--- src/Makie.jl
+++ src/Makie.jl
@@ -1,6 +1,10 @@
 __precompile__(true)
 module Makie
 
+using AbstractPlotting
+importall AbstractPlotting
+using AbstractPlotting: @info, @log_performance, @warn, jl_finalizer
+
 using Reactive, GeometryTypes, Colors, StaticArrays
 
 using Colors, GeometryTypes, ColorVectorSpace
@@ -14,58 +18,26 @@ using IntervalSets
 using PlotUtils, Showoff
 
 using Base: RefValue
+
 import Base: push!, isopen
 
-include("logging.jl")
-include("types.jl")
-include("utils.jl")
-include("signals.jl")
-include("camera_math.jl")
+# functions we overload
 
+include("types.jl")
 include("scene.jl")
 
-include("basic_drawing.jl")
-include("basic_recipes.jl")
-include("layouting.jl")
+include("makie_recipes.jl")
 
 include("attribute_conversion.jl")
+include("argument_conversion.jl")
 
-include("events.jl")
 include("glbackend/glbackend.jl")
 include("cairo/cairo.jl")
 
-include("argument_conversion.jl")
 include("plot.jl")
-include("camera2d.jl")
-include("camera3d.jl")
-include("axis2d.jl")
-include("axis3d.jl")
-include("buffers.jl")
-include("legend.jl")
+include("theming.jl")
 include("output.jl")
-include("gui.jl")
 
-export Scene, Screen, plot!, CairoScreen, axis2d, RGBAf0
-export Combined, Theme, node, @extract
-export translated, translate!, transform!, scale!, rotate!, grid, Accum, Absolute
-export @key_str, convert_attribute, Attributes, colorlegend, Node
-
-# camera related
-export cam2d!, campixel!, cam3d!, update_cam!
-
-# picking
-export mouseover, onpick, pick
-
-# gui
-export slider, button, playbutton
-
-export (..) # reexport interval
-
-# attribute_conversion shortcuts
-export to_color, to_colormap, to_rotation, to_font, to_align, to_textsize
-
-# Raymarching algorithms
-export IsoValue, Absorption, MaximumIntensityProjection, AbsorptionRGBA, IndexedAbsorptionRGBA
-
+export Scene
 
 end
