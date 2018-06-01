1932c1bcdb7f73d5995aacbaa3de40859721a4d5
Anthony Wang <anthony81212@gmail.com>
2018-06-01 23:23:34 +0200

    Extend to_string to also handle strings as input
    Former-commit-id: 0fd078036b0b3f3a9d9dd29aa32dd80c04b65788

--- src/documentation.jl
+++ src/documentation.jl
@@ -58,7 +58,7 @@ function to_string(func::Function)
 end
 
 to_string(Typ::Type{T}) where T <: AbstractPlot = to_string(to_func(Typ))
-
+to_string(s::String) = s
 
 # ==========================================================
 # Help functions
