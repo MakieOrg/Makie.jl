4ad1de2ef122d3edb71877d09af33bcec5f62e68
Anthony Wang <anthony81212@gmail.com>
2018-06-03 15:26:45 +0200

    Add allowed types of meshes (WIP) -> figure out where to place this docstring
    Former-commit-id: c5309edab749df6292b174e2671614bc12d07b0b

--- src/documentation.jl
+++ src/documentation.jl
@@ -179,3 +179,17 @@ function help_attributes(io, Typ::Type{T}; extended = false) where T <: Makie.Ab
         println(io, "```")
     end
 end
+
+# ==========================================================
+"""
+Allowed types for meshes:
+
+Vector of indices
+e.g. [1, 2, 3,   3, 4, 2] where each triplet represent a triangle
+
+Array of faces
+e.g. Face(1,2,3)
+
+NTuples of faces
+e.g. ((1, 2, 3), (3, 4, 2))
+"""
