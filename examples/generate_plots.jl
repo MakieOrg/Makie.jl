124d60ced2b6e89f30e83b8d23490af7e74ae380
Anthony Wang <anthony81212@gmail.com>
2018-06-12 16:44:48 +0200

    Refactored generation of plots using generate_plots.jl instead of relying on Documenter
    Former-commit-id: 861b00ab9df53deaf91fbe5630ee57476b9813be

--- examples/generate_plots.jl
+++ examples/generate_plots.jl
@@ -4,29 +4,35 @@ cd(@__DIR__)
 using Makie, GLFW, GeometryTypes, Reactive, FileIO
 
 cd(Pkg.dir("Makie"))
-
-sort!(database, by = (x)-> x.groupid)
-index = start(database)
 isdir("docs/media") || mkdir("docs/media")
 
-while length(database) >= index
+sort!(database, by = (x)-> x.groupid)
+
+index = start(database)
+dblen = length(database)
+while dblen - 1 >= index
+    # use the unique_name of the database entry as filename
     uname = string(database[index].unique_name)
+    info("Working on index: $index, uname: $uname")
     str = sprint() do io
+        # declare global index so it can be modified by the function inside loop
         global index
         index = print_code(io, database, index; scope_start = "", scope_end = "")
     end
-    println(index)
+    # sandbox the string in a module
     tmpmod = eval(:(module $(gensym(uname)); end))
+    # eval the sandboxed module using include_string
     try
         result = eval(tmpmod, Expr(:call, :include_string, str, uname))
         if isa(result, String) && isfile(result)
-            println("it's a path!")
-            println(result)
+            info("it's a path! -- video")
+            info("path is: $result")
         elseif isa(result, AbstractPlotting.Scene)
-            println("it's a plot")
+            info("it's a plot")
             Makie.save("docs/media/$uname.png", result)
+            generate_thumbnail("docs/media/$uname.png"; sz = 200)
         else
-            warn("something went really badly with $index & $(typeof(result))")
+            warn("something went really badly with index $index & $(typeof(result))")
         end
     catch e
         Base.showerror(STDERR, e)
