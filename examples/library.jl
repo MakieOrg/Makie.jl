48c8f1cfc45cc48415b7cb341749aa3e90052e32
SimonDanisch <sdanisch@gmail.com>
2018-06-17 11:28:53 +0200

    improve comment
    Former-commit-id: 53a92041c85b38d65a66f9a465f55a2255e83981

--- examples/library.jl
+++ examples/library.jl
@@ -263,7 +263,9 @@ end
             1:10, 1:10, rand(10, 10) .* 10,
             rotations = normalize.(rand(Quaternionf0, 10*10)),
             markersize = 1,
-            marker = Makie.logo() # can also be an array of images for each point
+             # can also be an array of images for each point
+             # need to be the same size for best performance
+            marker = Makie.logo()
         )
     end
     @cell "Simple meshscatter" [meshscatter] begin
