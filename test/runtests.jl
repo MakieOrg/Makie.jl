c1aa9a9eafc655a945d9393a660fbbeeabcf31c1
SimonDanisch <sdanisch@gmail.com>
2018-06-18 18:22:10 +0200

    add DISPLAY env
    Former-commit-id: 379643763bcab8ad43f11aef9c1dd6801b0536aa

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,2 @@
-include("../examples/library.jl")
-cd(@__DIR__)
-
-open("global_form.jl", "w") do io
-    sort!(database, by = (x)-> x.groupid)
-    i = start(database)
-    while length(database) >= i
-        i = print_code(io, database, i, scope_start = "let\n")
-    end
-end
-include("global_form.jl")
+ENV["DISPLAY"] = ":0"
+include("../examples/generate_plots.jl")
