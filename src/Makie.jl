23f2c1c961c72fd5314f6e98131b13d8f8bf845c
SimonDanisch <sdanisch@gmail.com>
2018-06-20 13:37:31 +0200

    dont use fontconfig no more
    Former-commit-id: 607216dc31de3782d8700e73d6e8e9ca1a3862e7

--- src/Makie.jl
+++ src/Makie.jl
@@ -29,7 +29,7 @@ const Contours = ContoursTemp.Contour
 using Primes
 
 using Base.Iterators: repeated, drop
-using Fontconfig, FreeType, FreeTypeAbstraction, UnicodeFun
+using FreeType, FreeTypeAbstraction, UnicodeFun
 using PlotUtils, Showoff
 using Base: RefValue
 import Base: push!, isopen, show
@@ -63,7 +63,6 @@ include("glbackend/glbackend.jl")
 include("cairo/cairo.jl")
 include("output.jl")
 include("video_io.jl")
-
 
 # conversion infrastructure
 include("documentation.jl")
