bdf55d3a664bce5bed680f3b76d42803bccfa2bf
Unknown <louisponet@gmail.com>
2018-06-24 22:55:48 -0400

    Fixed rendering
    makes sense that one has to bind the actual programs....

--- src/glbackend/rendering.jl
+++ src/glbackend/rendering.jl
@@ -52,7 +52,7 @@ import .GLVisualize: GLVisualizeShader
 default_pipeline(fbo, program)=
     # Pipeline(:default, [default_renderpass(fbo, program)])
     # Pipeline(:default, [default_renderpass(fbo, program), postprocess_renderpass(fbo), final_renderpass(fbo)])
-    Pipeline(:default, [default_renderpass(fbo, program), postprocess_renderpass(fbo), fxaa_renderpass(fbo), final_renderpass(fbo)])
+    Pipeline(:default, [default_renderpass(fbo, program), postprocess_renderpass(fbo), fxaa_renderpass(fbo),final_renderpass(fbo)])
 
 #TODO shadercleanup: cleanup gl_convert GLVisualizeShader etc
 default_renderpass(fbo, program) = RenderPass(:default, program, fbo)
@@ -137,6 +137,7 @@ end
 
 function setup(rp::RenderPass{:postprocess})
     draw(rp.target, 3) #only the color part of the fbo
+    bind(rp.program)
     glDepthMask(GL_TRUE)
     glDisable(GL_DEPTH_TEST)
     glDisable(GL_BLEND)
@@ -158,6 +159,7 @@ end
 #maybe glviewport needs to be here
 function setup(rp::RenderPass{:fxaa})
     bind(rp.target)
+    bind(rp.program)
     draw(rp.target, 1) #copy back to original color
 end
 
@@ -172,6 +174,7 @@ end
 
 function setup(rp::RenderPass{:final})
     unbind(rp.target)
+    bind(rp.program)
     glClearColor(0, 0, 0, 0)
     glClear(GL_COLOR_BUFFER_BIT)
 end
