425aec00857f89d5cb54d0626d4aa233693483bc
Anthony Wang <anthony81212@gmail.com>
2018-07-25 18:02:08 +0200

    Add the "iris" example from RDatasets
    Former-commit-id: 003ea04ffffd66925995213795d9fefaa8c65065

--- examples/examples2d.jl
+++ examples/examples2d.jl
@@ -195,4 +195,26 @@
         end
         scatter!(scene, posis, markersize = 10)
     end
+
+    @cell "The famous iris example" [RDatasets, DataFrames, scatter, axis] begin
+        using DataFrames, RDatasets # do Pkg.add.(["DataFrames", "RDatasets"]) if you don't have these packages installed
+        iris = dataset("datasets", "iris")
+
+        x = iris[:SepalWidth]
+        y = iris[:SepalLength]
+
+        scene = Scene()
+        colors = [:red, :green, :blue]
+        i = 1 #color incrementer
+        for sp in unique(iris[:Species])
+            idx = iris[:Species] .== sp
+            sel = iris[idx, [:SepalWidth, :SepalLength]]
+            scatter!(scene, sel[:,1], sel[:,2], color = colors[i], limits = FRect(1.5, 4.0, 3.0, 4.0))
+            global i = i+1
+        end
+        scene
+        axis = scene[Axis] # get axis
+        axis[:names][:axisnames] = ("Sepal width", "Sepal length")
+        scene
+    end
 end
