4959b501a96583adbd71423d6fe84493445ce8eb
SimonDanisch <sdanisch@gmail.com>
2018-10-24 16:41:20 +0200

    add example for orthographic camera

--- examples/examples3d.jl
+++ examples/examples3d.jl
@@ -1,5 +1,30 @@
 
 @block SimonDanisch ["3d"] begin
+    @cell "Orthographic Camera" [meshscatter, cameracontrols, update_cam] begin
+        using GeometryTypes
+        x = Vec3f0(0); baselen = 0.2f0; dirlen = 1f0
+        # create an array of differently colored boxes in the direction of the 3 axes
+        rectangles = [
+            (FRect3D(Vec3f0(x), Vec3f0(dirlen, baselen, baselen)), RGBAf0(1,0,0,1)),
+            (FRect3D(Vec3f0(x), Vec3f0(baselen, dirlen, baselen)), RGBAf0(0,1,0,1)),
+            (FRect3D(Vec3f0(x), Vec3f0(baselen, baselen, dirlen)), RGBAf0(0,0,1,1))
+        ]
+        meshes = map(GLNormalMesh, rectangles)
+        scene = mesh(merge(meshes))
+        display(scene)
+        cam = Makie.cameracontrols(scene)
+        dir = scene.limits[].widths ./ 2.
+        dir_scaled = Vec3f0(
+            dir[1] * scene.transformation.scale[][1],
+            0.0,
+            dir[3] * scene.transformation.scale[][2],
+        )
+        cam.upvector[] = (0.0, 0.0, 1.0)
+        cam.lookat[] = scene.limits[].origin + dir_scaled
+        cam.eyeposition[] = (cam.lookat[][1], cam.lookat[][2] + 6.3, cam.lookat[][3])
+        cam.projectiontype[] = AbstractPlotting.Orthographic
+        update_cam!(scene, cam)
+    end
 
     @cell "Volume Function" [volume] begin
         volume(rand(32, 32, 32), algorithm = :mip)
