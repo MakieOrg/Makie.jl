9ada4011da31e081b4263e5cd45fe6b3d3473019
SimonDanisch <sdanisch@gmail.com>
2018-10-25 17:10:06 +0200

    add time series example
    Former-commit-id: 549bc566fe399722cbbd94ee73feed31b6004d0b

--- examples/interactive.jl
+++ examples/interactive.jl
@@ -67,4 +67,37 @@
         p = scatter(data, markersize = s1[end][:value])
         hbox(p, vbox(s1, s2))
     end
+
+    @cell "Time Series" [lines] begin
+
+        function makie_live(;
+                f0 = 1/2, fs = 100,
+                winsec = 4, hopsec = 1/60
+            )
+            nwin = round(Integer, winsec*fs)
+            nhop = round(Integer, hopsec*fs)
+            # do the loop
+            frame_start = -winsec
+            frame_time = collect((0:(nwin-1)) * (1/fs))
+            aframe = sin.(2*pi*f0.*(frame_start .+ frame_time))
+            scene = lines(frame_start .+ frame_time, aframe)
+            display(scene)
+            lineplot = scene[end]
+            fix = 0
+            i = 0
+            while isopen(scene) && i < 50 # dont loop longer than 50x for testing
+                aframe .= sin.(2*pi*f0.*(frame_start .+ frame_time))
+                # append!(aframe, randn(nhop)); deleteat!(aframe, 1:nhop)
+                lineplot[1] = frame_start .+ frame_time
+                lineplot[2] = aframe
+                AbstractPlotting.update_limits!(scene)
+                AbstractPlotting.update!(scene)
+                sleep(hopsec)
+                frame_start += hopsec
+            end # while loop
+            scene
+        end
+        makie_live()
+
+    end
 end
