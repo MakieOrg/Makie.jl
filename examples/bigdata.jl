17f0f09b2a00c8cdf655a0deabc41fd05e0dd756
SimonDanisch <sdanisch@gmail.com>
2018-11-05 14:59:27 +0100

    fix world climate example

--- examples/bigdata.jl
+++ examples/bigdata.jl
@@ -1,7 +1,6 @@
 @block SimonDanisch ["dataset_examples"] begin
     @cell "WorldClim visualization" [visualization, dataset, bigdata] begin
         using FileIO, GeometryTypes, Colors, GDAL
-
         #=
         This example requires the GDAL package, from https://github.com/JuliaGeo/GDAL.jl
         For more information about GDAL, see the official documentation at: https://gdal.org/
@@ -13,8 +12,10 @@
         # function to check if a file is a .tif file
         istiff(x) = endswith(x, ".tif")
 
+
         # set up 7zip
-        exe7z = is_windows() ? joinpath(JULIA_HOME, "7z.exe") : joinpath(JULIA_HOME, "7z")
+        exe7z = "7z"
+
         unzip(in, out) = run(`$exe7z x -y $in -o$out`)
 
         # function to read the raster data from the GeoTIFF
@@ -23,7 +24,7 @@
             band = GDAL.getrasterband(img, 1)
             xsize = GDAL.getrasterbandxsize(band)
             ysize = GDAL.getrasterbandysize(band)
-            data = Array{Float32}(xsize, ysize)
+            data = Array{Float32}(undef, xsize, ysize)
             GDAL.rasterio(band, GDAL.GF_Read, 0, 0, xsize, ysize, data, xsize, ysize, GDAL.GDT_Float32, 0, 0)
             data'
         end
@@ -32,6 +33,7 @@
         function load_dataset(name)
             # get the dataset from:
             # http://worldclim.org/version2
+
             """
             This is WorldClim 2.0 Beta version 1 (June 2016) downloaded from http://worldclim.org
             They represent average monthly climate data for 1970-2000.
@@ -60,25 +62,31 @@
 
         # plot the temperature as color map on the globe
         cmap = [:darkblue, :deepskyblue2, :deepskyblue, :gold, :tomato3, :red, :darkred]
-        scene = Makie.mesh(m, color = temperature[10], colorrange = (-50, 50), colormap = cmap, shading = true, show_axis = false)
-        temp_plot = scene[end];
 
         # function to scale precipitation to a suitable marker size
-        function to_msize(uv, water, xysize = 0.008, normalization = 908f0 * 4f0)
+        function watermap(uv, water, normalization = 908f0 * 4f0)
             markersize = map(uv) do uv
-                wh = size(water) .- 1
+                wsize = reverse(size(water))
+                wh = wsize .- 1
                 x, y = round.(Int, Tuple(uv) .* wh) .+ 1
-                val = water[x, y] / normalization
-                val = val < 0.0 ? 0f0 : val
-                Vec3f0(xysize, xysize, val)
+                val = water[size(water)[1] - (y - 1), x] / normalization
+                val < 0.0 ? -1f0 : val
             end
         end
 
+        scene = Scene(resolution = (800, 800))
+        scene = Makie.mesh!(m, color = temperature[10], colorrange = (-50, 50), colormap = cmap, shading = true, show_axis = false)
+        temp_plot = scene[end];
         # plot precipitation as vertical bars
+        watervals = watermap(uv, water[1])
+        extrema(watervals)
+        xyw = 0.01
         meshscatter!(scene,
             p, rotations = norms,
-            marker = Rect3D(Vec3f0(0), Vec3f0(1)), markersize = to_msize(uv, water[1]), raw = true,
-            color = (:skyblue2, 0.9)
+            marker = Rect3D(Vec3f0(0), Vec3f0(1)),
+            markersize = Vec3f0.(xyw, xyw, watervals), raw = true,
+            color = watervals, colormap = [(:black, 0.0), (:skyblue2, 0.6)],
+            shading = false
         )
         wplot = scene[end]
 
@@ -86,17 +94,21 @@
         eye_position, lookat, upvector = Vec3f0(0.5, 0.8, 2.5), Vec3f0(0), Vec3f0(0, 1, 0)
         update_cam!(scene, eye_position, lookat, upvector)
         scene
-
+        scene.center = false
         # save animation
-        r = record(scene, @outputfile(mp4), 0:(11*4)) do i
-            # Make simulation slower. TODO figure out how do this nicely with ffmpeg
-            if i % 4 == 0
-                i2 = (i ÷ 4) + 1
-                temp_plot[:color] = temperature[i2]
-                wplot[:markersize] = to_msize(uv, water[i2])
-            end
+        r = record(scene, "test.gif", 0:(11*4)) do i
+           # Make simulation slower. TODO figure out how do this nicely with ffmpeg
+           if i % 4 == 0
+               i2 = (i ÷ 4) + 1
+               temp_plot[:color] = temperature[i2]
+               watervals = watermap(uv, water[i2])
+               wplot[:color] = watervals
+               wplot[:markersize][] .= Vec3f0.(xyw, xyw, watervals)
+               # since we modify markersize inplace above, we need to notify the signal
+               AbstractPlotting.notify!(wplot[:markersize])
+           end
         end
-        return r
+        r
     end
-    
+
 end
