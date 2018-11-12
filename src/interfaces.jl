c71640aa16a70ba94f42cd832f3735000487d488
Simon <sdanisch@gmail.com>
2018-11-12 16:25:42 +0100

    Lifted convert_arguments + attribute propagation (#42)
    Former-commit-id: 6103cafd88f8a9a90f0147416e9ea539cf205360

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -298,34 +298,109 @@ function (PT::Type{<: Combined})(parent, transformation, attributes, input_args,
 end
 plotsym(::Type{<:AbstractPlot{F}}) where F = Symbol(typeof(F).name.mt.name)
 
-function (PlotType::Type{<: AbstractPlot{Typ}})(scene::SceneLike, attributes::Attributes, args::Tuple) where Typ
-    # make sure all arguments are a node
-    # with a sensible name
-    arg_nodes = node.(ntuple(i-> Symbol("input $i"), length(args)), args)
-    args_converted = lift(arg_nodes...) do args...
-        # do the argument conversion inside a lift
-        convert_arguments(PlotType, args...)
+"""
+    used_attributes(args...) = ()
+
+function used to indicate what keyword args one wants to get passed in `convert_arguments`.
+Usage:
+```example
+    struct MyType end
+    used_attributes(::MyType) = (:attribute,)
+    function convert_arguments(x::MyType; attribute = 1)
+        ...
     end
-    # now get a signal node/signal for each argument
-    N = length(to_value(args_converted))
-    names = argument_names(PlotType, N)
-    node_args_seperated = ntuple(N) do i
-        lift(args_converted, name = string(names[i])) do x
+    # attribute will get passed to convert_arguments
+    # without keyword_verload, this wouldn't happen
+    plot(MyType, attribute = 2)
+    #You can also use the convenience macro, to overload convert_arguments in one step:
+    @keywords convert_argumetns(x::MyType; attribute = 1)
+        ...
+    end
+```
+"""
+used_attributes(PlotType, args...) = ()
+
+
+"""
+apply for return type
+    (args...,)
+"""
+function apply_convert!(P, attributes::Attributes, x::Tuple)
+    return (plottype(P, x...), x)
+end
+
+
+"""
+apply for return type
+    PlotType => (args...)
+
+"""
+function apply_convert!(P, attributes::Attributes, x::Pair)
+    return x[1], x[2]
+end
+
+"""
+apply for return type
+    (
+        (args...),
+        (
+            attribute = :red
+        )
+    )
+"""
+function apply_convert!(P, attributes::Attributes, x::Tuple{<: Tuple, <: NamedTuple})
+    args, kw = x
+    for (k, v) in kw
+        attributes[k] = v
+    end
+    return plottype!(P, attributes, args)
+end
+
+"""
+apply for return type
+    PlotType => (
+        (args...),
+        (
+            attribute = :red
+        )
+    )
+"""
+function apply_convert!(P, attributes::Attributes, x::Pair{Type{<: AbstractPlot}, <: Tuple{<: Tuple, <: NamedTuple}})
+    ptype, (args, kw) = x
+    for (k, v) in kw
+        attributes[k] = v
+    end
+    return apply_convert!(P, attributes, ptype => args)
+end
+
+function seperate_tuple(args::Node{<: NTuple{N, Any}}) where N
+    ntuple(N) do i
+        lift(args) do x
             if i <= length(x)
                 x[i]
             else
-                error("You changed the number of arguments for $PlotType. This isn't allowed!")
+                error("You changed the number of arguments. This isn't allowed!")
             end
         end
     end
+end
+
+function (PlotType::Type{<: AbstractPlot{Typ}})(scene::SceneLike, attributes::Attributes, args) where Typ
+    input = to_node.(args)
+    argnodes = lift(input...) do args...
+        convert_arguments(PlotType, args...)
+    end
+    PlotType(scene, attributes, input, argnodes)
+end
+
+function (PlotType::Type{<: AbstractPlot{Typ}})(scene::SceneLike, attributes::Attributes, input, args) where Typ
     # The argument type of the final plot object is the assumened to stay constant after
     # argument conversion. This might not always hold, but it simplifies
     # things quite a bit
-    ArgTyp = typeof(to_value(args_converted))
+    ArgTyp = typeof(to_value(args))
     # construct the fully qualified plot type, from the possible incomplete (abstract)
     # PlotType
     FinalType = Combined{Typ, ArgTyp}
-
     plot_attributes, scene_attributes = merged_get!(()-> default_theme(scene, FinalType), plotsym(FinalType), scene, attributes)
     trans = get(plot_attributes, :transformation, automatic)
     transformation = if to_value(trans) == automatic
@@ -341,9 +416,8 @@ function (PlotType::Type{<: AbstractPlot{Typ}})(scene::SceneLike, attributes::At
     replace_automatic!(plot_attributes, :model) do
         transformation.model
     end
-
     # create the plot, with the full attributes, the input signals, and the final signal nodes.
-    plot_obj = FinalType(scene, transformation, plot_attributes, arg_nodes, node_args_seperated)
+    plot_obj = FinalType(scene, transformation, plot_attributes, input, seperate_tuple(args))
     calculated_attributes!(plot_obj)
     plot_obj, scene_attributes
 end
@@ -361,11 +435,27 @@ e.g.:
     plottype(x::Array{<: AbstractFlot, 3}) = Volume
 ```
 """
-plottype(plot_args...) = Combined{Any, Tuple{typeof.(to_value.(plot_args))...}}
-
-
+plottype(plot_args...) = Combined{Any, Tuple{typeof.(to_value.(plot_args))...}} # default to dispatch to type recipes!
 plottype(::AbstractVector, ::AbstractVector) = Lines
 plottype(::AbstractMatrix) = Heatmap
+# If the Combined has no plot func, calculate them
+plottype(::Type{<: Combined{Any}}, argvalues...) = plottype(argvalues...)
+# If it has something more concrete than Any, use it directly
+plottype(P::Type{<: Combined{T}}, argvalues...) where T = P
+
+"""
+    plottype(P1::Type{<: Combined{T1}}, P2::Type{<: Combined{T2}})
+
+Chooses the more concrete plot type
+```example
+function convert_arguments(P::PlotFunc, args...)
+    ptype = plottype(P, Lines)
+    ...
+end
+"""
+plottype(P1::Type{<: Combined{Any}}, P2::Type{<: Combined{T}}) where T = P2
+plottype(P1::Type{<: Combined{T}}, P2::Type{<: Combined}) where T = P1
+
 
 """
 Returns the Combined type that represents the signature of `args`.
@@ -389,7 +479,6 @@ plot(scene::SceneLike, P::PlotFunc, args...; kw_attributes...) = plot!(Scene(sce
 plot!(scene::SceneLike, P::PlotFunc, args...; kw_attributes...) = plot!(scene, P, Attributes(kw_attributes), args...)
 
 plot(scene::SceneLike, P::PlotFunc, attributes::Attributes, args...; kw_attributes...) = plot!(Scene(scene), P, merge!(Attributes(kw_attributes), attributes), args...)
-plot!(scene::SceneLike, P::PlotFunc, attributes::Attributes, args...; kw_attributes...) = plot!(scene, P, merge!(Attributes(kw_attributes), attributes), args...)
 plot!(P::PlotFunc, attributes::Attributes, args...; kw_attributes...) = plot!(current_scene(), P, merge!(Attributes(kw_attributes), attributes), args...)
 plot(P::PlotFunc, attributes::Attributes, args...; kw_attributes...) = plot!(Scene(), P, merge!(Attributes(kw_attributes), attributes), args...)
 
@@ -398,12 +487,45 @@ eval(default_plot_signatures(:plot, :plot!, :Any))
 
 # plots to scene
 
+plotfunc(::Combined{F}) where F = F
+
+
+
 """
 Main plotting signatures that plot/plot! route to if no Plot Type is given
 """
-function plot!(scene::SceneLike, ::Type{Any}, attributes::Attributes, args...)
-    PlotType = plottype(to_value.(args)...)
-    plot!(scene, PlotType, attributes, args...)
+function plot!(scene::SceneLike, P::PlotFunc, attributes::Attributes, args...; kw_attributes...)
+    attributes = merge!(Attributes(kw_attributes), attributes)
+    argvalues = to_value.(args)
+    PreType = plottype(P, argvalues...)
+    # plottype will lose the argument types, so we just extract the plot func
+    # type and recreate the type with the argument type
+    PreType = Combined{plotfunc(PreType), typeof(argvalues)}
+    convert_keys = intersect(used_attributes(PreType, argvalues...), keys(attributes))
+    kw_signal = if isempty(convert_keys) # lift(f) isn't supported so we need to catch the empty case
+        Node(())
+    else
+        lift((args...)-> Pair.(convert_keys, args), getindex.(attributes, convert_keys)...) # make them one tuple to easier pass through
+    end
+    # call convert_arguments for a first time to get things started
+    converted = convert_arguments(PreType, argvalues...; kw_signal[]...)
+    # convert_arguments can return different things depending on the recipe type
+    # apply_conversion deals with that!
+    FinalType, argsconverted = apply_convert!(PreType, attributes, converted)
+    converted_node = Node(argsconverted)
+    input_nodes =  to_node.(args)
+    onany(kw_signal, lift(tuple, input_nodes...)) do kwargs, args
+        # do the argument conversion inside a lift
+        result = convert_arguments(FinalType, args...; kwargs...)
+        finaltype, argsconverted = apply_convert!(FinalType, attributes, result)
+        if finaltype != FinalType
+            error("Plot type changed from $FinalType to $finaltype after conversion.
+                Changing the plot type based on values in convert_arguments is not allowed"
+            )
+        end
+        converted_node[] = argsconverted
+    end
+    plot!(scene, FinalType, attributes, input_nodes, converted_node)
 end
 
 plot!(p::Combined) = _plot!(p)
@@ -422,9 +544,10 @@ function _plot!(p::Combined{X, T}) where {X, T}
 end
 
 
-function plot!(scene::SceneLike, ::Type{PlotType}, attributes::Attributes, args...) where PlotType <: AbstractPlot
+function plot!(scene::SceneLike, ::Type{PlotType}, attributes::Attributes, input::NTuple{N, Node}, args::Node) where {N, PlotType <: AbstractPlot}
+
     # create "empty" plot type - empty meaning containing no plots, just attributes + arguments
-    plot_object, scene_attributes = PlotType(scene, attributes, args)
+    plot_object, scene_attributes = PlotType(scene, attributes, input, args)
 
     attributes, rest = merge_attributes!(scene_attributes, theme(scene))
     # TODO warn about rest - should be unused arguments!
@@ -445,7 +568,6 @@ function plot!(scene::SceneLike, ::Type{PlotType}, attributes::Attributes, args.
         axis = pop!(scene.plots)
         pushfirst!(scene.plots, axis)
     end
-    #compose_plot!(scene)
     # call the assembly recipe, that also adds this to the scene
     # kw_args not consumed by PlotType will be passed forward to plot! as non_plot_kwargs
     #plot!(scene, plot_object, scene_attributes)
