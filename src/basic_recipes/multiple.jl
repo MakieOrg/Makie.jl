cd71d971bb7f9b74865cf89e6cda4ec597363a56
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-20 12:57:45 +0000

    introduce PlotSpecs

--- src/basic_recipes/multiple.jl
+++ src/basic_recipes/multiple.jl
@@ -1,29 +1,29 @@
-export PlotList
+export PlotList, PlotSpecs
 
-abstract type AbstractPlotList{T, S} <: AbstractVector{T} end
-
-to_vector(T::Type, args...) = convert(Vector{T}, to_vector(args...))
-to_vector(v::AbstractVector, n) = convert(Vector, v)
-to_vector(v, n) = fill(v, n)
-
-function tuple_type(v::AbstractVector)
-    vec = unique(first(el) for el in v if el isa Pair)
-    Tuple{vec...}
+struct PlotSpecs{P<:AbstractPlot}
+    args::Tuple
+    transform_attributes::Function
 end
 
-tuple_type(::Type{<:AbstractPlotList{T, S}}) where {T, S} = S
+PlotSpecs(args...) = PlotSpecs{Combined{Any}}(args...)
+PlotSpecs{P}(args::Tuple) where {P} = PlotSpecs{P}(args, identity)
+PlotSpecs{P}(x::PlotSpecs) where {P} = PlotSpecs{P}(x.args, x.transform_attributes)
 
-struct PlotList{T, S} <: AbstractPlotList{T, S}
-    plots::Vector{T}
-    transform_attributes::Vector{Function}
-    function PlotList(plots::AbstractVector{T}; transform_attributes = identity) where {T}
-        plots_vec = convert(Vector{T}, plots)
-        funcs_vec = to_vector(Function, transform_attributes, length(plots_vec))
-        new{T, tuple_type(plots)}(plots_vec, funcs_vec)
+plottype(::PlotSpecs{P}) where {P} = P
+
+abstract type AbstractPlotList{T<:Tuple} end
+
+plottype(::Type{<:AbstractPlotList{T}}) where {T} = T.parameters
+
+struct PlotList{T<:Tuple} <: AbstractPlotList{T}
+    plots::Tuple
+    transform_attributes::Function
+    function PlotList(plots...; transform_attributes::Function = identity)
+        T = Tuple{unique(plottype.(plots))...}
+        new{T}(plots, transform_attributes)
     end
 end
 
-PlotList(args...; kwargs...) = PlotList(collect(args); kwargs...)
 
 Base.parent(p::PlotList) = p.plots
 
@@ -35,31 +35,34 @@ Base.size(m::AbstractPlotList) = size(parent(m))
 end
 
 function default_theme(scene, ::Type{<:Combined{multipleplot, Tuple{P}}}) where {P<:AbstractPlotList}
-    merge((default_theme(scene, pt) for pt in tuple_type(P).parameters)...)
+    merge((default_theme(scene, pt) for pt in plottype(P))...)
 end
 # Allow MultiplePlot to prevail on user input: the plot type of each series will be defined in convert_arguments
 plottype(::Type{<: Combined{Any}}, A::Type{<:MultiplePlot}, argvalues...) = A
 plottype(::Type{<: Combined{T}}, A::Type{<:MultiplePlot}, argvalues...) where T = A
 
-function convert_arguments(P::PlotFunc, m::PlotList)
-    function convert_series(s)
-        ptype, args = to_pair(P, s)
-        to_pair(ptype, convert_arguments(ptype, args...))
-    end
-    MultiplePlot => (PlotList(convert_series.(m); transform_attributes = m.transform_attributes),)
-end
-
 to_pair(P, t) = P => t
 to_pair(P, p::Pair) = to_pair(plottype(P, first(p)), last(p))
+
+function convert_arguments(P::PlotFunc, m::PlotList)
+    function convert_series(plot::PlotSpecs)
+        ptype = plottype(P, plottype(plot))
+        finaltype, converted_args = to_pair(ptype, convert_arguments(ptype, plot.args...))
+        PlotSpecs{finaltype}(converted_args, plot.transform_attributes)
+    end
+    pl = PlotList(convert_series.(m.plots)...; transform_attributes = m.transform_attributes)
+    MultiplePlot => (pl,)
+end
 
 # This allows plotting an arbitrary combination of series form one argument
 # The recipe framework can be constructed using this as a building block and computing
 # PlotList with convert_arguments
 function plot!(p::Combined{multipleplot, <:Tuple{PlotList}})
     mp = to_value(p[1]) # TODO how to preserve interactivity here, as number of series may change?
-    for (i, s) in enumerate(mp)
-        PlotType, args = s
-        attr = mp.transform_attributes[i](Theme(p))
-        plot!(p, PlotType, attr, args...)
+    theme = mp.transform_attributes(Theme(p))
+    for s in mp.plots
+        args, transform_attributes = s.args, s.transform_attributes
+        attr = transform_attributes(theme)
+        plot!(p, plottype(s), attr, args...)
     end
 end
