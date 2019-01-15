91c5c96f4850b62f20f3ef3bbdbc5900a6a943ec
Simon <sdanisch@gmail.com>
2019-01-15 17:17:03 +0100

    update tomls (#273)
    * update tomls
    
    * fix tests
    
    * update package versions
    
    Former-commit-id: 8e9fb1ed3cdb6a565914bb2c825117e170fcdd0e

--- src/Makie.jl
+++ src/Makie.jl
@@ -1,20 +1,17 @@
 module Makie
 
-using AbstractPlotting, GeometryTypes
-import IntervalSets, FileIO
-using IntervalSets: ClosedInterval, (..)
+using AbstractPlotting, GLMakie
+import FileIO
+using GLMakie
+using GLMakie: assetpath, loadasset
 
 for name in names(AbstractPlotting)
     @eval import AbstractPlotting: $(name)
     @eval export $(name)
 end
 
-
 function logo()
     FileIO.load(joinpath(@__DIR__, "..", "assets", "logo.png"))
 end
-
-using GLMakie
-using GLMakie: assetpath, loadasset
 
 end
