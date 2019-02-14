14f98408ef6a452fc713ed8f6659a35b933722e1
SimonDanisch <sdanisch@gmail.com>
2019-02-14 10:46:27 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,7 @@
 using MakieGallery, AbstractPlotting, GLMakie
 
 push!(MakieGallery.plotting_backends, "GLMakie")
-
 database = MakieGallery.load_database()
-# THese examples download additional data - don't want to deal with that!
-to_skip = ["WorldClim visualization", "Image on Geometry (Moon)", "Image on Geometry (Earth)"]
-# we directly modify the database, which seems easiest for now
-filter!(entry-> !(entry.title in to_skip), database)
-
-ref_path = MakieGallery.download_reference(v"0.0.9")
-
 tested_diff_path = joinpath(@__DIR__, "tested_different")
 test_record_path = joinpath(@__DIR__, "test_recordings")
 rm(tested_diff_path, force = true, recursive = true)
@@ -18,4 +10,4 @@ rm(test_record_path, force = true, recursive = true)
 mkpath(test_record_path)
 
 MakieGallery.record_examples(test_record_path)
-MakieGallery.run_comparison(test_record_path, ref_path, tested_diff_path)
+MakieGallery.run_comparison(test_record_path, tested_diff_path)
