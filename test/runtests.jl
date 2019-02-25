9e86dde3965cc293298a4b792c6c966a6de4745c
SimonDanisch <sdanisch@gmail.com>
2019-02-25 11:04:36 +0100

    improved tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,17 @@
-using MakieGallery, AbstractPlotting, GLMakie
+using MakieGallery, AbstractPlotting, GLMakie, Test
 
 push!(MakieGallery.plotting_backends, "GLMakie")
 database = MakieGallery.load_database()
 tested_diff_path = joinpath(@__DIR__, "tested_different")
 test_record_path = joinpath(@__DIR__, "test_recordings")
-rm(tested_diff_path, force = true, recursive = true)
-mkpath(tested_diff_path)
-rm(test_record_path, force = true, recursive = true)
-mkpath(test_record_path)
+for path in (tested_diff_path, test_record_path)
+    rm(path, force = true, recursive = true)
+    mkpath(path)
+end
+recordings = MakieGallery.record_examples(test_record_path)
+@test length(recordings) == length(database)
+
+MakieGallery.run_comparison(test_record_path, tested_diff_path)
 
 MakieGallery.record_examples(test_record_path)
 MakieGallery.run_comparison(test_record_path, tested_diff_path)
