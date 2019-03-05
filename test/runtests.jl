95995bd121125d197f7fdefe7273f229343e6ece
SimonDanisch <sdanisch@gmail.com>
2019-03-05 18:44:07 +0100

    clean up runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,17 @@ for path in (tested_diff_path, test_record_path)
 end
 recordings = MakieGallery.record_examples(test_record_path)
 @test length(recordings) == length(database)
+MakieGallery.run_comparison(test_record_path, tested_diff_path)
 
 empty!(database) # remove other examples
 include("glmakie_tests.jl") # include GLMakie specific tests
 # THese examples download additional data - don't want to deal with that!
+for path in (tested_diff_path, test_record_path)
+    rm(path, force = true, recursive = true)
+    mkpath(path)
+end
 examples = MakieGallery.record_examples(test_record_path)
 MakieGallery.run_comparison(test_record_path, tested_diff_path, maxdiff = 0.0)
+# MakieGallery.generate_preview(test_record_path)
+# repo = joinpath(homedir(), "ReferenceImages", "gallery")
+# cp(test_record_path, repo, force = true)
