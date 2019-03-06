25e00d87eeb1679dd2ba578a62d3c0bde0864c3b
SimonDanisch <sdanisch@gmail.com>
2019-03-06 22:06:29 +0100

    update versions
    Former-commit-id: 25532c135237e46f2bc74ce35b9f505c35502bc8

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,14 @@
-using Pkg
-#pkg"dev https://github.com/JuliaPlots/MakieGallery.jl"
+using MakieGallery, AbstractPlotting, GLMakie, Test
+using MakieGallery: @block, @cell
 
-using MakieGallery, Makie
+push!(MakieGallery.plotting_backends, "GLMakie")
 database = MakieGallery.load_database()
-# THese examples download additional data - don't want to deal with that!
-to_skip = ["WorldClim visualization", "Image on Geometry (Moon)", "Image on Geometry (Earth)"]
-# we directly modify the database, which seems easiest for now
-filter!(entry-> !(entry.title in to_skip), database)
-
-ref_path = MakieGallery.download_reference(v"0.0.9")
-
 tested_diff_path = joinpath(@__DIR__, "tested_different")
 test_record_path = joinpath(@__DIR__, "test_recordings")
-rm(tested_diff_path, force = true, recursive = true)
-mkpath(tested_diff_path)
-rm(test_record_path, force = true, recursive = true)
-mkpath(test_record_path)
-
-MakieGallery.record_examples(test_record_path)
-MakieGallery.run_comparison(test_record_path, ref_path, tested_diff_path)
+for path in (tested_diff_path, test_record_path)
+    rm(path, force = true, recursive = true)
+    mkpath(path)
+end
+recordings = MakieGallery.record_examples(test_record_path)
+@test length(recordings) == length(database)
+MakieGallery.run_comparison(test_record_path, tested_diff_path)
