db0699561ee99b58400621fd47fff8b27117d225
Jan Weidner <jw3126@gmail.com>
2019-04-12 21:31:29 +0200

    fix

--- src/basic_recipes/title.jl
+++ src/basic_recipes/title.jl
@@ -1,30 +1,28 @@
 @recipe(Title, titletext, plot) do scene
-    Theme(
-        textsize=40,
-        raw=true,
-        font=theme(scene, :font),
-        align = (:center, :bottom), 
-    )
+    t = default_theme(scene, Text)
+    t[:align] = (:center, :bottom)
+    t
 end
 
-function convert_arguments(::Type{<:Title}, titletext::AbstractString, plot::Scene)
-    @info "convert_argument called"
-    (titletext, plot)
-end
 function AbstractPlotting.plot!(t::Title)
-    @extract t (titletext, plot)
-    
+    @extract t (titletext, plot, align, alpha, color, font,
+                linewidth, overdraw, rotation,
+                strokecolor, strokewidth,
+                textsize, transparency, visible)
+
     plot = to_value(plot)
     pos = lift(pixelarea(plot)) do area
         x = widths(area)[1] ./ 2
         Vec2f0(x, 10) # offset 10px, to give it some space
     end
-    
-    @show typeof(t.parent)
+
     text!(t.parent,
         titletext,
         position = pos, 
         camera=campixel!,
         raw=true,
+        align=align,
+        alpha=alpha, color=color, font=font, linewidth=linewidth, overdraw=overdraw, rotation=rotation,
+        strokecolor= strokecolor, strokewidth=strokewidth, textsize=textsize, transparency=transparency, visible=visible
         )
 end
