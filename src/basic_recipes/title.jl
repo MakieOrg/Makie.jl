1540794c6ba5867a8456d5a584b1834890798ee5
Jan Weidner <jw3126@gmail.com>
2019-04-15 22:02:54 +0200

    fix

--- src/basic_recipes/title.jl
+++ src/basic_recipes/title.jl
@@ -1,30 +1,30 @@
-export addtitle
-
 """
-    title(titletext, plot)
+    title(titletext, scene)
 
-Create a `title` for `plot`. Note that the title is not automatically added to `plot`. Instead [`hbox`](@ref) can be used for this. Alternatively the [`addtitle`](@ref) convenience does create a title and add it to the plot.
+Create a `title` for `scene`. Note that the title is not automatically added to `scene`. Instead [`hbox`](@ref) can be used for this. Alternatively [`addtitle`](@ref) does create a title and add it to the scene.
 """
-@recipe(Title, titletext, plot) do scene
-    t = default_theme(scene, Text)
+@recipe(Title, scene, string) do s
+    t = default_theme(s, Text)
     t[:align] = (:center, :bottom)
     t
 end
 
 function AbstractPlotting.plot!(t::Title)
-    @extract t (titletext, plot, align, alpha, color, font,
+    @extract t (scene, string, align, alpha, color, font,
                 linewidth, overdraw, rotation,
                 strokecolor, strokewidth,
                 textsize, transparency, visible)
 
-    plot = to_value(plot)
-    pos = lift(pixelarea(plot)) do area
+    # scene is the scene in need of a title. It is not the scene which contains
+    # the title in the end.
+    s = to_value(scene)
+    pos = lift(pixelarea(s)) do area
         x = widths(area)[1] ./ 2
         Vec2f0(x, 10) # offset 10px, to give it some space
     end
 
     text!(t.parent,
-        titletext,
+        string,
         position = pos, 
         camera=campixel!,
         raw=true,
@@ -40,9 +40,9 @@ end
 Add a title with content `string` to `scene`.
 """
 function addtitle(scene, string; kw...)
-    t = title(string, scene; kw...)
+    t = title(scene, string; kw...)
     hbox(scene, t)
-end # works
+end
 
 function addtitle(string; kw...)
     scene = current_scene()
