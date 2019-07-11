ba31c6b7f5c736e52046efb6c08b0cb52d43d18d
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-11 15:03:03 +0530

    polish the tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,14 +4,14 @@ using Test
 # does this machine have a OPENGL?
 const OPENGL = haskey(ENV, "OPENGL") || haskey(ENV, "GITLAB_CI") || try; success(pipeline(`glxinfo`, `grep version`)); catch; false; end # if it's Gitlab, it must be JuliaGPU
 
-OPENGL && @info "OpenGL detected"
+OPENGL && begin @info "OpenGL detected"; using GLMakie end
+OPENGL || @warn "No OpenGL detected!  Software tests only."
 
 # does this machine have FFMPEG?  We'll take it on faith if you tell us...
 const fmp = haskey(ENV, "FFMPEG") || try; AbstractPlotting.@ffmpeg_env success(`$ffmpeg -version`); catch; false end;
 
-const _MINIMAL = get(ENV, "ABSTRACTPLOTTING_MINIMAL", "true")
+const _MINIMAL = get(ENV, "ABSTRACTPLOTTING_MINIMAL", "false")
 
-OPENGL && using GLMakie
 
 scene = scatter(rand(4))
 
