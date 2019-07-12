d4fc6e7593f1aef3f6e776c3bdf30dc7b5d7ba78
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-12 10:47:45 +0530

    less semicolons
    Former-commit-id: 953675a4c98779922da0565846445ece10eb2b99

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using AbstractPlotting
 using Test
 
 # does this machine have a OPENGL?
-const OPENGL = haskey(ENV, "OPENGL") || haskey(ENV, "GITLAB_CI") || try; success(pipeline(`glxinfo`, `grep version`)); catch; false; end # if it's Gitlab, it must be JuliaGPU
+const OPENGL = haskey(ENV, "OPENGL") || haskey(ENV, "GITLAB_CI") || try success(pipeline(`glxinfo`, `grep version`)) catch; false end # if it's Gitlab, it must be JuliaGPU
 
 OPENGL && begin @info "OpenGL detected"; using GLMakie end
 OPENGL || @warn "No OpenGL detected!  Software tests only."
