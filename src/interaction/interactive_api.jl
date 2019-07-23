c0fb7b1066abe7463fd52db95e36e6e5e4f0a272
smldis <smldis@gmail.com>
2019-07-23 06:36:12 +0200

    Remove extra ` in docstring (#170)
    Former-commit-id: 64c15b8f290ee2de8a66d194612b04ab1d5c8101

--- src/interaction/interactive_api.jl
+++ src/interaction/interactive_api.jl
@@ -15,7 +15,7 @@ end
 """
     onpick(f, scene::SceneLike, plots::AbstractPlot...)
 
-Calls `f(idx)`` whenever the mouse is over any of `plots`.
+Calls `f(idx)` whenever the mouse is over any of `plots`.
 `idx` is an index, e.g. when over a scatter plot, it will be the index of the
 hovered element
 """
