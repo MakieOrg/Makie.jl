e560c6ac5d3def16591d30ed39d561d470d046ff
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-03 13:03:11 +0100

    update layoutedcolorbar with layoutnodes

--- layoutedobjects/layoutedcolorbar.jl
+++ layoutedobjects/layoutedcolorbar.jl
@@ -18,27 +18,24 @@ function LayoutedColorbar(parent::Scene; kwargs...)
         tickwidth, tickcolor, spinewidth, idealtickdistance, topspinevisible,
         rightspinevisible, leftspinevisible, bottomspinevisible, topspinecolor,
         leftspinecolor, rightspinecolor, bottomspinecolor, colormap, limits,
-        alignment, vertical, flipaxisposition, ticklabelalign, width, height)
+        alignment, vertical, flipaxisposition, ticklabelalign)
+
+    widthattr = attrs.width
+    heightattr = attrs.height
 
     decorations = Dict{Symbol, Any}()
 
-    bboxnode = Node(BBox(0, 100, 0, 100))
+    suggestedbbox = Node(BBox(0, 100, 0, 100))
 
-    scenearea = Node(IRect(0, 0, 100, 100))
+    computedwidth = computedsizenode!(1, widthattr)
+    computedheight = computedsizenode!(2, heightattr)
 
     # here limits isn't really useful, maybe split up the functions for colorbar and axis
-    connect_scenearea_and_bbox_colorbar!(scenearea, bboxnode, width, height, alignment)
+    finalbbox = alignedbboxnode!(suggestedbbox, computedwidth, computedheight, alignment)
+
+    scenearea = lift(IRect2D, finalbbox)
 
     scene = Scene(parent, scenearea, camera = campixel!, raw = true)
-
-    # # have one standard projection that always fits the mesh
-    # on(scene.px_area) do pxarea
-    #     pxarea = BBox(pxarea)
-    #     projection = AbstractPlotting.orthographicprojection(
-    #         0f0, pxarea.widths[1], 0f0, pxarea.widths[2], -10000f0, 10000f0)
-    #     camera(scene).projection[] = projection
-    #     camera(scene).projectionview[] = projection
-    # end
 
     framebox = lift(scene.px_area) do pxa
         BBox(0, pxa.widths[1], 0, pxa.widths[2])
@@ -87,14 +84,6 @@ function LayoutedColorbar(parent::Scene; kwargs...)
         rightspinecolor, bottomspinecolor)
 
     campixel!(scene)
-
-    protrusions = Node(RectSides{Float32}(0, 0, 0, 0))
-
-    needs_update = Node(false)
-
-    on(protrusions) do p
-        needs_update[] = true
-    end
 
     axispoints = lift(scenearea, vertical, flipaxisposition) do scenearea,
             vertical, flipaxisposition
@@ -147,118 +136,20 @@ function LayoutedColorbar(parent::Scene; kwargs...)
         RectSides{Float32}(left, right, bottom, top)
     end
 
-    LayoutedColorbar(
-        parent, scene, bboxnode, protrusions,
-        needs_update, attrs, decorations)
+    layoutnodes = LayoutNodes(suggestedbbox, protrusions, computedwidth, computedheight, finalbbox)
+
+    LayoutedColorbar(parent, scene, layoutnodes, attrs, decorations)
 end
 
 defaultlayout(lc::LayoutedColorbar) = ProtrusionLayout(lc)
 
-function protrusionnode(lc::LayoutedColorbar)
-    # work around the new optional protrusions
-    node = Node{Union{Nothing, RectSides{Float32}}}(lc.protrusions[])
-    on(lc.protrusions) do p
-        node[] = p
-    end
-    node
-end
-
-function sizenodecontent(s)
-    if s isa Union{Real, Fixed}
-        s
-    else
-        nothing
-    end
-end
-
-function widthnode(lc::LayoutedColorbar)
-    node = Node{Union{Nothing, Float32}}(sizenodecontent(lc.attributes.width[]))
-    on(lc.attributes.width) do w
-        node[] = sizenodecontent(w)
-    end
-    node
-end
-
-function heightnode(lc::LayoutedColorbar)
-    node = Node{Union{Nothing, Float32}}(sizenodecontent(lc.attributes.height[]))
-    on(lc.attributes.height) do h
-        node[] = sizenodecontent(h)
-    end
-    node
-end
+protrusionnode(lc::LayoutedColorbar) = lc.layoutnodes.protrusions
+widthnode(lc::LayoutedColorbar) = lc.layoutnodes.computedwidth
+heightnode(lc::LayoutedColorbar) = lc.layoutnodes.computedheight
 
 function align_to_bbox!(lc::LayoutedColorbar, bbox)
-    lc.bboxnode[] = bbox
+    lc.layoutnodes.suggestedbbox[] = bbox
 end
-
-
-function connect_scenearea_and_bbox_colorbar!(scenearea, bboxnode, widthnode, heightnode, alignment)
-    onany(bboxnode, widthnode, heightnode, alignment) do bbox, widthnode, heightnode, alignment
-
-        w = width(bbox)
-        h = height(bbox)
-
-
-        mw = if isnothing(widthnode)
-            w
-        elseif widthnode isa Real
-            widthnode
-        elseif widthnode isa Fixed
-            widthnode.x
-        elseif widthnode isa Relative
-            widthnode.x * w
-        else
-            error("Invalid width $widthnode, can only be Fixed, Relative or Real")
-        end
-
-        mh = if isnothing(heightnode)
-            h
-        elseif heightnode isa Real
-            heightnode
-        elseif heightnode isa Fixed
-            heightnode.x
-        elseif heightnode isa Relative
-            heightnode.x * h
-        else
-            error("Invalid height $heightnode, can only be Fixed, Relative or Real")
-        end
-
-        restw = w - mw
-        resth = h - mh
-
-        xalign = if alignment[1] == :left
-            0
-        elseif alignment[1] == :center
-            0.5
-        elseif alignment[1] == :right
-            1
-        else
-            error("Invalid x alignment $(alignment[1])")
-        end
-
-        yalign = if alignment[2] == :bottom
-            0
-        elseif alignment[2] == :center
-            0.5
-        elseif alignment[2] == :top
-            1
-        else
-            error("Invalid y alignment $(alignment[1])")
-        end
-
-        l = left(bbox) + xalign * restw
-        b = bottom(bbox) + yalign * resth
-
-        newbbox = BBox(l, l + mw, b, b + mh)
-
-        # only update scene if pixel positions change
-        new_scenearea = IRect2D(newbbox)
-        if new_scenearea != scenearea[]
-            scenearea[] = new_scenearea
-        end
-    end
-end
-
 
 function tight_ticklabel_spacing!(lc::LayoutedColorbar)
     tight_ticklabel_spacing!(lc.decorations[:axis])
