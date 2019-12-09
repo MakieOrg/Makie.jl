7d4de3ff04530044ea7cbfc1f6efa4fb54c3f89e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-12-09 18:08:49 +0100

    move functions to gridlayout.jl

--- gridlayout.jl
+++ gridlayout.jl
@@ -113,3 +113,231 @@ end
 function request_update(gl::GridLayout, parent::GridLayout)
     parent.needs_update[] = true
 end
+
+
+function convert_contentsizes(n, sizes)::Vector{ContentSize}
+    if isnothing(sizes)
+        [Auto() for _ in 1:n]
+    elseif sizes isa ContentSize
+        [sizes for _ in 1:n]
+    elseif sizes isa Vector{<:ContentSize}
+        length(sizes) == n ? sizes : error("$(length(sizes)) sizes instead of $n")
+    else
+        error("Illegal sizes value $sizes")
+    end
+end
+
+function convert_gapsizes(n, gaps)::Vector{GapSize}
+    if isnothing(gaps)
+        [Fixed(20) for _ in 1:n]
+    elseif gaps isa GapSize
+        [gaps for _ in 1:n]
+    elseif gaps isa Vector{<:GapSize}
+        length(gaps) == n ? gaps : error("$(length(gaps)) gaps instead of $n")
+    else
+        error("Illegal gaps value $gaps")
+    end
+end
+
+function appendrows!(gl::GridLayout, n::Int; rowsizes=nothing, addedrowgaps=nothing)
+
+    rowsizes = convert_contentsizes(n, rowsizes)
+    addedrowgaps = convert_gapsizes(n, addedrowgaps)
+
+    with_updates_suspended(gl) do
+        gl.nrows += n
+        append!(gl.rowsizes, rowsizes)
+        append!(gl.addedrowgaps, addedrowgaps)
+    end
+end
+
+function appendcols!(gl::GridLayout, n::Int; colsizes=nothing, addedcolgaps=nothing)
+
+    colsizes = convert_contentsizes(n, colsizes)
+    addedcolgaps = convert_gapsizes(n, addedcolgaps)
+
+    with_updates_suspended(gl) do
+        gl.ncols += n
+        append!(gl.colsizes, colsizes)
+        append!(gl.addedcolgaps, addedcolgaps)
+    end
+end
+
+function prependrows!(gl::GridLayout, n::Int; rowsizes=nothing, addedrowgaps=nothing)
+
+    rowsizes = convert_contentsizes(n, rowsizes)
+    addedrowgaps = convert_gapsizes(n, addedrowgaps)
+
+    gl.content = map(gl.content) do spal
+        span = spal.sp
+        newspan = Span(span.rows .+ n, span.cols)
+        SpannedLayout(spal.al, newspan, spal.side)
+    end
+
+    with_updates_suspended(gl) do
+        gl.nrows += n
+        prepend!(gl.rowsizes, rowsizes)
+        prepend!(gl.addedrowgaps, addedrowgaps)
+    end
+end
+
+function prependcols!(gl::GridLayout, n::Int; colsizes=nothing, addedcolgaps=nothing)
+
+    colsizes = convert_contentsizes(n, colsizes)
+    addedcolgaps = convert_gapsizes(n, addedcolgaps)
+
+    gl.content = map(gl.content) do spal
+        span = spal.sp
+        newspan = Span(span.rows, span.cols .+ n)
+        SpannedLayout(spal.al, newspan, spal.side)
+    end
+
+    with_updates_suspended(gl) do
+        gl.ncols += n
+        prepend!(gl.colsizes, colsizes)
+        prepend!(gl.addedcolgaps, addedcolgaps)
+    end
+end
+function gridnest!(gl::GridLayout, rows::Indexables, cols::Indexables)
+
+    newrows, newcols = adjust_rows_cols!(gl, rows, cols)
+
+    subgl = GridLayout(
+        length(newrows), length(newcols);
+        parent = nothing,
+        colsizes = gl.colsizes[newcols],
+        rowsizes = gl.rowsizes[newrows],
+        addedrowgaps = gl.addedrowgaps[newrows.start:(newrows.stop-1)],
+        addedcolgaps = gl.addedcolgaps[newcols.start:(newcols.stop-1)],
+    )
+
+    # remove the content from the parent that is completely inside the replacement grid
+    subgl.block_updates = true
+    i = 1
+    while i <= length(gl.content)
+        spal = gl.content[i]
+
+        if (spal.sp.rows.start >= newrows.start && spal.sp.rows.stop <= newrows.stop &&
+            spal.sp.cols.start >= newcols.start && spal.sp.cols.stop <= newcols.stop)
+
+            detachfromparent!(spal.al) # this deletes the alignable from its old parent already
+            # which would happen anyway hidden in the next assignment, but makes the intent clearer
+            subgl[spal.sp.rows .- (newrows.start - 1), spal.sp.cols .- (newcols.start - 1)] = spal.al
+            continue
+            # don't advance i because there's one piece of content less in the queue
+            # and the next item is in the same position as the old removed one
+        end
+        i += 1
+    end
+    subgl.block_updates = false
+
+    gl[newrows, newcols] = subgl
+
+    subgl
+end
+
+function find_in_grid(obj, container::GridLayout)
+    for i in 1:length(container.content)
+        layout = container.content[i].al
+        if layout isa ProtrusionLayout
+            if layout.content === obj
+                return i
+            end
+        end
+    end
+    nothing
+end
+
+function find_in_grid(layout::AbstractLayout, container::GridLayout)
+    for i in 1:length(container.content)
+        if container.content[i].al === layout
+            return i
+        end
+    end
+    nothing
+end
+
+function topmost_grid(gl::GridLayout)
+    candidate = gl
+    while true
+        if candidate.parent isa GridLayout
+            candidate = candidate.parent
+        else
+            return candidate
+        end
+    end
+end
+
+function find_in_grid_and_subgrids(obj, container::GridLayout)
+    for i in 1:length(container.content)
+        candidate = container.content[i].al
+        # for non layout objects like LAxis we check if they are inside
+        # any protrusion layout we find
+        if candidate isa ProtrusionLayout
+            if candidate.content === obj
+                return container, i
+            end
+        elseif candidate isa GridLayout
+            return find_in_grid_and_subgrids(obj, candidate)
+        end
+    end
+    nothing, nothing
+end
+
+function find_in_grid_and_subgrids(layout::AbstractLayout, container::GridLayout)
+    for i in 1:length(container.content)
+        candidate = container.content[i].al
+        if candidate === layout
+            return container, i
+        elseif candidate isa GridLayout
+            return find_in_grid_and_subgrids(layout, candidate)
+        end
+    end
+    nothing, nothing
+end
+
+function find_in_grid_tree(obj, container::GridLayout)
+    topmost = topmost_grid(container)
+    find_in_grid_and_subgrids(obj, topmost)
+end
+
+function detachfromgridlayout!(obj, gl::GridLayout)
+    i = find_in_grid(obj, gl)
+    if !isnothing(i)
+        deleteat!(gl.content, i)
+    end
+end
+
+function Base.show(io::IO, gl::GridLayout)
+
+    println(io, "GridLayout[$(gl.nrows), $(gl.ncols)] with $(length(gl.content)) children")
+    for (i, c) in enumerate(gl.content)
+        rows = c.sp.rows
+        cols = c.sp.cols
+        al = c.al
+        if i == 1
+            println(io, " ┗━┳━ [$rows | $cols] $(typeof(al))")
+        elseif i == length(gl.content)
+            println(io, "   ┗━ [$rows | $cols] $(typeof(al))")
+        else
+            println(io, "   ┣━ [$rows | $cols] $(typeof(al))")
+        end
+    end
+
+end
+
+function colsize!(gl::GridLayout, i::Int, s::ContentSize)
+    if !(1 <= i <= gl.ncols)
+        error("Can't set size of invalid column $i.")
+    end
+    gl.colsizes[i] = s
+    gl.needs_update[] = true
+end
+
+function rowsize!(gl::GridLayout, i::Int, s::ContentSize)
+    if !(1 <= i <= gl.nrows)
+        error("Can't set size of invalid row $i.")
+    end
+    gl.rowsizes[i] = s
+    gl.needs_update[] = true
+end
