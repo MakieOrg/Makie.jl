713e21e4cfb16eebe124309c33343ed5abb4de30
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-22 12:09:02 -0500

    add formatter kwarg to title (#273)
    * add formatter kwarg to title
    
    fix #272
    
    * lift the string into formatter
    
    * remove staticarrays explicit add again
    
    * fix tests
    
    * add gitlab tag
    
    
    Former-commit-id: 9da077e4cb049f87859e6b03f0575089ab9f5186

--- src/basic_recipes/title.jl
+++ src/basic_recipes/title.jl
@@ -2,18 +2,22 @@
 """
     title(
         [scene=current_scene(), ], string;
-        align = (:center, :bottom), textsize = 30, parent = Scene(), kw...
+        align = (:center, :bottom), textsize = 30, parent = Scene(), formatter = string, kw...
     )
 
-Add a title with content `string` to `scene`.
+Add a title with content `string` to `scene`.  Pass a `Function` to the `formatter` kwarg
+if the value passed to `string` isn't actually a String.
 """
-function title(scene, string; align = (:center, :bottom), textsize = 30, parent = Scene(), kw...)
+function title(scene, tstring; align = (:center, :bottom), textsize = 30, parent = Scene(), formatter = string, kw...)
+    
+    string = to_node(tstring)
+    
     pos = lift(pixelarea(scene)) do area
         x = widths(area)[1] ./ 2
         Vec2f0(x, 10) # offset 10px, to give it some space
     end
     t = text(
-        string,
+        lift(formatter, string),
         position = pos,
         camera = campixel!,
         raw = true, align = align,
