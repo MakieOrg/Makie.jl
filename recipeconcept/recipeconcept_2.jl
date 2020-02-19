897599bd73761bd2bec0fc24a9d2f03e231c6faf
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-19 11:50:23 +0100

    fix small bugs

--- recipeconcept/recipeconcept_2.jl
+++ recipeconcept/recipeconcept_2.jl
@@ -6,6 +6,7 @@ using StatsBase
 using GLFW; GLFW.WindowHint(GLFW.FLOATING, 1)
 using StatsMakie
 using DataFrames
+using Colors
 
 
 struct Infrastructure{T<:NamedTuple}
@@ -124,14 +125,15 @@ function myplot(::Type{Image}, infra::Infrastructure, args...; kwargs...)
     hm = image!(infra.axis, args...; kwargs...)
 end
 
+##
 scene, layout, _ = myplot(Heatmap, rand(100, 100), infra = (;axis = (;aspect = DataAspect(), xlabel = "helloo"))); scene
-
-myplot(Image, rand(100, 100), infra = (; axis = (; xlabel = "hello", ylabel = "world", title = "whoop")))
-
-myplot(Lines, layout[1, 2], xx, yy)
+myplot(Lines, layout[1, 2], 1:10, sin.(1:10))
 scene
+##
 
 myplot(Scatter, Group(color = rand(1:3, 100)), rand(100), rand(100), markersize = 20px)
+
+
 
 struct MarginDensityScatter end
 
@@ -180,10 +182,11 @@ function myplot(::Type{MarginDensityScatter}, infra::Infrastructure, x, y; kwarg
     (scatter = scat, xkde = kde1, ykde = kde2)
 end
 
+##
 scene, layout = layoutscene()
 
 for i in 1:3, j in 1:3
-    myplot(MarginDensityScatter, scene, layout[i, j], randn(100), randn(100);
+    myplot(MarginDensityScatter, layout[i, j], randn(100), randn(100);
         color = rand(RGBAf0),
         infra = (
             marginfraction = 0.2,
@@ -193,16 +196,16 @@ for i in 1:3, j in 1:3
 end
 
 scene
+##
 
-
-
+##
 scene, layout = layoutscene()
 myplot(Scatter, layout[1, 1], rand(100), rand(100), markersize = 20px, color = :green, strokecolor = :black, strokewidth = 1)
 myplot(Scatter, layout[2, 1], rand(100), rand(100), markersize = 20px, color = :red, strokecolor = :black, strokewidth = 1)
 myplot(Scatter, layout[1:2, 2], rand(100), rand(100), markersize = 20px, color = :blue, strokecolor = :black, strokewidth = 1)
 myplot(MarginDensityScatter, layout[1:2, 3:4], randn(100), randn(100))
 scene
-
+##
 
 struct FacetPlot end
 
@@ -255,6 +258,8 @@ function myplot(::Type{FacetPlot}, infra::Infrastructure, plottype, rows, cols, 
     autolimits!(infra.axes[1])
 end
 
+
+##
 
 msize = Node(10px)
 scene, layout, infra = myplot(FacetPlot, Scatter,
