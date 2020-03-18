beb74b280d5b03bb36e2c989ec2aaa984600e35b
SimonDanisch <sdanisch@protonmail.com>
2020-03-18 22:02:18 +0100

    add minimal tests
    Former-commit-id: 808fef7544c2a3f172b886e21c511d42c51db08c

--- test/simple.jl
+++ test/simple.jl
@@ -1,21 +1,32 @@
 using AbstractPlotting
 using GLMakie
 using GeometryBasics
+using FFTW
+using LinearAlgebra
+using Random
+using Interpolations
 
-GLMakie.GLFW.WindowHint(0x0002000C, 0)
-1 + 1
 scatter(1:4)
+
 lines(1:4)
+
 linesegments(1:4)
+
 surface(0..1, 0..1, rand(9, 11))
+
 meshscatter(rand(Point3f0, 3)) |> display
+
 mesh(Sphere(Point3f0(0), 1f0))
+
 poly(decompose(Point2f0, Circle(Point2f0(0), 1f0)))
+
 image(rand(10, 10))
+
 heatmap(rand(10, 10))
+
 mesh(m, color = :green)
 
-volume(rand(4, 4, 4), algorithm=:iso)
+volume(rand(4, 4, 4), isovalue=0.5, isorange=0.01, algorithm=:iso)
 volume(rand(4, 4, 4), algorithm=:mip)
 volume(rand(4, 4, 4), algorithm=:absorption)
-volume(rand(RGBAf0, 4, 4, 4), algorithm=:absorptionrgba) |> display
+volume(rand(RGBAf0, 4, 4, 4), algorithm=:absorptionrgba)
