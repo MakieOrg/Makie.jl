f718a77447273504815dee104cdfd1a696bcf538
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-03-24 08:59:05 +0100

    hack for LText left alignment at no rotation
    fix

--- lobjects/ltext.jl
+++ lobjects/ltext.jl
@@ -23,27 +23,43 @@ function LText(parent::Scene; bbox = nothing, kwargs...)
 
     textpos = Node(Point3f0(0, 0, 0))
 
-    t = text!(parent, text, position = textpos, textsize = textsize, font = font, color = color,
-        visible = visible, align = (:center, :center), rotation = rotation, raw = true)[end]
+    # this is just a hack until boundingboxes in abstractplotting are perfect
+    alignnode = lift(halign, rotation) do h, rot
+        # left align the text if it's not rotated and left aligned
+        if rot == 0 && (h == :left || h == 0.0)
+            (:left, :center)
+        else
+            (:center, :center)
+        end
+    end
 
-    textbb = BBox(0, 1, 0, 1)
+    t = text!(parent, text, position = textpos, textsize = textsize, font = font, color = color,
+        visible = visible, align = alignnode, rotation = rotation, raw = true)[end]
+
+    textbb = Ref(BBox(0, 1, 0, 1))
 
     onany(text, textsize, font, rotation, padding) do text, textsize, font, rotation, padding
-        textbb = FRect2D(boundingbox(t))
-        autowidth = width(textbb) + padding[1] + padding[2]
-        autoheight = height(textbb) + padding[3] + padding[4]
+        textbb[] = FRect2D(boundingbox(t))
+        autowidth = width(textbb[]) + padding[1] + padding[2]
+        autoheight = height(textbb[]) + padding[3] + padding[4]
         autosizenode[] = (autowidth, autoheight)
     end
 
     onany(finalbbox, padding) do bbox, padding
 
-        tw = width(textbb)
-        th = height(textbb)
+        tw = width(textbb[])
+        th = height(textbb[])
 
         box = bbox.origin[1]
         boy = bbox.origin[2]
 
-        tx = box + padding[1] + 0.5 * tw
+        # this is also part of the hack to improve left alignment until
+        # boundingboxes are perfect
+        tx = if rotation[] == 0 && (halign[] == :left || halign[] == 0.0)
+            box + padding[1]
+        else
+            box + padding[1] + 0.5 * tw
+        end
         ty = boy + padding[3] + 0.5 * th
 
         textpos[] = Point3f0(tx, ty, 0)
