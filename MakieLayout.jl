a726756c5581e920ac49a9bb37bd22c745f145df
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-03-27 18:46:42 +0100

    reexport some gridlayoutbase names

--- MakieLayout.jl
+++ MakieLayout.jl
@@ -50,6 +50,27 @@ export tight_xticklabel_spacing!, tight_yticklabel_spacing!, tight_ticklabel_spa
 export layoutscene
 export set_close_to!
 
+
+# from GridLayoutBase
+export GridLayout, GridPosition
+export GridLayoutSpec
+export BBox
+export LayoutObservables
+export Inside, Outside, Mixed
+export Fixed, Auto, Relative, Aspect
+export width, height, top, bottom, left, right
+export with_updates_suspended
+export appendcols!, appendrows!, prependcols!, prependrows!, deletecol!, deleterow!, trim!
+export gridnest!
+export AxisAspect, DataAspect
+export colsize!, rowsize!, colgap!, rowgap!
+export Left, Right, Top, Bottom, TopLeft, BottomLeft, TopRight, BottomRight
+export grid!, hbox!, vbox!
+export swap!
+export ncols, nrows
+
+
+
 const FPS = Node(30)
 const COLOR_ACCENT = Ref(RGBf0(((79, 122, 214) ./ 255)...))
 const COLOR_ACCENT_DIMMED = Ref(RGBf0(((174, 192, 230) ./ 255)...))
