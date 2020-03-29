57ebac2fe3366ef168d1fe8cb63b5f947940430d
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-03-29 19:42:48 +0200

    fix set_ft_font
    but using this function for real somehow results in crashes, small tests were fine

--- src/CairoMakie.jl
+++ src/CairoMakie.jl
@@ -503,10 +503,20 @@ function set_ft_font(cr, font)
     font_face = ccall(
         (:cairo_ft_font_face_create_for_ft_face, LIB_CAIRO),
         Ptr{Cvoid}, (Ptr{Cvoid}, Cint),
-        font, 0
+        getfield(font, :ft_ptr), 0
     )
     ccall((:cairo_set_font_face, LIB_CAIRO), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), cr.ptr, font_face)
+    font_face
 end
+
+function cairo_font_face_destroy(font_face)
+    ccall(
+        (:cairo_ft_font_face_create_for_ft_face, LIB_CAIRO),
+        Cvoid, (Ptr{Cvoid},),
+        font_face
+    )
+end
+
 fontname(x::String) = x
 fontname(x::Symbol) = string(x)
 function fontname(x::NativeFont)
