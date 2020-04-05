97cbd1d967032ea8e73abc99396d9094f64bd55c
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-05 18:46:20 +0530

    add nicer tomakie syntax

--- src/MakieRecipes.jl
+++ src/MakieRecipes.jl
@@ -6,4 +6,11 @@ using RecipesBase: @recipe
 include("pipeline_integration.jl")
 include("attribute_table.jl")
 
+function tomakie!(sc::Scene, args...; attrs...)
+    RecipePipeline.recipe_pipeline!(sc, Dict{Symbol, Any}(; attrs...), args)
 end # module
+
+tomakie!(args...; attrs...) = tomakie!(AbstractPlotting.current_scene(), args...; attrs...)
+
+
+tomakie(args...; attrs...) = tomakie!(Scene(), args...; attrs...)
