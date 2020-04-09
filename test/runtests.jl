49915fec3142d2f41bba3e626d306b1a2312a269
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-09 17:56:30 +0530

    upload examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,12 @@ using MakieRecipes
 using Literate, AbstractPlotting, CairoMakie
 using Test
 
+cd(@__DIR__)
+
 @testset "Examples" begin
     literatedir = joinpath(@__DIR__, "..", "docs", "src", "literate")
-    @test_nowarn include(joinpath(literatedir, "examples.jl")) # execute the source file directly
+    ispath("test_examples") || mkpath("test_examples")
+    cd("test_examples") do
+        @test_nowarn include(joinpath(literatedir, "examples.jl")) # execute the source file directly
+    end
 end
