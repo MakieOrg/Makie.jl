a9385251de7cdf1f01f2424e3ddca7a836487530
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-14 09:10:59 +0530

    Update test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,6 @@ examples = MakieGallery.record_examples(test_record_path)
 MakieGallery.run_comparison(test_record_path, tested_diff_path)
 
 empty!(database) # remove other examples
-include("glmakie_tests.jl") # include GLMakie specific tests
 # THese examples download additional data - don't want to deal with that!
 for path in (tested_diff_path, test_record_path)
     rm(path, force = true, recursive = true)
