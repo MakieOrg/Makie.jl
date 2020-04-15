a631867d189aaa438641ca60da3ea3f814922fd0
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-04-15 13:25:08 +0200

    fix incorrect projection (off by a pixel, seen in heatmap)
    this caused an annoying white gap between an LAxis edge and a heatmap with tight limits

--- src/CairoMakie.jl
+++ src/CairoMakie.jl
@@ -96,9 +96,14 @@ function project_position(scene, point, model)
     res = scene.camera.resolution[]
     p4d = to_ndim(Vec4f0, to_ndim(Vec3f0, point, 0f0), 1f0)
     clip = scene.camera.projectionview[] * model * p4d
+    # between -1 and 1
     p = (clip ./ clip[4])[Vec(1, 2)]
-    p = Vec2f0(p[1], -p[2])
-    ((((p .+ 1f0) / 2f0) .* (res .- 1f0)) .+ 1f0)
+    # flip y to match cairo
+    p_yflip = Vec2f0(p[1], -p[2])
+    # normalize to between 0 and 1
+    p_0_to_1 = (p_yflip .+ 1f0) / 2f0
+    # multiply with scene resolution for final position
+    result = p_0_to_1 .* res
 end
 
 project_scale(scene::Scene, s::Number, model = Mat4f0(I)) = project_scale(scene, Vec2f0(s), model)
