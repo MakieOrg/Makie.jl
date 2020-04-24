9a1a1a59e5672365032788d0bf25fb492bde910c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-04-24 09:40:46 +0200

    improve menu

--- lobjects/lmenu.jl
+++ lobjects/lmenu.jl
@@ -8,9 +8,9 @@ end
 function default_attributes(::Type{LMenu}, scene)
     attrs, docdict, defaultdict = @documented_attributes begin
         "The height setting of the menu."
-        height = Auto(false)
+        height = Auto(true)
         "The width setting of the menu."
-        width = 200
+        width = nothing
         "The horizontal alignment of the menu in its suggested bounding box."
         halign = :center
         "The vertical alignment of the menu in its suggested bounding box."
@@ -19,19 +19,23 @@ function default_attributes(::Type{LMenu}, scene)
         alignmode = Inside()
         "Index of selected item"
         i_selected = 1
+        "Selected item value"
+        selection = nothing
         "Is the menu showing the available options"
         is_open = false
         "Cell color when hovered"
         cell_color_hover = COLOR_ACCENT_DIMMED[]
+        "Cell color when active"
+        cell_color_active = COLOR_ACCENT[]
         "Cell color when inactive even"
-        cell_color_inactive_even = RGBf0(0.9, 0.9, 0.9)
+        cell_color_inactive_even = RGBf0(0.95, 0.95, 0.95)
         "Cell color when inactive odd"
-        cell_color_inactive_odd = RGBf0(0.9, 0.9, 0.9)
+        cell_color_inactive_odd = RGBf0(0.95, 0.95, 0.95)
         "Color of the dropdown arrow"
         dropdown_arrow_color = (:black, 0.3)
         "Size of the dropdown arrow"
         dropdown_arrow_size = 12px
-        "Options"
+        "The list of options selectable in the menu. This can be any iterable of a mixture of strings and containers with one string and one other value. If an entry is just a string, that string is both label and selection. If an entry is a container with one string and one other value, the string is the label and the other value is the selection."
         options = ["no options"]
         "Font size of the cell texts"
         textsize = 20
@@ -40,6 +44,7 @@ function default_attributes(::Type{LMenu}, scene)
 end
 
 @doc """
+LMenu
 LMenu has the following attributes:
 
 $(let
@@ -58,7 +63,7 @@ function LMenu(parent::Scene; bbox = nothing, kwargs...)
 
     @extract attrs (halign, valign, i_selected, is_open, cell_color_hover,
         cell_color_inactive_even, cell_color_inactive_odd, dropdown_arrow_color,
-        options, dropdown_arrow_size, textsize)
+        options, dropdown_arrow_size, textsize, selection, cell_color_active)
 
     decorations = Dict{Symbol, Any}()
 
@@ -76,15 +81,6 @@ function LMenu(parent::Scene; bbox = nothing, kwargs...)
 
     scene = Scene(parent, scenearea, raw = true, camera = campixel!)
 
-    dropdown_arrow = scatter!(scene,
-        lift(x -> [Point2f0(width(x) - 20, height(x) / 2)], scenearea),
-        marker = '▼',
-        markersize = dropdown_arrow_size,
-        visible = @lift(!$is_open),
-        color = dropdown_arrow_color,
-        raw = true)[end]
-    translate!(dropdown_arrow, 0, 0, 1)
-
     contentgrid = GridLayout(
         bbox = lift(x -> FRect2D(AbstractPlotting.zero_origin(x)), scenearea),
         valign = :top)
@@ -100,15 +96,16 @@ function LMenu(parent::Scene; bbox = nothing, kwargs...)
     rects = [LRect(scene, width = nothing, height = nothing,
         color = iseven(i) ? cell_color_inactive_even[] : cell_color_inactive_odd[], strokewidth = 0) for i in 1:length(options[])]
 
-    # strings = ["Bananas", "Apples", "Oranges", "Kiwi", "Grapes"]
+    strings = optionlabel.(options[])
+
     texts = [LText(scene, s, halign = :left, width = Auto(false),
         textsize = textsize,
-        padding = (10, 10, 10, 10)) for s in options[]]
+        padding = (10, 10, 10, 10)) for s in strings]
 
     contentgrid[:v] = rects
     contentgrid[:v] = texts
 
-    on(i_selected) do i
+    onany(i_selected, textsize) do i, args...
         h = texts[i].layoutobservables.autosize[][2]
         layoutobservables.autosize[] = (nothing, h)
     end
@@ -116,11 +113,17 @@ function LMenu(parent::Scene; bbox = nothing, kwargs...)
     i_selected[] = i_selected[]
     is_open[] = is_open[]
 
+    on(i_selected) do i
+        # collect in case options is a zip or other generator without indexing
+        option = collect(options[])[i]
+        selection[] = optionvalue(option)
+    end
+
     rowgap!(contentgrid, 0)
 
-    mousestates = [addmousestate!(scene, r.rect) for r in rects]
+    mousestates = [addmousestate!(scene, r.rect, t.textobject) for (r, t) in zip(rects, texts)]
 
-    for (i, (mousestate, r)) in enumerate(zip(mousestates, rects))
+    for (i, (mousestate, r, t)) in enumerate(zip(mousestates, rects, texts))
         onmouseleftclick(mousestate) do state
             if is_open[]
                 i_selected[] = i
@@ -143,10 +146,39 @@ function LMenu(parent::Scene; bbox = nothing, kwargs...)
         onmouseout(mousestate) do state
             r.color = iseven(i) ? cell_color_inactive_even[] : cell_color_inactive_odd[]
         end
+        onmouseleftdown(mousestate) do state
+            r.color = cell_color_active[]
+        end
     end
+
+    dropdown_arrow = scatter!(scene,
+        lift(x -> [Point2f0(width(x) - 20, height(x) / 2)], scenearea),
+        marker = '▼',
+        markersize = dropdown_arrow_size,
+        visible = @lift(!$is_open),
+        color = dropdown_arrow_color,
+        raw = true)[end]
+    translate!(dropdown_arrow, 0, 0, 1)
 
     # trigger bbox
     layoutobservables.suggestedbbox[] = layoutobservables.suggestedbbox[]
 
     LMenu(scene, attrs, layoutobservables, decorations)
+end
+
+
+function optionlabel(option::AbstractString)
+    string(option)
+end
+
+function optionlabel(option)
+    string(option[1])
+end
+
+function optionvalue(option::AbstractString)
+    option
+end
+
+function optionvalue(option)
+    option[2]
 end
