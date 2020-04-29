eaabf7d1d6085f23d834d088b2b24a707081c05a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-29 17:56:54 +0530

    Factor fonts out

--- src/CairoMakie.jl
+++ src/CairoMakie.jl
@@ -18,6 +18,7 @@ end
 
 include("infrastructure.jl")
 include("utils.jl")
+include("fonts.jl")
 
 function draw_atomic(scene::Scene, screen::CairoScreen, primitive::AbstractPlotting.Mesh)
     @get_attribute(primitive, (color,))
@@ -315,36 +316,6 @@ function draw_atomic(scene::Scene, screen::CairoScreen, primitive::Scatter)
         end
     end
     nothing
-end
-
-scale_matrix(x, y) = Cairo.CairoMatrix(x, 0.0, 0.0, y, 0.0, 0.0)
-
-function set_font_matrix(cr, matrix)
-    ccall((:cairo_set_font_matrix, LIB_CAIRO), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), cr.ptr, Ref(matrix))
-end
-
-function set_ft_font(cr, font)
-    font_face = ccall(
-        (:cairo_ft_font_face_create_for_ft_face, LIB_CAIRO),
-        Ptr{Cvoid}, (AbstractPlotting.FreeTypeAbstraction.FT_Face, Cint),
-        font, 0
-    )
-    ccall((:cairo_set_font_face, LIB_CAIRO), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), cr.ptr, font_face)
-    font_face
-end
-
-function cairo_font_face_destroy(font_face)
-    ccall(
-        (:cairo_font_face_destroy, LIB_CAIRO),
-        Cvoid, (Ptr{Cvoid},),
-        font_face
-    )
-end
-
-fontname(x::String) = x
-fontname(x::Symbol) = string(x)
-function fontname(x::NativeFont)
-    return x.family_name
 end
 
 function draw_atomic(scene::Scene, screen::CairoScreen, primitive::Text)
---
+++ src/fonts.jl
@@ -0,0 +1,40 @@
+function set_font_matrix(cr, matrix)
+    ccall((:cairo_set_font_matrix, LIB_CAIRO), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), cr.ptr, Ref(matrix))
+end
+
+function set_ft_font(cr, font)
+    font_face = ccall(
+        (:cairo_ft_font_face_create_for_ft_face, LIB_CAIRO),
+        Ptr{Cvoid}, (AbstractPlotting.FreeTypeAbstraction.FT_Face, Cint),
+        font, 0
+    )
+    ccall((:cairo_set_font_face, LIB_CAIRO), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), cr.ptr, font_face)
+    font_face
+end
+
+function cairo_font_face_destroy(font_face)
+    ccall(
+        (:cairo_font_face_destroy, LIB_CAIRO),
+        Cvoid, (Ptr{Cvoid},),
+        font_face
+    )
+end
+
+scale_matrix(x, y) = Cairo.CairoMatrix(x, 0.0, 0.0, y, 0.0, 0.0)
+
+
+"""
+Finds a font that can represent the unicode character!
+Returns AbstractPlotting.defaultfont() if not representable!
+"""
+function best_font(c::Char, font = AbstractPlotting.defaultfont())
+    if AbstractPlotting.FreeType.FT_Get_Char_Index(font, c) == 0
+        for afont in AbstractPlotting.alternativefonts()
+            if AbstractPlotting.FreeType.FT_Get_Char_Index(afont, c) != 0
+                return afont
+            end
+        end
+        return AbstractPlotting.defaultfont()
+    end
+    return font
+end
