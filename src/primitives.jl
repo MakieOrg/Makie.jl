d64e06063f2dba65fd5d290d18946abb038a0754
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-29 18:08:36 +0530

    Factor poly out to primitives

--- src/primitives.jl
+++ src/primitives.jl
@@ -344,3 +344,78 @@ function draw_atomic(scene::Scene, screen::CairoScreen, primitive::AbstractPlott
     Cairo.paint(ctx)
     return nothing
 end
+
+################################################################################
+#                    Poly - the not so primitive, primitive                    #
+################################################################################
+
+
+"""
+Special method for polys so we don't fall back to atomic meshes, which are much more
+complex and slower to draw than standard paths with single color.
+"""
+function draw_plot(scene::Scene, screen::CairoScreen, poly::Poly)
+    # dispatch on input arguments to poly to use smarter drawing methods than
+    # meshes if possible
+    draw_poly(scene, screen, poly, to_value.(poly.input_args)...)
+end
+
+"""
+Fallback method for args without special treatment.
+"""
+function draw_poly(scene::Scene, screen::CairoScreen, poly, args...)
+    draw_poly_as_mesh(scene, screen, poly)
+end
+
+function draw_poly_as_mesh(scene, screen, poly)
+    draw_plot(scene, screen, poly.plots[1])
+    draw_plot(scene, screen, poly.plots[2])
+end
+
+function draw_poly(scene::Scene, screen::CairoScreen, poly, points::Vector{<:Point2})
+
+    # in the rare case of per-vertex colors redirect to mesh drawing
+    if poly.color[] isa Array
+        draw_poly_as_mesh(scene, screen, poly)
+        return
+    end
+
+    model = poly.model[]
+    points = project_position.(Ref(scene), points, Ref(model))
+    Cairo.move_to(screen.context, points[1]...)
+    for p in points[2:end]
+        Cairo.line_to(screen.context, p...)
+    end
+    Cairo.close_path(screen.context)
+    Cairo.set_source_rgba(screen.context, rgbatuple(to_color(poly.color[]))...)
+    Cairo.fill_preserve(screen.context)
+    Cairo.set_source_rgba(screen.context, rgbatuple(to_color(poly.strokecolor[]))...)
+    Cairo.set_line_width(screen.context, poly.strokewidth[])
+    Cairo.stroke(screen.context)
+end
+
+
+draw_poly(scene::Scene, screen::CairoScreen, poly, rect::Rect2D) = draw_poly(scene, screen, poly, [rect])
+
+function draw_poly(scene::Scene, screen::CairoScreen, poly, rects::Vector{<:Rect2D})
+    model = poly.model[]
+    projected_rects = project_rect.(Ref(scene), rects, Ref(model))
+
+    color = poly.color[]
+    if color isa AbstractArray{<:Number}
+        color = numbers_to_colors(color, poly)
+    end
+    strokecolor = poly.strokecolor[]
+    if strokecolor isa AbstractArray{<:Number}
+        strokecolor = numbers_to_colors(strokecolor, poly)
+    end
+
+    broadcast_foreach(projected_rects, color, strokecolor, poly.strokewidth[]) do r, c, sc, sw
+        Cairo.rectangle(screen.context, origin(r)..., widths(r)...)
+        Cairo.set_source_rgba(screen.context, rgbatuple(to_color(c))...)
+        Cairo.fill_preserve(screen.context)
+        Cairo.set_source_rgba(screen.context, rgbatuple(to_color(sc))...)
+        Cairo.set_line_width(screen.context, sw)
+        Cairo.stroke(screen.context)
+    end
+end
