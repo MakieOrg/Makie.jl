4c68dc4a684e87f5cdd90a01a16922c68ee19492
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2020-06-11 17:02:08 +0200

    rename nodes to observables

--- test/liftmacro.jl
+++ test/liftmacro.jl
@@ -1,6 +1,6 @@
 @testset "lift macro" begin
-    x = Node(1.0)
-    y = Node(2.0)
+    x = Observable(1.0)
+    y = Observable(2.0)
     z = (x = x, y = y)
     
     t1 = @lift($x + $y)
@@ -8,18 +8,18 @@
     t2 = @lift($(z.x) - $(z.y))
     @test t2[] == -1.0
 
-    f = Node(sin)
+    f = Observable(sin)
 
     t3 = @lift($f($x))
     @test t3[] == sin(x[])
     t4 = @lift($f($f($(z.x))))
     @test t4[] == sin(sin(z.x[]))
 
-    arr = Node([1, 2, 3])
-    t5 = @lift($arr[2])
+    arrobs = Observable([1, 2, 3])
+    t5 = @lift($arrobs[2])
     @test t5[] == 2
 
-    nodes = [Node(1.0), Node(2.0)]
-    t6 = @lift($(nodes[1]) + $(nodes[2]))
+    observables = [Observable(1.0), Observable(2.0)]
+    t6 = @lift($(observables[1]) + $(observables[2]))
     @test t6[] == 3.0
 end
