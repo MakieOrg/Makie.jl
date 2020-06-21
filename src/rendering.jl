7fc76d688cc2b7f7df6b1c25f80107d4716118cf
Bart Janssens <bart@bartjanssens.org>
2020-06-21 14:48:38 +0200

    Dispatch PollEvents on screen type

--- src/rendering.jl
+++ src/rendering.jl
@@ -3,7 +3,7 @@ function renderloop(screen::Screen; framerate = 1/30, prerender = () -> nothing)
     try
         while isopen(screen)
             t = time()
-            GLFW.PollEvents() # GLFW poll
+            pollevents(screen) # GLFW poll
             screen.render_tick[] = nothing
             prerender()
             make_context_current(screen)
