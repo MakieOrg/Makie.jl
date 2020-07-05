6a080e1bd166e7bc3468e20795ba0801a2fe7496
F Freyer <frederic481994@hotmail.de>
2020-07-05 02:30:23 +0200

    update buffer length missmatch error

--- src/GLAbstraction/GLTypes.jl
+++ src/GLAbstraction/GLTypes.jl
@@ -203,10 +203,21 @@ function GLVertexArray(bufferdict::Dict, program::GLProgram)
             attribute = string(name)
             len == -1 && (len = length(buffer))
             # TODO: use glVertexAttribDivisor to allow multiples of the longest buffer
-            len != length(buffer) && error(
-              "buffer $attribute has not the same length as the other buffers.
-              Has: $(length(buffer)). Should have: $len"
-            )
+            if len != length(buffer)
+                # We don't know which buffer has the wrong size, so list all of them
+                bufferlengths = ""
+                for (name, buffer) in bufferdict
+                    if isa(buffer, GLBuffer) && buffer.buffertype == GL_ELEMENT_ARRAY_BUFFER
+                    elseif Symbol(name) == :indices
+                    else
+                        bufferlengths *= "\n\t$name has length $(length(buffer))"
+                    end
+                end
+                error(
+                    "Buffer $attribute does not have the same length as the other buffers." *
+                    bufferlengths
+                )
+            end
             bind(buffer)
             attribLocation = get_attribute_location(program.id, attribute)
             (attribLocation == -1) && continue
