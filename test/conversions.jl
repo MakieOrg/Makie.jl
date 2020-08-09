ef3202e64b194b566ced3014e3b985dac2ab1066
ARSH SHARMA <184517@nith.ac.in>
2020-08-10 01:38:46 +0530

    Add tests

--- test/conversions.jl
+++ test/conversions.jl
@@ -53,6 +53,31 @@ end
     p2 = convert_arguments(AbstractPlotting.PointBased(), mls)
     @test p2[1][1:4] == pts
     @test p2[1][6:9] == pts1
+
+    pol_e = Polygon(ls)
+    p3_e = convert_arguments(AbstractPlotting.PointBased(), pol_e)
+    @test p3_e[1] == pts
+
+    pol = Polygon(ls, [ls1])
+    p3 = convert_arguments(AbstractPlotting.PointBased(), pol)
+    @test p3[1][1:4] == pts
+    @test p3[1][6:9] == pts1
+
+    pts2 = Point{2, Int}[(5, 1), (3, 3), (4, 8), (1, 2), (5, 1)]
+    pts3 = Point{2, Int}[(2, 2), (2, 3),(3, 4), (2, 2)]
+    pts4 = Point{2, Int}[(2, 2), (3, 8),(5, 6), (3, 4), (2, 2)]
+    ls2 = LineString(pts2)
+    ls3 = LineString(pts3)
+    ls4 = LineString(pts4)
+    pol1 = Polygon(ls2, [ls3, ls4])
+    apol = [pol, pol1]
+    p4 = convert_arguments(AbstractPlotting.PointBased(), apol)
+    mpol = MultiPolygon([pol, pol1])
+    @test p4[1][1:4] == pts
+    @test p4[1][6:9] == pts1
+    @test p4[1][11:15] == pts2 
+    @test p4[1][17:20] == pts3
+    @test p4[1][22:26] == pts4
 end
 
 @testset "Categorical values" begin
