dd978e8ff05aa667c5acbfb65a00185b28ced911
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-10-18 14:44:59 +0200

    move recipes into separate files

---
+++ src/basic_recipes/convenience_functions.jl
@@ -0,0 +1,77 @@
+function AbstractPlotting.plot!(plot::Plot(AbstractVector{<: Complex}))
+    plot[:axis, :labels] = ("Re(x)", "Im(x)")
+    lines!(plot, lift(im-> Point2f0.(real.(im), imag.(im)), x[1]))
+end
+
+
+"""
+    showlibrary(lib::Symbol)::Scene
+
+Shows all colour gradients in the given library.
+Returns a Scene with these colour gradients arranged
+as horizontal colourbars.
+"""
+function showlibrary(lib::Symbol)::Scene
+
+    cgrads = sort(PlotUtils.cgradients(lib))
+
+    PlotUtils.clibrary(lib)
+
+    showgradients(cgrads)
+
+end
+
+
+"""
+    showgradients(
+        cgrads::AbstractVector{Symbol};
+        h = 0.0, offset = 0.2, textsize = 0.7,
+        resolution = (800, length(cgrads) * 84)
+    )::Scene
+
+Plots the given colour gradients arranged as horizontal colourbars.
+If you change the offsets or the font size, you may need to change the resolution.
+"""
+function showgradients(
+        cgrads::AbstractVector{Symbol};
+        h = 0.0,
+        offset = 0.4,
+        textsize = 0.7,
+        resolution = (800, length(cgrads) * 84),
+        monospace = true
+    )::Scene
+
+    scene = Scene(resolution = resolution)
+
+    map(collect(cgrads)) do cmap
+
+         c = to_colormap(cmap)
+
+         cbar = image!(
+             scene,
+             range(0, stop = 10, length = length(c)),
+             range(0, stop = 1, length = length(c)),
+             reshape(c, (length(c),1)),
+             show_axis = false
+         )[end]
+
+         cmapstr = monospace ? UnicodeFun.to_latex("\\mono{$cmap}") : string(cmap, ":")
+
+         text!(
+             scene,
+             cmapstr,
+             position = Point2f0(-0.1, 0.5 + h),
+             align = (:right, :center),
+             show_axis = false,
+             textsize = textsize
+         )
+
+         translate!(cbar, 0, h, 0)
+
+         h -= (1 + offset)
+
+    end
+
+    scene
+
+end
---
+++ src/basic_recipes/spy.jl
@@ -0,0 +1,81 @@
+
+"""
+spy(x::Range, y::Range, z::AbstractSparseArray)
+
+Visualizes big sparse matrices.
+Usage:
+```julia
+N = 200_000
+x = sprand(Float64, N, N, (3(10^6)) / (N*N));
+spy(x)
+# or if you want to specify the range of x and y:
+spy(0..1, 0..1, x)
+```
+## Attributes
+$(ATTRIBUTES)
+"""
+@recipe(Spy, x, y, z) do scene
+    Attributes(
+        marker = automatic,
+        markersize = automatic,
+        colormap = theme(scene, :colormap),
+        colorrange = automatic,
+        framecolor = :black,
+        framesize = 1,
+    )
+end
+
+function convert_arguments(::Type{<: Spy}, x::SparseArrays.AbstractSparseArray)
+    (0..size(x, 1), 0..size(x, 2), x)
+end
+function convert_arguments(::Type{<: Spy}, x, y, z::SparseArrays.AbstractSparseArray)
+    (x, y, z)
+end
+
+function calculated_attributes!(::Type{<: Spy}, plot)
+end
+
+function plot!(p::Spy)
+    rect = lift(p.x, p.y) do x, y
+        xe = extrema(x)
+        ye = extrema(y)
+        FRect2D((xe[1], ye[1]), (xe[2] - xe[1], ye[2] - ye[1]))
+    end
+    # TODO FastPixel isn't accepting marker size in data coordinates
+    # but instead in pixel - so we need to fix that in GLMakie for consistency
+    # and make this nicer when redoing unit support
+    markersize = lift(p.markersize, rect, p.z) do msize, rect, z
+        if msize === automatic
+            widths(rect) ./ Vec2f0(size(z))
+        else
+            msize
+        end
+    end
+    # TODO correctly align marker
+    xycol = lift(rect, p.z, markersize) do rect, z, markersize
+        x, y, color = SparseArrays.findnz(z)
+        points = map(x, y) do x, y
+            (((Point2f0(x, y) .- 1) ./ Point2f0(size(z) .- 1)) .*
+            widths(rect) .+ minimum(rect))
+        end
+        points, convert(Vector{Float32}, color)
+    end
+
+    replace_automatic!(p, :colorrange) do
+        lift(xycol) do (xy, col)
+            extrema_nan(col)
+        end
+    end
+
+    marker = lift(p.marker) do x
+        return x === automatic ? FastPixel() : x
+    end
+
+    scatter!(
+        p,
+        lift(first, xycol), color = lift(last, xycol),
+        marker = marker, markersize = markersize, colorrange = p.colorrange
+    )
+
+    lines!(p, rect, color = p.framecolor, linewidth = p.framesize)
+end
