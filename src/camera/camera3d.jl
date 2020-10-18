27caa4e42d09998e117ce40b190550516984225c
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-18 15:56:45 -0400

    Clearer docstring for `rotate_cam!`

--- src/camera/camera3d.jl
+++ src/camera/camera3d.jl
@@ -252,7 +252,8 @@ end
     rotate_cam!(scene::Scene, theta_v::Number...)
     rotate_cam!(scene::Scene, theta_v::VecTypes)
 
-Rotate the camera of the Scene by the given rotation.
+Rotate the camera of the Scene by the given rotation. Passing `theta_v = (α, β, γ)` will rotate
+the camera according to the Euler angles (α, β, γ).
 """
 rotate_cam!(scene::Scene, theta_v::Number...) = rotate_cam!(scene, cameracontrols(scene), theta_v)
 rotate_cam!(scene::Scene, theta_v::VecTypes) = rotate_cam!(scene, cameracontrols(scene), theta_v)
