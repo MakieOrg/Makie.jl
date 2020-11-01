a2f9667aa855d15356120c035a5dd7ff297b7e64
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-11-01 09:18:41 +0100

    export interaction stuff

--- src/makielayout/MakieLayout.jl
+++ src/makielayout/MakieLayout.jl
@@ -87,6 +87,7 @@ export set_close_to!
 export xaxis_bottom!, xaxis_top!, yaxis_left!, yaxis_right!
 export labelslider!
 export addmouseevents!
+export interactions, register_interaction!, deregister_interaction!, activate_interaction!, deactivate_interaction!
 export hlines!, vlines!
 
 
