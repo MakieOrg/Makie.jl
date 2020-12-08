ccec1bfdc0e010347bc22fa3c766f41d652a9eac
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-12-08 21:55:06 +0100

    misc
    Former-commit-id: b9c623494060e485b892328e7437fd85624dfd7d

--- scratch/figuretest.jl
+++ scratch/figuretest.jl
@@ -3,29 +3,31 @@ CairoMakie.activate!(type = "png")
 
 ##
 
-fig, ax, bars = errorbars(1:10, 1:10, 1:10, color = 1:10, colormap = :viridis)
+fig, ax, bars = errorbars(1:10, 1:10, 1:10, color = 1:10, colormap = :viridis,
+    figure = (resolution = (800, 600), font = "Helvetica Light"))
+
 ax.backgroundcolor = :gray95
 
-scatter!(ax, 1:10, 1:10)
+ax, scat = scatter(fig[1, 2], 1:10, 1:10, axis = (title = "hello", xlabel = "green"))
 
-ax, scat = scatter(fig[1, 2], 1:10, 1:10)
-scatter(fig[2, :], 1:10, 1:10)
+lines(fig[2, :], cumsum(randn(1000000)))
+lines!(fig[2, :], cumsum(randn(1000000)), color = "green")
+lines!(fig[2, :], cumsum(randn(1000000)), color = "red")
 
-heatmap(fig[:, 3], randn(10, 10))
-lines!(fig[1, 1], randn(100))
-lines!(fig[1, 1], randn(100))
-
-
-# fig[0, :] = LText(fig.scene, "hello")
+heatmap(fig[:, 3], randn(10, 20), axis = (;title = "heatmap"))
 
 fig
 
 ##
 
-
 fig, _ = scatter(randn(100))
 
 lines(fig[1, 2], randn(100))
+lines!(fig[1, 2], randn(100) .+ 1, color = :red)
 lines(fig[2, :], randn(100))
 
 fig
+
+##
+
+fig = Figure()
