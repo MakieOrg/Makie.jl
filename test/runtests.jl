5256681e77074f18f2f37584fc8c0d1c482e4955
SimonDanisch <sdanisch@gmail.com>
2020-12-08 19:36:16 +0100

    fix nightly

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using MeshIO
 using StaticArrays
 using AbstractPlotting
+using ImageMagick
 
 using AbstractPlotting.Observables
 using AbstractPlotting.GeometryBasics
@@ -10,6 +11,11 @@ using AbstractPlotting.PlotUtils
 using AbstractPlotting.FileIO
 using AbstractPlotting.IntervalSets
 using GeometryBasics: Pyramid
+
+# ImageIO seems broken on 1.6 ... and there doesn't
+# seem to be a clean way anymore to force not to use a loader library?
+filter!(x-> x !== :ImageIO, FileIO.sym2saver[:PNG])
+filter!(x-> x !== :ImageIO, FileIO.sym2loader[:PNG])
 
 include("reference_tests.jl")
 include("unit_tests/runtests.jl")
