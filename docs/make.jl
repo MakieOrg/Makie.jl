5009cf9bbee69eec23f5a6610ce66807890fcbe2
SimonDanisch <sdanisch@gmail.com>
2021-01-21 03:32:14 +0100

    push preview

--- docs/make.jl
+++ docs/make.jl
@@ -4,4 +4,4 @@ makedocs(; modules=[WGLMakie], format=Documenter.HTML(), pages=["Home" => "index
          repo="https://github.com/JuliaPlots/WGLMakie.jl/blob/{commit}{path}#L{line}",
          sitename="WGLMakie.jl", authors="Simon Danisch")
 
-deploydocs(; repo="github.com/JuliaPlots/WGLMakie.jl")
+deploydocs(; repo="github.com/JuliaPlots/WGLMakie.jl", push_preview=true)
