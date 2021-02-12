9774b43802b8e7d7ddb21bf812806a4b40cde68b
Simon Byrne <simonbyrne@gmail.com>
2021-02-12 08:48:57 -0800

    more to_vertices fixes
    Former-commit-id: 2004f8af897bd029629c1ca9aaf6c939395486b2

--- test/unit_tests/conversions.jl
+++ test/unit_tests/conversions.jl
@@ -35,7 +35,7 @@ end
     X4 = rand(2,10)
     V4 = to_vertices(X4)
     @test Float32(X4[1,7]) == V4[7][1]
-    @test V4[3,7] == 0
+    @test V4[7][3] == 0
 
     X5 = rand(3,10)
     V5 = to_vertices(X5)
@@ -44,6 +44,7 @@ end
     X6 = rand(10,2)
     V6 = to_vertices(X6)
     @test Float32(X6[7,1]) == V6[7][1]
+    @test V6[7][3] == 0
 
     X7 = rand(10,3)
     V7 = to_vertices(X7)
