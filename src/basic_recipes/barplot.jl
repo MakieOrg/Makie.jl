76b8059ada26e8a7a92bcbe5037b1b249aa9af82
Fabian Greimel <fabgrei@gmail.com>
2021-02-24 17:44:05 +0100

    with DataFrames
    qualify names from DataFrames
    
    
    remove @show
    
    
    edge cases
    
    
    1
    
    
    5

--- src/basic_recipes/barplot.jl
+++ src/basic_recipes/barplot.jl
@@ -8,11 +8,15 @@ $(ATTRIBUTES)
 """
 @recipe(BarPlot, x, y) do scene
     Attributes(;
-        fillto = 0.0,
+        fillto = automatic,
         color = theme(scene, :color),
         colormap = theme(scene, :colormap),
         colorrange = automatic,
+        dodge = automatic,
+        x_gap = 0.1,
+        dodge_gap = 0.03,
         marker = Rect,
+        stack = automatic,
         strokewidth = 0,
         strokecolor = :white,
         width = automatic,
@@ -23,8 +27,7 @@ end
 
 conversion_trait(::Type{<: BarPlot}) = PointBased()
 
-function bar_rectangle(xy, width, fillto)
-    x, y = xy
+function bar_rectangle(x, y, width, fillto)
     # y could be smaller than fillto...
     ymin = min(fillto, y)
     ymax = max(fillto, y)
@@ -33,6 +36,9 @@ function bar_rectangle(xy, width, fillto)
 end
 
 flip(r::Rect2D) = Rect2D(reverse(origin(r)), reverse(widths(r)))
+
+using DataAPI: refarray, levels
+import DataFrames
 
 function AbstractPlotting.plot!(p::BarPlot)
 
@@ -46,15 +52,65 @@ function AbstractPlotting.plot!(p::BarPlot)
         end
     end
 
-    bars = lift(p[1], p.fillto, p.width, in_y_direction) do xy, fillto, width, in_y_direction
+    bars = lift(p[1], p.fillto, p.width, p.dodge, p.x_gap, p.dodge_gap, p.stack, in_y_direction) do xy, fillto, width, dodge, x_gap, dodge_gap, stack, in_y_direction
+      
+        #
+        if dodge === automatic
+            n_dodge = 1
+        else
+            n_dodge = length(levels(dodge))
+            n_dodge
+        end
+        
+        x = first.(xy)
+        y = last.(xy)
+        
         # compute half-width of bars
         if width === automatic
-            # times 0.8 for default gap
-            width = mean(diff(first.(xy))) * 0.8
-            width = ifelse(isfinite(width), width, 0.8)
+            x_unique = unique(filter(isfinite, x))
+            
+            if length(x_unique) == 1
+                width = 1.0
+            else
+                width = mean(diff(sort(x_unique)))
+            end
         end
+        
+        # --------------------------------
+        # ------------ Dodging -----------
+        # --------------------------------
+        dodge_width = scale_width(x_gap, dodge_gap, n_dodge)
 
-        rects = bar_rectangle.(xy, width, fillto)
+        if dodge === automatic
+            i_dodge = 1
+        else
+            i_dodge = refarray(dodge)
+            @assert eltype(i_dodge) <: Integer
+            # This is satisfied if dodge isa PooledArray, CategoricalArray or Vector{<:Integer}
+        end
+        
+        shft = shift_dodge.(1:n_dodge, x_gap, dodge_gap, n_dodge)
+
+        # --------------------------------
+        # ----------- Stacking -----------
+        # --------------------------------
+
+        if stack === automatic
+            if fillto === automatic
+                fillto = 0.0
+            end
+        else
+            fillto === automatic || @warn "Ignore keyword fillto when keyword stack is provided"
+            i_stack = refarray(stack)
+            @assert eltype(i_dodge) <: Integer
+            # This is satisfied if dodge isa PooledArray, CategoricalArray or Vector{<:Integer}
+            
+            grp = dodge === automatic ? (x = x, ) : (i_dodge = i_dodge, x = x)
+            from, to = stack_grouped_from_to(i_stack, y, grp)
+            y, fillto = to, from
+        end
+        
+        rects = bar_rectangle.(x .+ width .* shft[i_dodge], y, width .* dodge_width, fillto)
         return in_y_direction ? rects : flip.(rects)
     end
 
@@ -62,4 +118,38 @@ function AbstractPlotting.plot!(p::BarPlot)
         p, bars, color = p.color, colormap = p.colormap, colorrange = p.colorrange,
         strokewidth = p.strokewidth, strokecolor = p.strokecolor, visible = p.visible
     )
+end
+
+scale_width(x_gap, dodge_gap, n_dodge) = ((1 - x_gap) - n_dodge * dodge_gap) / n_dodge
+
+function shift_dodge(i, x_gap, dodge_gap, n_dodge)
+    wdt = scale_width(x_gap, dodge_gap, n_dodge)
+
+    - (1/2) + (i-1)*(wdt + dodge_gap) + (0.5 * (wdt + x_gap + dodge_gap))
+end
+
+function stack_grouped_from_to(i_stack, y, grp)
+	tmp_df = DataFrames.DataFrame(; i_stack, y, is_pos = y .> 0, grp..., order = 1:length(y), copycols = false)
+    
+	# sort by i_stack
+	sort!(tmp_df, :i_stack)
+	
+	grp_keys = [keys(grp)..., :is_pos]
+	
+	DataFrames.transform!(
+    	DataFrames.groupby(tmp_df, grp_keys),
+		:y => stack_from_to_sorted => DataFrames.AsTable
+	)
+	
+	# restore original ordering
+	sort!(tmp_df, :order)
+	
+	(; tmp_df.from, tmp_df.to)
+end
+
+function stack_from_to_sorted(y)
+	to = cumsum(y)
+	from = [0.0; to[begin:end-1]]
+	
+	(from = from, to = to)
 end
