ed28de93e8fa9582a2b7cfd93e39861cb3ebb821
David Widmann <devmotion@users.noreply.github.com>
2021-02-26 18:05:41 +0100

    Add more attributes to `scatterlines` (#646)

--- src/basic_recipes/scatterlines.jl
+++ src/basic_recipes/scatterlines.jl
@@ -14,11 +14,13 @@ $(ATTRIBUTES)
         colormap = l_theme.colormap,
         colorrange = get(l_theme.attributes, :colorrange, automatic),
         linestyle = l_theme.linestyle,
+        linewidth = l_theme.linewidth,
         markercolor = s_theme.color,
         markercolormap = s_theme.colormap,
         markercolorrange = get(s_theme.attributes, :colorrange, automatic),
         markersize = s_theme.markersize,
         strokecolor = s_theme.strokecolor,
+        strokewidth = s_theme.strokewidth,
         marker = s_theme.marker,
     )
 end
@@ -28,12 +30,14 @@ function plot!(p::Combined{scatterlines, <:NTuple{N, Any}}) where N
     lines!(p, p[1:N]...;
         color = p.color,
         linestyle = p.linestyle,
+        linewidth = p.linewidth,
         colormap = p.colormap,
         colorrange = p.colorrange,
     )
     scatter!(p, p[1:N]...;
         color = p.markercolor,
         strokecolor = p.strokecolor,
+        strokewidth = p.strokewidth,
         marker = p.marker,
         markersize = p.markersize,
         colormap = p.markercolormap,
