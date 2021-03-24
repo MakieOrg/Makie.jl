85ef1d8e024d5c86ec0dead9e8b7e410dbd37bd4
SimonDanisch <sdanisch@gmail.com>
2021-03-24 17:59:52 +0100

    fix axis & bb
    Former-commit-id: 9ccac5bf882e3d8b05cd6dbe6b724de1514bb788

--- src/layouting/boundingbox.jl
+++ src/layouting/boundingbox.jl
@@ -88,7 +88,7 @@ end
 function boundingbox(x::Text, text::String, position::VecTypes)
 
     glyphlayout = x._glyphlayout[]
-    
+
     pos = to_ndim(Point3f0, x.position[], 0)
     rot = convert_attribute(x.rotation[], key"rotation"())
 
@@ -98,33 +98,6 @@ function boundingbox(x::Text, text::String, position::VecTypes)
         data_limits(x)
     end
 end
-
-# function boundingbox(x::Text, text::String, positions::AbstractArray)
-
-#     glyphorigins, glyphbbs = x._glyphlayout[]
-
-#     if x.space[] == :data
-
-#         bb = FRect3D()
-#         for (pos, char, charo, glyphbb) in zip(positions, x[1][], glyphorigins, glyphbbs)
-#             # ignore line breaks
-#             char in ('\r', '\n') && continue
-
-#             # TODO: Correct BBox
-#             charbb = FRect3D(glyphbb) + charo + to_ndim(Point3f0, pos, 0)
-#             if !isfinite(bb)
-#                 bb = charbb
-#             else
-#                 bb = union(bb, charbb)
-#             end
-#         end
-
-#     elseif x.space[] == :screen
-#         bb = data_limits(x)
-#     end
-
-#     bb
-# end
 
 function data_text_boundingbox(string::String, glyphlayout::Glyphlayout, rotation::Quaternion, position::Point3f0)
     bb = FRect3D()
