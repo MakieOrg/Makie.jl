616fc65edfdb65c244d07aa4cf227f9e7a95b0f2
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2021-03-26 15:05:52 +0100

    use linear gradients for smaller and more responsive color page (#673)
    * try using linear gradients for smaller page
    
    * fix gradients
    
    Former-commit-id: bf52e793a3912b140bd92ec7f320654d18fa00cd

--- docs/src_generation/colormap_generation.jl
+++ docs/src_generation/colormap_generation.jl
@@ -5,7 +5,7 @@ using AbstractPlotting.PlotUtils, AbstractPlotting.Colors
 ################################################################################
 
 
-function colors_svg(cs, w, h)
+function colors_svg(key::Symbol, cs, w, h; categorical)
     n = length(cs)
     ws = min(w / n, h)
     html = """
@@ -13,13 +13,32 @@ function colors_svg(cs, w, h)
     <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
      "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
      <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-          width="$(n * ws)mm" height="$(h)mm"
+          width="$(categorical ? n * ws : w)mm" height="$(h)mm"
           viewBox="0 0 $n 1" preserveAspectRatio="none"
           shape-rendering="crispEdges" stroke="none">
     """
-    for (i, c) in enumerate(cs)
+    if categorical
+        for (i, c) in enumerate(cs)
+            html *= """
+            <rect width="$(ws)mm" height="$(h)mm" x="$(i-1)" y="0" fill="#$(hex(convert(RGB, c)))" />
+            """
+        end
+    else
         html *= """
-        <rect width="$(ws)mm" height="$(h)mm" x="$(i-1)" y="0" fill="#$(hex(convert(RGB, c)))" />
+        <defs>                        |                      |
+        <linearGradient id="lgrad_$key" x1="0" y1="0" x2="1" y2="0">
+        """
+
+        for (i, c) in enumerate(cs)
+            html *= """
+            <stop offset="$((i - 1) / (n - 1))" stop-color="#$(hex(convert(RGB, c)))" />
+            """
+        end
+            
+        html *= """
+        </linearGradient>
+        </defs>
+        <rect width="100%" height="100%" x="0" y="0" fill="url(#lgrad_$key)" />
         """
     end
     html *= "</svg>"
@@ -42,9 +61,9 @@ function generate_colorschemes_table(ks)
         # cp7 = color_list(palette(k, 7))
 
         html *= "<tr><td class=\"attr\">:$k</td><td>"
-        html *= colors_svg(cp, w, h)
+        html *= colors_svg(k, cp, w, h, categorical = true)
         html *= "</td><td>"
-        html *= colors_svg(cg, w, h)
+        html *= colors_svg(k, cp, w, h, categorical = false)
         # html *= "</td><td>"
         # html *= colors_svg(cp7, 35, h)
         html *= "</td></tr>"
