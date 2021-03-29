7a38020adf1587b13a6559c627e4808218b57919
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2021-03-29 16:41:12 +0200

    add delete and empty for axis and delete for lscene (#679)
    Former-commit-id: 83ce8be954faf1c3418bc8f73bc74a2427af577a

--- test/unit_tests/makielayout.jl
+++ test/unit_tests/makielayout.jl
@@ -17,3 +17,17 @@
     is = layout[end + 1, :] = IntervalSlider(scene)
     @test true
 end
+
+@testset "deleting from axis" begin
+    f = Figure()
+    ax = Axis(f[1, 1])
+    sc = scatter!(ax, randn(100, 2))
+    li = lines!(ax, randn(100, 2))
+    hm = heatmap!(ax, randn(20, 20))
+    @test length(ax.scene.plots) == 3
+    delete!(ax, sc)
+    @test length(ax.scene.plots) == 2
+    @test sc ∉ ax.scene.plots
+    empty!(ax)
+    @test isempty(ax.scene.plots)
+end
