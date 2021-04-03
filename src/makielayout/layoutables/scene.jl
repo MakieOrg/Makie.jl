8b51592a1b435356e689ea82c7d153ad7d1b1ab0
ffreyer <frederic481994@hotmail.de>
2021-04-03 22:10:01 +0200

    set clear = false by default

--- src/makielayout/layoutables/scene.jl
+++ src/makielayout/layoutables/scene.jl
@@ -25,12 +25,9 @@ function layoutable(::Type{LScene}, fig_or_scene; bbox = nothing, scenekw = Name
     layoutobservables = LayoutObservables{LScene}(attrs.width, attrs.height, attrs.tellwidth, attrs.tellheight,
         attrs.halign, attrs.valign, attrs.alignmode; suggestedbbox = bbox)
 
-    # Using `clear = false` (default for scenes constructed from other scenes)
-    # breaks SSAO, so we're using clear = true as a default here. This means
-    # that this LScene might draw over plot objects from other scenes.
     # We also set `raw = false` because otherwise the scene will not automatically
     # pick a camera and draw axis.
-    scenekw = merge((raw = false, clear = true), scenekw)
+    scenekw = merge((raw = false, clear = false), scenekw)
     scene = Scene(topscene, lift(round_to_IRect2D, layoutobservables.computedbbox); scenekw...)
 
     ls = LScene(fig_or_scene, layoutobservables, attrs, Dict{Symbol, Any}(), scene)
