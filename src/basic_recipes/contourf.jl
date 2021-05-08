e1ace4ed1ef4b6e00d7a8d8abc7a8e76eb8f1765
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-05-08 14:27:45 +0200

    add relative contourf mode

--- src/basic_recipes/contourf.jl
+++ src/basic_recipes/contourf.jl
@@ -8,7 +8,11 @@ The attribute `levels` can be either
 - an `Int` that produces n equally wide levels or bands
 - an `AbstractVector{<:Real}` that lists n consecutive edges from low to high, which result in n-1 levels or bands
 
-If you want to show a band from `-Inf` to the low edge, set `extendlow` to `:auto` for the same color as the first level, or specify a different color (default `nothing` means no extended band)
+You can also set the `mode` attribute to `:relative`.
+In this mode you specify edges by the fraction between minimum and maximum value of `zs`.
+This can be used for example to draw bands for the upper 90% while excluding the lower 10% with `levels = 0.1:0.1:1.0, mode = :relative`.
+
+In :normal mode, if you want to show a band from `-Inf` to the low edge, set `extendlow` to `:auto` for the same color as the first level, or specify a different color (default `nothing` means no extended band)
 If you want to show a band from the high edge to `Inf`, set `extendhigh` to `:auto` for the same color as the last level, or specify a different color (default `nothing` means no extended band)
 
 ## Attributes
@@ -17,6 +21,7 @@ $(ATTRIBUTES)
 @recipe(Contourf) do scene
     Theme(
         levels = 10,
+        mode = :normal,
         colormap = :viridis,
         extendlow = nothing,
         extendhigh = nothing,
@@ -41,12 +46,22 @@ end
 
 conversion_trait(::Type{<:Contourf}) = ContinuousSurface()
 
+function _get_isoband_levels(::Val{:normal}, levels, values)
+    _get_isoband_levels(levels, extrema_nan(values)...)
+end
+
+function _get_isoband_levels(::Val{:relative}, levels::AbstractVector, values)
+    mi, ma = extrema_nan(values)
+    Float32.(levels .* (ma - mi) .+ mi)
+end
+
+
 function AbstractPlotting.plot!(c::Contourf{<:Tuple{<:AbstractVector{<:Real}, <:AbstractVector{<:Real}, <:AbstractMatrix{<:Real}}})
     xs, ys, zs = c[1:3]
 
 
-    c.attributes[:_computed_levels] = lift(zs, c.levels) do zs, levels
-        _get_isoband_levels(levels, extrema_nan(zs)...)
+    c.attributes[:_computed_levels] = lift(zs, c.levels, c.mode) do zs, levels, mode
+        _get_isoband_levels(Val(mode), levels, vec(zs))
     end
 
     colorrange = lift(c._computed_levels) do levels
