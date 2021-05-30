943db21f7c5edcdfcd7b6943ea780c77cb0869e7
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-05-30 18:24:37 +0200

    default patchcolors were too translucent for dark themes

--- src/theming.jl
+++ src/theming.jl
@@ -47,7 +47,7 @@ end
 
 const default_palettes = Attributes(
     color = wong_colors(1),
-    patchcolor = Makie.wong_colors(0.6),
+    patchcolor = Makie.wong_colors(0.8),
     marker = [:circle, :utriangle, :cross, :rect, :diamond, :dtriangle, :pentagon, :xcross],
     linestyle = [nothing, :dash, :dot, :dashdot, :dashdotdot],
     side = [:left, :right]
