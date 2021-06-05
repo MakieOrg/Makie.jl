f8aaac2b028614162b6aee9cfc75a1bc5b440a9d
ffreyer <frederic481994@hotmail.de>
2021-06-05 15:58:36 +0200

    move all settings into attributes

--- src/camera/keyboard_camera3d.jl
+++ src/camera/keyboard_camera3d.jl
@@ -1,12 +1,4 @@
 struct KeyCamera3D <: AbstractCamera
-    rotationspeed::Node{Float32}
-    translationspeed::Node{Float32}
-    zoomspeed::Node{Float32}
-
-    enable_crosshair::Node{Bool}
-    keep_lookat::Node{Bool}
-    update_rate::Node{Float64}
-
     eyeposition::Node{Vec3f0}
     lookat::Node{Vec3f0}
     upvector::Node{Vec3f0}
@@ -15,83 +7,104 @@ struct KeyCamera3D <: AbstractCamera
     near::Node{Float32}
     far::Node{Float32}
 
-    pulser::Node{Bool}
+    pulser::Node{Float64}
 
-    key_controls::Dict{Symbol, Keyboard.Button}
+    #key_attr::Dict{Symbol, Keyboard.Button}
+    attributes::Attributes
 end
 
 to_node(x) = Node(x)
 to_node(n::Node) = n
 
+"""
+
+keyboard keys can be sets too
+"""
 function keyboard_cam!(scene; kwargs...)
     kwdict = Dict(kwargs)
-    key_controls = Dict{Symbol, Keyboard.Button}(
-        # translations
-        :up        => to_value(get(kwdict, :up, Keyboard.left_shift)),
-        :down      => to_value(get(kwdict, :down, Keyboard.left_control)),
-        :left      => to_value(get(kwdict, :left, Keyboard.j)),
-        :right     => to_value(get(kwdict, :right, Keyboard.l)),
-        :forward   => to_value(get(kwdict, :forward, Keyboard.w)),
-        :backward  => to_value(get(kwdict, :backward, Keyboard.s)),
-        # zooms
-        :zoom_in   => to_value(get(kwdict, :backward, Keyboard.i)),
-        :zoom_out  => to_value(get(kwdict, :backward, Keyboard.k)),
-        # rotations
-        :pan_left  => to_value(get(kwdict, :pan_left, Keyboard.a)),
-        :pan_right => to_value(get(kwdict, :pan_right, Keyboard.d)),
-        :tilt_up   => to_value(get(kwdict, :tilt_up, Keyboard.r)),
-        :tilt_down => to_value(get(kwdict, :tilt_down, Keyboard.f)),
-        :roll_clockwise => to_value(get(kwdict, :roll_clockwise, Keyboard.e)),
-        :roll_counterclockwise => to_value(get(kwdict, :roll_counterclockwise, Keyboard.q))
-    )
+    attributes = merged_get!(:cam3d, scene, Attributes(kwargs)) do 
+        Attributes(
+            # Keyboard
+            # Translations
+            up_key        = Keyboard.left_shift,
+            down_key      = Keyboard.left_control,
+            left_key      = Keyboard.j,
+            right_key     = Keyboard.l,
+            forward_key   = Keyboard.w,
+            backward_key  = Keyboard.s,
+            # Zooms
+            zoom_in_key   = Keyboard.i,
+            zoom_out_key  = Keyboard.k,
+            # Rotations
+            pan_left_key  = Keyboard.a,
+            pan_right_key = Keyboard.d,
+            tilt_up_key   = Keyboard.r,
+            tilt_down_key = Keyboard.f,
+            roll_clockwise_key        = Keyboard.e,
+            roll_counterclockwise_key = Keyboard.q,
+            # Mouse
+            translation_button = Mouse.right,
+            rotation_button    = Mouse.left,
+            # Settings
+            # TODO differentiate mouse and keyboard speeds
+            rotationspeed = 1f0,
+            translationspeed = 1f0,
+            zoomspeed = 1f0,
+            fov = 45f0, # base fov
+            rotation_center = :lookat,
+            enable_crosshair = true,
+            update_rate = 1/30,
+            projectiontype = Perspective
+        )
+    end
 
     cam = KeyCamera3D(
-        to_node(get(kwdict, :rotationspeed, 1.0)),
-        to_node(get(kwdict, :translationspeed, 1.0)),
-        to_node(get(kwdict, :zoomspeed, 1.0)),
-
-        to_node(get(kwdict, :enable_crosshair, true)),
-        to_node(get(kwdict, :keep_lookat, Node(true))),
-        to_node(get(kwdict, :update_rate, Node(1/60))),
-
-        to_node(get(kwdict, :eyeposition, Vec3f0(3))),
-        to_node(get(kwdict, :lookat, Vec3f0(0))),
-        to_node(get(kwdict, :upvector, Vec3f0(0, 0, 1))),
+        to_node(pop!(attributes, :eyeposition, Vec3f0(3))),
+        to_node(pop!(attributes, :lookat,      Vec3f0(0))),
+        to_node(pop!(attributes, :upvector,    Vec3f0(0, 0, 1))),
 
         to_node(get(kwdict, :fov, 45f0)),
-        to_node(get(kwdict, :near, 0.01f0)),
-        to_node(get(kwdict, :far, 100f0)),
+        to_node(pop!(kwdict, :near, 0.01f0)),
+        to_node(pop!(kwdict, :far, 100f0)),
 
-        Node(false),
+        Node(-1.0),
 
-        key_controls
+        attributes
     )
 
     disconnect!(camera(scene))
 
-    # ticks every so often to get consistent position updates
-    on(cam.pulser) do active
+    # ticks every so often to get consistent position updates.
+    on(cam.pulser) do prev_time
+        current_time = time()
+        active = on_pulse(scene, cam, Float32(current_time - prev_time))
         @async if active
-            sleep(cam.update_rate[])
-            cam.pulser[] = any(key -> key in Set(values(cam.key_controls)), events(scene).keyboardstate)
+            sleep(cam.attributes.update_rate[])
+            cam.pulser[] = current_time
+        else
+            cam.pulser.val = -1.0
         end
     end
-    on(_ -> on_pulse(scene, cam), cam.pulser)
 
+    keynames = (
+        :up_key, :down_key, :left_key, :right_key, :forward_key, :backward_key, 
+        :zoom_in_key, :zoom_out_key, :pan_left_key, :pan_right_key, :tilt_up_key, 
+        :tilt_down_key, :roll_clockwise_key, :roll_counterclockwise_key
+    )
     # This stops working with camera(scene)?
     # camera(scene),
     on(events(scene).keyboardbutton) do event
-        if event.action == Keyboard.press && event.key in values(cam.key_controls)
-            if !cam.pulser[]
-                cam.pulser[] = true
-            end
+        if event.action == Keyboard.press && cam.pulser[] == -1.0 &&
+            any(key -> ispressed(scene, cam.attributes[key][]), keynames)
+              
+            cam.pulser[] = time()
             return true
         end
         return false
     end
    
-    add_translation!(scene, cam)
-    add_rotation!(scene, cam)
+    add_translation!(scene, cam, attributes[:translation_button])
+    add_rotation!(scene, cam, attributes[:rotation_button])
     
     cameracontrols!(scene, cam)
     on(camera(scene), scene.px_area) do area
@@ -106,7 +119,7 @@ function keyboard_cam!(scene; kwargs...)
         marker = '+', 
         # TODO this needs explicit cleanup
         markersize = lift(rect -> 0.01f0 * sum(widths(rect)), scene.data_limits), 
-        markerspace = SceneSpace, color = :red, visible = cam.enable_crosshair
+        markerspace = SceneSpace, color = :red, visible = cam.attributes[:enable_crosshair]
     )
 
     cam
@@ -115,6 +128,8 @@ end
 
 # TODO switch button and key because this is the wrong order
 function add_translation!(scene, cam::KeyCamera3D, button = Node(Mouse.right))
+    translationspeed = cam.attributes[:translationspeed]
+    zoomspeed = cam.attributes[:zoomspeed]
     last_mousepos = RefValue(Vec2f0(0, 0))
     dragging = RefValue(false)
 
@@ -128,7 +143,7 @@ function add_translation!(scene, cam::KeyCamera3D, button = Node(Mouse.right))
             elseif event.action == Mouse.release && dragging[]
                 mousepos = mouseposition_px(scene)
                 dragging[] = false
-                diff = (last_mousepos[] - mousepos) * 0.01f0 * cam.translationspeed[]
+                diff = (last_mousepos[] - mousepos) * 0.01f0 * translationspeed[]
                 last_mousepos[] = mousepos
                 translate_cam!(scene, cam, Vec3f0(diff[1], diff[2], 0f0))
                 update_cam!(scene, cam)
@@ -142,7 +157,7 @@ function add_translation!(scene, cam::KeyCamera3D, button = Node(Mouse.right))
     on(camera(scene), scene.events.mouseposition) do mp
         if dragging[] && ispressed(scene, button[])
             mousepos = screen_relative(scene, mp)
-            diff = (last_mousepos[] .- mousepos) * 0.01f0 * cam.translationspeed[]
+            diff = (last_mousepos[] .- mousepos) * 0.01f0 * translationspeed[]
             last_mousepos[] = mousepos
             translate_cam!(scene, cam, Vec3f0(diff[1], diff[2], 0f0))
             update_cam!(scene, cam)
@@ -156,7 +171,7 @@ function add_translation!(scene, cam::KeyCamera3D, button = Node(Mouse.right))
             cam_res = Vec2f0(widths(scene.px_area[]))
             mouse_pos_normalized = mouseposition_px(scene) ./ cam_res
             mouse_pos_normalized = 2*mouse_pos_normalized .- 1f0
-            zoom = (1f0 + 0.1f0 * cam.zoomspeed[]) ^ scroll[2]
+            zoom = (1f0 + 0.1f0 * zoomspeed[]) ^ -scroll[2]
             _zoom!(scene, cam, mouse_pos_normalized, zoom)
             update_cam!(scene, cam)
             return true
@@ -166,6 +181,7 @@ function add_translation!(scene, cam::KeyCamera3D, button = Node(Mouse.right))
 end
 
 function add_rotation!(scene, cam::KeyCamera3D, button = Node(Mouse.left))
+    rotationspeed = cam.attributes[:rotationspeed]
     last_mousepos = RefValue(Vec2f0(0, 0))
     dragging = RefValue(false)
     e = events(scene)
@@ -179,7 +195,7 @@ function add_rotation!(scene, cam::KeyCamera3D, button = Node(Mouse.left))
             elseif event.action == Mouse.release && dragging[]
                 mousepos = mouseposition_px(scene)
                 dragging[] = false
-                rot_scaling = cam.rotationspeed[] * (e.window_dpi[] * 0.005)
+                rot_scaling = rotationspeed[] * (e.window_dpi[] * 0.005)
                 mp = (last_mousepos[] - mousepos) * 0.01f0 * rot_scaling
                 last_mousepos[] = mousepos
                 rotate_cam!(scene, cam, Vec3f0(-mp[2], mp[1], 0f0))
@@ -193,7 +209,7 @@ function add_rotation!(scene, cam::KeyCamera3D, button = Node(Mouse.left))
     on(camera(scene), e.mouseposition) do mp
         if dragging[]
             mousepos = screen_relative(scene, mp)
-            rot_scaling = cam.rotationspeed[] * (e.window_dpi[] * 0.005)
+            rot_scaling = rotationspeed[] * (e.window_dpi[] * 0.005)
             mp = (last_mousepos[] .- mousepos) * 0.01f0 * rot_scaling
             last_mousepos[] = mousepos
             rotate_cam!(scene, cam, Vec3f0(-mp[2], mp[1], 0f0))
@@ -205,40 +221,58 @@ function add_rotation!(scene, cam::KeyCamera3D, button = Node(Mouse.left))
 end
 
 
-function on_pulse(scene, cc)
-    controls = cc.key_controls
+function on_pulse(scene, cc, timestep)
+    attr = cc.attributes
 
-    lookat = cc.lookat[]
-    eyepos = cc.eyeposition[]
-    up = cc.upvector[]          # +y
-    viewdir = lookat - eyepos   # -z
-    right = cross(viewdir, up)  # +x
+    right = ispressed(scene, attr[:right_key][])
+    left = ispressed(scene, attr[:left_key][])
+    up = ispressed(scene, attr[:up_key][])
+    down = ispressed(scene, attr[:down_key][])
+    backward = ispressed(scene, attr[:backward_key][])
+    forward = ispressed(scene, attr[:forward_key][])
+    translating = right || left || up || down || backward || forward
 
-    # TODO scale translation with zoom or ||lookat-eyeposition||
-    translation = cc.translationspeed[] * norm(viewdir) * cc.update_rate[] * Vec3f0(
-        ispressed(scene, controls[:right])    - ispressed(scene, controls[:left]),
-        ispressed(scene, controls[:up])       - ispressed(scene, controls[:down]),
-        ispressed(scene, controls[:backward]) - ispressed(scene, controls[:forward])
-    )
+    if translating
+        lookat = cc.lookat[]
+        eyepos = cc.eyeposition[]
+        viewdir = lookat - eyepos
+        translation = attr[:translationspeed][] * norm(viewdir) * timestep * 
+            Vec3f0(right - left, up - down, backward - forward)
 
-    translate_cam!(scene, cc, translation)
+        translate_cam!(scene, cc, translation)
+    end
 
-    # rotations around x/y/z axes
-    angles = cc.rotationspeed[] * cc.update_rate[] * Vec3f0(
-        ispressed(scene, controls[:tilt_up])        - ispressed(scene, controls[:tilt_down]),
-        ispressed(scene, controls[:pan_left])       - ispressed(scene, controls[:pan_right]),
-        ispressed(scene, controls[:roll_counterclockwise]) - ispressed(scene, controls[:roll_clockwise])
-    )
+    up = ispressed(scene, attr[:tilt_up_key][])
+    down = ispressed(scene, attr[:tilt_down_key][])
+    left = ispressed(scene, attr[:pan_left_key][])
+    right = ispressed(scene, attr[:pan_right_key][])
+    counterclockwise = ispressed(scene, attr[:roll_counterclockwise_key][])
+    clockwise = ispressed(scene, attr[:roll_clockwise_key][])
+    rotating = up || down || left || right || counterclockwise || clockwise
 
-    rotate_cam!(scene, cc, angles)
+    if rotating
+        # rotations around x/y/z axes
+        angles = attr[:rotationspeed][] * timestep * 
+            Vec3f0(up - down, left - right, counterclockwise - clockwise)
 
-    # apply zoom (lookat - eyepos distance)
-    zoom = (1f0 + cc.zoomspeed[] * cc.update_rate[]) ^ 
-        (ispressed(scene, controls[:zoom_out]) - ispressed(scene, controls[:zoom_in]))
-    
-    _zoom!(scene, cc, Vec3f0(0), zoom)
+        rotate_cam!(scene, cc, angles)
+    end
 
-    update_cam!(scene, cc)
+    zoom_out = ispressed(scene, attr[:zoom_out_key][])
+    zoom_in = ispressed(scene, attr[:zoom_in_key][])
+    zooming = zoom_out || zoom_in
+
+    if zooming
+        zoom = (1f0 + attr[:zoomspeed][] * timestep) ^ (zoom_out - zoom_in)
+        _zoom!(scene, cc, Vec3f0(0), zoom)
+    end
+
+    if translating || rotating || zooming
+        update_cam!(scene, cc)
+        return true
+    else 
+        return false 
+    end
 end
 
 
@@ -273,7 +307,7 @@ function rotate_cam!(scene, cc::KeyCamera3D, angles)
     
     cc.upvector[] = rotation * up
     viewdir = rotation * viewdir
-    if cc.keep_lookat[]
+    if cc.attributes[:rotation_center] == :lookat
         cc.eyeposition[] = lookat - viewdir    
     else
         cc.lookat[] = eyepos + viewdir
@@ -282,11 +316,11 @@ function rotate_cam!(scene, cc::KeyCamera3D, angles)
 end
 
 function _zoom!(scene::Scene, cc::KeyCamera3D, mouse_pos_normalized, zoom::AbstractFloat)
-    lookat = cc.lookat[]
+    # lookat = cc.lookat[]
     eyepos = cc.eyeposition[]
-    viewdir = lookat - eyepos
-    cc.eyeposition[] = lookat - zoom * viewdir
-    # cc.fov[] = zoom * cc.fov[]
+    # viewdir = lookat - eyepos
+    # cc.eyeposition[] = lookat - zoom * viewdir
+    cc.fov[] = clamp(zoom * cc.fov[], 0.01f0, 175f0)
     nothing
 end
 
