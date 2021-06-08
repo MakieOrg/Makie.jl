f7eb29c627bb33dfec065f003f878061653f78f6
Dehaybe <henri.dehaybe@uclouvain.be>
2021-06-08 18:22:16 +0200

    Fix when color scalar is not a Symbal

--- src/stats/boxplot.jl
+++ src/stats/boxplot.jl
@@ -75,7 +75,6 @@ _flip_xy(r::Rect{2,T}) where {T} = Rect{2,T}(reverse(r.origin), reverse(r.widths
 
 function Makie.plot!(plot::BoxPlot)
     args = @extract plot (width, range, show_outliers, whiskerwidth, show_notch, orientation, x_gap, dodge, n_dodge, dodge_gap)
-
     signals = lift(
         plot[1],
         plot[2],
@@ -138,7 +137,7 @@ function Makie.plot!(plot::BoxPlot)
             push!(medians, q3)
             push!(boxmax, q4)
         end
-
+        
         # for horizontal boxplots just flip all components
         if orientation == :horizontal
             outlier_points = _flip_xy.(outlier_points)
@@ -171,7 +170,7 @@ function Makie.plot!(plot::BoxPlot)
 
     outliercolor = lift(plot[:outliercolor], plot[:color], centers, outliers, orientation) do outliercolor, color, centers, outliers, orientation
         if outliercolor === automatic
-            color isa Symbol && return color
+            color isa AbstractVector || return color
             center_colors = Dict{eltype(centers), eltype(color)}()
             for (col, center) in zip(color, centers)
                 center_colors[center] = col
