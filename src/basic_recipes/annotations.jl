2c22b0e43474723fbaa104f03c11e1b0bdd5f0e2
piever <pietro.vertechi@protonmail.com>
2021-08-04 12:48:35 +0200

    add multiargument method to annotations

--- src/basic_recipes/annotations.jl
+++ src/basic_recipes/annotations.jl
@@ -11,6 +11,25 @@ $(ATTRIBUTES)
 end
 
 function convert_arguments(::Type{<: Annotations},
+                           xs::AbstractVector{<: Number},
+                           ys::AbstractVector{<: Number},
+                           strings::AbstractVector{<: AbstractString})
+    return (map(strings, xs, ys) do str, x, y
+        (String(str), Point{2, Float32}(x, y))
+    end,)
+end
+
+function convert_arguments(::Type{<: Annotations},
+                           xs::AbstractVector{<: Number},
+                           ys::AbstractVector{<: Number},
+                           zs::AbstractVector{<: Number},
+                           strings::AbstractVector{<: AbstractString})
+    return (map(strings, xs, ys, zs) do str, x, y, z
+        (String(str), Point{3, Float32}(x, y, z))
+    end,)
+end
+
+function convert_arguments(::Type{<: Annotations},
                            strings::AbstractVector{<: AbstractString},
                            text_positions::AbstractVector{<: Point{N}}) where N
     return (map(strings, text_positions) do str, pos
