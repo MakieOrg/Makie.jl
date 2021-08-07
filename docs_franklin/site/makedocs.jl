e2ad38194a4d42cd4d98acf68db4c728372e61a7
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-08-07 23:37:28 +0200

    use documenter deploydocs

---
+++ docs_franklin/site/makedocs.jl
@@ -0,0 +1,13 @@
+using NodeJS
+using Franklin
+using Documenter: deploydocs
+
+run(`$(npm_cmd()) install highlight.js`)
+
+optimize()
+
+deploydocs(
+    repo = "github.com/jkrumbiegel/Makie.jl.git",
+    push_preview = true,
+    target = "__site",
+)
