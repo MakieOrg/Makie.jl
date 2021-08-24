cca05ca520da9181fb3995921fc3cc407f92ff05
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-08-24 22:04:29 +0200

    fix layout bug and clipboard polyfill

--- docs/_libs/navbar.js
+++ docs/_libs/navbar.js
@@ -82,10 +82,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		b.onclick = function(){
 			b.innerText = "\uf46c";
+			b.classList.add("clicked");
+			copyToClipboard(el.innerText);
 			setTimeout(function(){
 				b.innerText = "\uf46d";
-				navigator.clipboard.writeText(el.innerText);
+				b.classList.remove("clicked");
 			}, 2000);
 		};
 	});
 });
+
+
+
+function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
