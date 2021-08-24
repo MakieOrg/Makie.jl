e5b4cfd2d345dc12098f779abababa4ef0af9746
ffreyer <frederic481994@hotmail.de>
2021-08-24 16:09:17 +0200

    remove old code

--- src/interaction/events.jl
+++ src/interaction/events.jl
@@ -43,6 +43,7 @@ struct Not{T} <: BooleanOperator
     x::T
 end
 
+
 function Base.show(io::IO, op::And)
     print(io, "(")
     show(io, op.l)
@@ -118,52 +119,6 @@ ispressed(scene, op::Not) = !ispressed(scene, op.x)
 ispressed(scene, set::Set) = all(x -> ispressed(scene, x), set)
 ispressed(scene, set::Vector) = all(x -> ispressed(scene, x), set)
 ispressed(scene, set::Tuple) = all(x -> ispressed(scene, x), set)
-
-
-
-#=
-button_key(x::Type{T}) where {T} = error("Must be a keyboard or mouse button. Found: $T")
-button_key(x::Type{Keyboard.Button}) = :keyboardstate
-button_key(x::Type{Mouse.Button}) = :mousebuttonstate
-button_key(x::Set{T}) where {T} = button_key(T)
-button_key(x::T) where {T} = button_key(T)
-
-"""
-    ispressed(scene, buttons)
-
-
-Returns true if all `buttons` are pressed in the given `scene`. `buttons` can be
-a `Vector` or `Tuple` of `Keyboard` buttons (e.g. `Keyboard.a`), `Mouse` buttons
-(e.g. `Mouse.left`) and `nothing`.
-"""
-function ispressed(scene::SceneLike, button::Union{Vector, Tuple})
-    all(x-> ispressed(scene, x), button)
-end
-
-# TODO this is a bit shady, but maybe a nice api!
-# So you can use void whenever you don't care what is pressed
-ispressed(scene::SceneLike, ::Nothing) = true
-
-function ispressed(buttons::Set{T}, button::T) where T <: Union{Keyboard.Button, Mouse.Button}
-    return button in buttons
-end
-
-function ispressed(buttons::Set{T}, button::Set{T}) where T <: Union{Keyboard.Button, Mouse.Button}
-    return issubset(button, buttons)
-end
-
-"""
-    ispressed(scene, button)
-
-Returns true if `button` is pressed in the given `scene`. The `button` can be
-a `Keyboard` button (e.g. `Keyboard.a`), a `Mouse` button (e.g. `Mouse.left`)
-or `nothing`. In the latter case `true` is always returned.
-"""
-function ispressed(scene::SceneLike, button)
-    buttons = getfield(events(scene), button_key(button))
-    ispressed(buttons, button)
-end
-=#
 
 
 """
