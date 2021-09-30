a46baa28ff83f2bca65cc676d21205d3a2766cec
SimonDanisch <sdanisch@gmail.com>
2021-09-30 19:20:53 +0200

    use Unitful + better default unit selection

--- src/makielayout/layoutables/time_axis.jl
+++ src/makielayout/layoutables/time_axis.jl
@@ -1,12 +1,22 @@
 using GLMakie, Dates, Makie
+using Unitful
+
 
 struct TimeTicks
     time_unit
     time_range
     tickformatter
 end
+const UnitfulTimes = (
+    typeof(1.0*u"d"), typeof(1.0*u"hr"), typeof(1.0*u"minute"),
+    typeof(1.0*u"s"), typeof(1.0*u"ms"), typeof(1.0*u"μs"), typeof(1.0*u"ns"))
 
-TimeTicks(ticks=Makie.automatic) = TimeTicks(Observable{Any}(nothing), Observable{Any}((Inf, -Inf)), ticks)
+TimeUnits = Union{
+    Number,  # No time units
+    Nothing, # nothing yet
+    UnitfulTimes...}
+
+TimeTicks(ticks=Makie.automatic) = TimeTicks(Observable{Any}(nothing), Observable((Inf*u"ns", -Inf*u"ns")), ticks)
 to_timeticks(ticks) = TimeTicks(ticks)
 to_timeticks(ticks::TimeTicks) = ticks
 
@@ -14,8 +24,7 @@ struct TimeAxis
     axis::Axis
 end
 
-unit_symbol(::Type{Hour}) = "Hr"
-unit_symbol(::Type{Second}) = "s"
+unit_symbol(::Type{T}) where T <: Unitful.AbstractQuantity = Unitful.unit(T)
 unit_symbol(::Union{Number, Nothing}) = ""
 
 function MakieLayout.get_ticks(ticks::TimeTicks, scale, formatter, vmin, vmax)
@@ -31,21 +40,58 @@ function TimeAxis(args...; xticks=TimeTicks(), yticks=TimeTicks(), kw...)
 end
 
 unit_convert(::Nothing, range, x) = x
-unit_convert(::Type{T}, range, x) where T <: Period = map(x-> convert(T, x).value, x)
+function unit_convert(::Type{T}, range, x) where T <: Unitful.AbstractQuantity
+    return map(x) do x
+        conv = convert(T, x)
+        return Unitful.ustrip(conv)
+    end
+end
 
-function new_unit(unit, values)
-    new_eltype = mapreduce(typeof, promote_type, values)
-    new_eltype <: Number && isnothing(unit) && return nothing
-    isnothing(unit) && return new_eltype
-    return promote_type(new_eltype, unit)
+const TimeLike = Union{UnitfulTimes..., Period}
+
+function best_unit(duration)
+    val = 100
+    duration < val * u"ns" && return typeof(1.0*u"ns")
+    duration < val * u"μs" && return typeof(1.0*u"μs")
+    duration < val * u"ms" && return typeof(1.0*u"ms")
+    duration < val * u"s" && return typeof(1.0*u"s")
+    duration < val * u"minute" && return typeof(1.0*u"minute")
+    duration < val * u"hr" && return typeof(1.0*u"hr")
+    return typeof(1.0*u"d")
+end
+
+function new_unit(unit, values, last_range)
+    new_eltype = Union{}
+    new_min = new_max = first(values)
+    for elem in values
+        new_eltype = promote_type(new_eltype, typeof(elem))
+        new_min = min(elem, new_min)
+        new_max = max(elem, new_max)
+    end
+
+    new_eltype <: Number && isnothing(unit) && return (nothing, last_range)
+
+    if new_eltype <: TimeLike
+        last_min, last_max = last_range
+        new_range = (min(last_min, uconvert(u"ns", new_min)), max(last_max, uconvert(u"ns", new_max)))
+        duration = new_range[2] - new_range[1]
+        @show duration
+        return best_unit(duration), new_range
+    end
+    # isnothing(unit) && return new_eltype
+    # return promote_type(new_eltype, unit)
 end
 
 function convert_times(ax::TimeAxis, x, y)
     xticks = ax.axis.xticks[]
     yticks = ax.axis.yticks[]
 
-    xticks.time_unit[] = new_unit(xticks.time_unit[], x[])
-    yticks.time_unit[] = new_unit(yticks.time_unit[], y[])
+    xunit, xrange = new_unit(xticks.time_unit[], x[], xticks.time_range[])
+    yunit, yrange = new_unit(yticks.time_unit[], y[], yticks.time_range[])
+    xticks.time_unit[] = xunit
+    yticks.time_unit[] = yunit
+    xticks.time_range[] = xrange
+    yticks.time_range[] = yrange
 
     xconv = map(unit_convert, xticks.time_unit, xticks.time_range, x)
     yconv = map(unit_convert, yticks.time_unit, yticks.time_range, y)
@@ -67,6 +113,7 @@ end
 
 f = Figure()
 ax = TimeAxis(f[1,1]; backgroundcolor=:white)
-scatter!(ax, rand(Hour(1):Hour(1):Hour(20), 10), 1:10)
-scatter!(ax, rand(Second(1):Second(60*60):Second(20*60*60), 10), 1:10)
+scatter!(ax, rand(Second(1):Second(60):Second(20*60), 10), 1:10)
 f
+scatter!(ax, rand(Hour(1):Hour(1):Hour(20), 10), 1:10)
+display(f)
