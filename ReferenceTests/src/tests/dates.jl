a007ec166d882f21f4587c6b7b119f185310a22a
SimonDanisch <sdanisch@gmail.com>
2021-10-27 12:11:19 +0200

    add some tests

---
+++ ReferenceTests/src/tests/dates.jl
@@ -0,0 +1,14 @@
+using GLMakie, Dates
+
+time = Time("11:11:55.914")
+date = Date("2021-10-27")
+date_time = DateTime("2021-10-27T11:11:55.914")
+time_range = range(time, step=Second(5), length=10)
+date_range = range(date, step=Day(5), length=10)
+date_time_range = range(date_time, step=Week(5), length=10)
+scatter(time_range, 1:10)
+scatter(date_range, 1:10)
+scatter(date_time_range, 1:10)
+
+date_time_range = range(date_time, step=Second(5), length=10)
+scatter(date_time_range, 1:10)
