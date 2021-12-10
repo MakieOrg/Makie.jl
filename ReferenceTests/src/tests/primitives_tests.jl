75dff9d8af37f899b1e011370edc79ecc67f1a9f
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2021-12-10 19:44:16 +0100

    start adding tests for primitives (#1481)
    * start adding tests for primitives
    
    * add poly test
    
    Co-authored-by: SimonDanisch <sdanisch@gmail.com>

---
+++ ReferenceTests/src/tests/primitives_tests.jl
@@ -0,0 +1,148 @@
+# "lines and linestyles"
+begin
+    s = Scene(resolution = (800, 800), camera = campixel!)
+    scalar = 30
+    points = Point2f[(1, 1), (1, 2), (2, 3), (2, 1)]
+    linestyles = [
+        :solid, :dash, :dot, :dashdot, :dashdotdot,
+        [1, 2, 3], [1, 2, 4, 5]
+    ]
+    for linewidth in 1:10
+        for (i, linestyle) in enumerate(linestyles)
+            lines!(s,
+                scalar .* (points .+ Point2f(linewidth*2, i * 3.25)),
+                linewidth = linewidth,
+                linestyle = linestyle,
+            )
+        end
+    end
+    s
+end
+
+# "lines with gaps"
+begin
+    s = Scene(resolution = (800, 800), camera = campixel!)
+    points = [
+        Point2f[(1, 0), (2, 0.5), (NaN, NaN), (4, 0.5), (5, 0)],
+        Point2f[(NaN, NaN), (2, 0.5), (3, 0), (4, 0.5), (5, 0)],
+        Point2f[(NaN, NaN), (2, 0.5), (3, 0), (4, 0.5), (NaN, NaN)],
+        Point2f[(NaN, NaN), (2, 0.5), (NaN, NaN), (4, 0.5), (NaN, NaN)],
+        Point2f[(NaN, NaN), (NaN, NaN), (3, 0), (NaN, NaN), (NaN, NaN)],
+        Point2f[(NaN, NaN), (NaN, NaN), (NaN, NaN), (NaN, NaN), (NaN, NaN)],
+    ]
+    for (i, p) in enumerate(points)
+        lines!(s, (p .+ Point2f(0, i)) .* 100, linewidth = 10)
+    end
+    s
+end
+
+# "scatters"
+begin
+    s = Scene(resolution = (800, 800), camera = campixel!)
+    
+    markersizes = 0:2:30
+    markers = [:circle, :rect, :cross, :utriangle, :dtriangle,
+        'a', 'x', 'h', 'g', 'Y', 'J', 'α', '↑'    
+    ]
+
+    for (i, ms) in enumerate(markersizes)
+        for (j, m) in enumerate(markers)
+            scatter!(s,
+                Point2f(i, j) .* 45,
+                marker = m,
+                markersize = ms,
+            )
+        end
+    end
+    s
+end
+
+# "scatter rotations"
+begin
+    s = Scene(resolution = (800, 800), camera = campixel!)
+    
+    rotations = range(0, 2pi, length = 15)
+    markers = [:circle, :rect, :cross, :utriangle, :dtriangle,
+        'a', 'x', 'h', 'g', 'Y', 'J', 'α', '↑'    
+    ]
+
+    for (i, rot) in enumerate(rotations)
+        for (j, m) in enumerate(markers)
+            p = Point2f(i, j) .* 45
+            scatter!(s,
+                p,
+                marker = m,
+                markersize = 30,
+                rotations = rot,
+            )
+            scatter!(s, p, color = :red, markersize = 6)
+        end
+    end
+    s
+end
+
+# "poly"
+begin
+    s = Scene(resolution = (800, 800), camera = campixel!)
+
+    scalefactor = 70
+    Pol = Makie.GeometryBasics.Polygon
+
+    translate(pol, p) = Pol(
+        Ref(p) .+ decompose(Point2f, pol.exterior),
+        map(x -> decompose(Point2f, x) .+ Ref(p), pol.interiors)
+    )
+    scale(pol, sca) = Pol(
+        sca .* decompose(Point2f, pol.exterior),
+        map(x -> decompose(Point2f, x) .* sca, pol.interiors)
+    )
+
+    polys = [
+        # three points
+        Pol(Point2f[(1, 1), (1, 2), (2, 1)]),
+        # four points
+        Pol(Point2f[(1, 1), (1, 2), (2, 2), (2, 1)]),
+        # double point
+        Pol(Point2f[(1, 1), (1, 2), (2, 2), (2, 2)]),
+        # one hole
+        Pol(
+            Point2f[(1, 1), (1, 2), (2, 2), (2, 1)],
+            [Point2f[(1.3, 1.3), (1.3, 1.7), (1.7, 1.7), (1.7, 1.3)]]
+        ),
+        # two holes
+        Pol(
+            Point2f[(1, 1), (1, 2), (2, 2), (2, 1)],
+            [
+                Point2f[(1.15, 1.15), (1.15, 1.85), (1.4, 1.85), (1.4, 1.15)],
+                Point2f[(1.6, 1.15), (1.6, 1.85), (1.85, 1.85), (1.85, 1.15)],
+            ]
+        ),
+        # hole half same as exterior
+        Pol(
+            Point2f[(1, 1), (1, 2), (2, 2), (2, 1)],
+            [Point2f[(1, 1), (1, 2), (2, 2)]],
+        ),
+        # point self intersection
+        Pol(
+            Point2f[(1, 1), (2, 1), (2, 2), (1.5, 1), (1, 2)],
+        ),
+    ]
+
+    linewidths = 0:2:9
+
+    for (i, p) in enumerate(polys)
+        for (j, lw) in enumerate(linewidths)
+            poly!(
+                s,
+                translate(
+                    scale(p, scalefactor),
+                    scalefactor * Point2f(1.3 * (i-1), 1.3 * j)
+                ),
+                color = (:red, 0.5),
+                strokewidth = lw,
+            )
+        end
+    end
+
+    s
+end
