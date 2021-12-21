a46d32d32c529cd8cf0ec20ffa1a57455e48d3af
ffreyer <frederic481994@hotmail.de>
2021-12-21 10:53:07 +0100

    cleanup fxaa docs

--- MakieCore/src/basic_plots.jl
+++ MakieCore/src/basic_plots.jl
@@ -24,7 +24,7 @@ Plots an image on range `x, y` (defaults to dimensions).
         colormap = [:black, :white],
         colorrange = automatic,
         interpolate = true,
-        fxaa = false,
+        fxaa = true,
         lowclip = nothing,
         highclip = nothing,
         inspectable = theme(scene, :inspectable)
