f12585fb6906c875acbd3a7a6ab28cba395eeaa3
ffreyer <frederic481994@hotmail.de>
2022-01-20 20:11:05 +0100

    fix 1.3

--- src/makielayout/defaultattributes.jl
+++ src/makielayout/defaultattributes.jl
@@ -1008,7 +1008,7 @@ function default_attributes(::Type{Axis3}, scene)
         "A number between 0 and 1, where 0 is orthographic, and 1 full perspective"
         perspectiveness = 0f0
         "Aspects of the 3 axes with each other"
-        aspect = (1, 1, 2/3) # :data :equal
+        aspect = (1.0, 1.0, 2/3) # :data :equal
         "The view mode which affects the final projection. `:fit` results in the projection that always fits the limits into the viewport, invariant to rotation. `:fitzoom` keeps the x/y ratio intact but stretches the view so the corners touch the scene viewport. `:stretch` scales separately in both x and y direction to fill the viewport, which can distort the `aspect` that is set."
         viewmode = :fitzoom # :fit :fitzoom :stretch
         "The background color"
