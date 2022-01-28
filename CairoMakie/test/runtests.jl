029d5819c2d30e65b22795d4bd38b74b55883c72
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-01-28 21:21:48 +0100

    try out githubactionsutils and disable all but cairomakie ci

--- CairoMakie/test/runtests.jl
+++ CairoMakie/test/runtests.jl
@@ -3,6 +3,11 @@ using CairoMakie
 using Pkg
 path = normpath(joinpath(dirname(pathof(Makie)), "..", "ReferenceTests"))
 Pkg.develop(PackageSpec(path = path))
+
+Pkg.develop(url = "https://github.com/jkrumbiegel/GitHubActionsUtils.jl/")
+using GitHubActionsUtils
+@assert haskey(ENV, "GITHUB_TOKEN")
+
 # Before changing Pkg environment, try the test in #864
 @testset "Runs without error" begin
     fig = Figure()
@@ -70,6 +75,85 @@ excludes = Set([
 functions = [:volume, :volume!, :uv_mesh]
 database = database_filtered(excludes, functions=functions)
 
-recorded = joinpath(@__DIR__, "recorded")
-rm(recorded; force=true, recursive=true); mkdir(recorded)
-@time ReferenceTests.run_reference_tests(database, recorded)
+tempd = mktempdir()
+recorded_dir = joinpath(tempd, "recorded")
+# rm(recorded; force=true, recursive=true);
+mkdir(recorded_dir)
+
+ReferenceTests.record_tests(database, recording_dir=recorded_dir)
+cp()
+
+refimage_dir = ReferenceTests.download_refimages()
+missing_files, scores = ReferenceTests.compare(
+    joinpath.(recording_folder, readdir(recording_folder)),
+    refimage_dir
+)
+
+# copy reference images outside of Makie folder which will get cleaned
+cp(refimage_dir, joinpath(tempd, "refimages"))
+
+score_cutoff = 0.03
+bad_scores = filter(x -> x[2] > score_cutoff, scores)
+
+
+
+if GitHubActionsUtils.is_pull_request()
+
+    # cd out of CairoMakie test folder into main repo folder
+    cd("../..")
+
+    pr_number = GitHubActionsUtils.pull_request_number()
+
+    image_branch_name = "pr$(pr_number)-test-images"
+
+    GitHubActionsUtils.set_github_actions_bot_as_git_user()
+
+    run(`git clean -f -d`)
+
+    GitHubActionsUtils.switch_to_or_create_branch(image_branch_name; orphan = true)
+    
+    run(`git rm -rf .`)
+
+    cp(refimage_dir, "refimages")
+    cp(recorded_dir, "recorded")
+
+    run(`git add -A`)
+    run(`git commit -m "save testimages"`)
+
+    GitHubActionsUtils.push_git_branch(image_branch_name)
+
+    commit_hash = chomp(read(`git rev-parse HEAD`, String))
+
+    function image_url(parts...)
+        string(
+            "https://raw.githubusercontent.com/",
+            GitHubActionsUtils.repository(),
+            "/",
+            commit_hash,
+            "/",
+            joinpath(parts...)
+        )
+    end
+
+    function comparison_markdown(filename, score)
+        """
+        <details>
+        <summary>$filename Score: $score</summary>
+
+        | Recorded | Reference |
+        |--|--|
+        | ![]($(image_url("recorded", filename))) | ![]($(image_url("refimages", filename))) |
+        
+        <details>
+        """
+    end
+
+    GitHubActionsUtils.comment_on_pr(
+        pr_number,
+        """
+        # Test images with high error scores:
+
+        $(join([comparison_markdown(bs[1], bs[2]) for bs in bad_scores], "\n\n"))
+        """
+    )
+end
