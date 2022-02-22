831f5f6e573a2239088b69feaf8e0a194040939b
SimonDanisch <sdanisch@protonmail.com>
2022-02-22 20:48:38 +0100

    newline is needed

--- metrics/ttfp/benchmark-ttfp.jl
+++ metrics/ttfp/benchmark-ttfp.jl
@@ -2,4 +2,4 @@ t_using = (tstart = time(); using CairoMakie; time() - tstart)
 
 t_plot = (tstart = time(); save("test.png", scatter(1:4)); time() - tstart)
 rm("test.png")
-print("($t_using, $t_plot)")
+println("($t_using, $t_plot)")
