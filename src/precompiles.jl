7e2f9bb8abe8bc3bd748d8a4051b1beebc9434e7
SimonDanisch <sdanisch@protonmail.com>
2022-02-22 16:37:10 +0100

    update precompile file

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -2,5 +2,7 @@ function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     f, ax1, pl = scatter(1:4)
     f, ax2, pl = lines(1:4)
+    f = Figure()
+    Axis(f[1,1])
     return
 end
