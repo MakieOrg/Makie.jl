e929cfab2bfd567c388a0da6bc6050842a724059
SimonDanisch <sdanisch@protonmail.com>
2022-02-24 12:02:21 +0100

    modularize etc

---
+++ metrics/ttfp/benchmark-library.jl
@@ -0,0 +1,143 @@
+using JSON, Statistics, GitHub, Base64, SHA, Downloads, Dates
+
+function cpu_key()
+    cpus = map(Sys.cpu_info()) do cpu
+        replace(cpu.model, " " => "")
+    end
+    return join(unique(cpus), "")
+end
+julia_key() = "julia-" * replace(string(VERSION), "."=>"-")
+tag_commit(tag) = chomp(read(`git show-ref -s $(tag)`, String))
+current_commit() =  chomp(read(`git rev-parse HEAD`, String))
+latest_commit(branch) = chomp(read(`git log -n 1 $branch --pretty=format:"%H"`, String))
+
+function get_file(ctx, repo_path)
+    file = GitHub.file(ctx.scratch_repo, repo_path; auth=ctx.auth, handle_error=false)
+    if isnothing(file.sha)
+        return (sha=nothing, content=nothing)
+    else
+        return (sha=file.sha, content = base64decode(file.content))
+    end
+end
+
+upload_file(ctx, file, repo_path) = upload_data(ctx, read(file), repo_path, file)
+function upload_data(ctx, bytes_or_str::Union{AbstractString, AbstractVector{UInt8}}, repo_path, file_name="data")
+    b64 = Base64.base64encode(bytes_or_str)
+    # see if there is an old file, (handle_error=false won't error, and instead returns file with all nothing)
+    old_file = get_file(ctx, repo_path)
+    params = Dict("content"=>b64, "message"=>"update benchmark for $(file_name)")
+    if !isnothing(old_file.sha)
+        # if there was a file already, we need to supply its sha
+        params["sha"] = old_file.sha
+    end
+    GitHub.create_file(ctx.scratch_repo, repo_path; auth=ctx.auth, params=params)
+    return "https://github.com/MakieOrg/scratch/blob/main/$(repo_path)?raw=true"
+end
+
+function make_or_edit_comment(ctx, pr, comment)
+    prev_comments, _ = GitHub.comments(ctx.repo, pr; auth=ctx.auth)
+    idx = findfirst(x-> occursin("## Compile Times compared to tagged", x.body), prev_comments)
+    if isnothing(idx)
+        GitHub.create_comment(ctx.repo, pr; auth=ctx.auth, params=Dict("body"=>comment))
+    else
+        GitHub.edit_comment(ctx.repo, prev_comments[idx], :pr; auth=ctx.auth, params=Dict("body"=>comment))
+    end
+end
+
+function run_bench(commit; n=10)
+    rm("benchmark-project/Manifest.toml")
+    Pkg.activate("benchmark-project")
+    pkgs = ["MakieCore", "Makie", "CairoMakie"]
+    pkgs = [PackageSpec(name=pkg, rev=commit) for pkg in pkgs]
+    Pkg.add(pkgs)
+    results = Vector{Float64}[]
+    for i in 1:n
+        result = read(`$(Base.julia_cmd()) --project=benchmark-project ./benchmark-ttfp.jl`, String)
+        tup = eval(Meta.parse(result))
+        @show tup
+        push!(results, [tup...])
+    end
+    Pkg.activate(".") # activate the main project
+    return results
+end
+
+function get_benchmark_data(ctx, commit)
+    repo_dir_path = "benchmarks/$(julia_key())/$(cpu_key())"
+    repo_base_path = "$(repo_dir_path)/$(commit)"
+    repo_data_path = "$(repo_base_path).json"
+    @info("Getting $(repo_data_path)")
+    old_file = get_file(ctx, repo_data_path)
+    if !isnothing(old_file.content)
+        return JSON.parse(String(old_file.content))
+    else
+        global results = run_bench(commit)
+        branch_name = chomp(read(`git describe --all --contains $(commit)`, String))
+        head="$(owner):$(branch_name)"
+        params = Dict("state" => "all", "head" => head);
+        prs, page_data = GitHub.pull_requests(ctx.repo; auth=ctx.auth, params = params);
+        pr_number = isempty(prs) ? nothing : prs[1].number
+        commit_info = GitHub.commit(ctx.repo, commit; handle_error=false)
+        message = isnothing(commit_info.commit) ? "" : commit_info.commit.message
+        result_with_info = Dict(
+            "branch" => branch_name,
+            "pr" => pr_number,
+            "message" => message,
+            "date" => string(now()),
+            "results" => results
+        )
+        upload_data(ctx, JSON.json(result_with_info), repo_data_path, "for commit $(commit[1:5])")
+        return result_with_info
+    end
+end
+
+function github_context()
+    owner = "JuliaPlots"
+    return (
+        owner = owner,
+        repo = GitHub.Repo("$(owner)/Makie.jl"),
+        auth = GitHub.authenticate(ENV["GITHUB_TOKEN"]),
+        scratch_repo = GitHub.Repo("MakieOrg/scratch")
+    )
+end
+
+function plot_benchmark!(ax, data, pos, width=0.9)
+    used = first.(data)
+    ttfp = last.(data)
+    sum_t = used .+ ttfp
+    n = length(data)
+    bplot!(d, c) = scatter!(ax, fill(pos, n), d, marker=Rect, markerspace=:data, markersize=Vec2f(width, 0.2), color=(c, 0.2))
+
+    bplot!(used, :darkred)
+    bplot!(ttfp, :blue)
+    bplot!(sum_t, :black)
+    hwidth = (width / 2) .+ 0.01
+    vlines!(ax, [pos - hwidth, pos + hwidth], linewidth=0.5, color=(:black, 0.5))
+    return (used, ttfp, sum_t)
+end
+
+function plot_benchmarks(benchmarks)
+    f = Figure()
+    ax = Axis(f[1,1]; ylabel="median time (s)")
+    mdata = Vector{Float64}[]
+    xticks = Int[]
+    xticklabels = String[]
+    for (i, data) in enumerate(benchmarks)
+        pdata = data["results"]
+        push!(mdata, plot_benchmark!(ax, pdata, i)...)
+        push!(xticks, i)
+        push!(xticklabels, data["branch"])
+    end
+    medians = median.(mdata)
+    medians = unique(round.(median.(mdata)))
+    ax.yticks = medians
+    ax.xticks = (xticks, xticklabels)
+    hidexdecorations!(ax, ticklabels=false)
+    f
+end
+
+function upload_data(ctx, fig, repo_path)
+    io = IOBuffer()
+    show(io, MIME"image/png"(), fig)
+    bytes = take!(io)
+    upload_data(ctx, bytes, repo_path, "plot")
+end
--- metrics/ttfp/run-benchmark.jl
+++ metrics/ttfp/run-benchmark.jl
@@ -1,145 +1,17 @@
 cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
-pkg"add JSON Statistics GitHub"
-pkg"dev ../../MakieCore/ ../../ ../../CairoMakie/; add JSON Statistics GitHub SHA"
-using JSON, Statistics, GitHub, Base64, SHA, Downloads, Dates
+using JSON, Statistics, GitHub, Base64, SHA, Downloads, Dates, CairoMakie
 
-commit = chomp(read(`git rev-parse HEAD`, String))
-repo = GitHub.Repo("JuliaPlots/Makie.jl")
-pr = GitHub.PullRequest(1691)
-auth = GitHub.authenticate(ENV["GITHUB_TOKEN"])
-scratch_repo = GitHub.Repo("MakieOrg/scratch")
-branch_name = chomp(read(`git rev-parse --abbrev-ref HEAD`, String))
-file_name = replace("julia_$(VERSION)_$(branch_name)", r"[\.\/]" => "-")
-repo_path = "benchmarks/$(file_name).png"
+include("benchmark-library.jl")
 
-function get_file(repo_path)
-    file = GitHub.file(scratch_repo, repo_path; auth=auth, handle_error=false)
-    if isnothing(file.sha)
-        return (sha=nothing, content=nothing)
-    else
-        return (sha=file.sha, content = base64decode(file.content))
-    end
-end
+ctx = github_context()
+commits_to_bench = [tag_commit("v0.16.5"), current_commit()]
+benchmarks = get_benchmark_data.(Ref(ctx), commits_to_bench)
+fig = plot_benchmarks(benchmarks)
 
-upload_file(file, repo_path) = upload_data(read(file), repo_path)
-function upload_data(bytes_or_str, repo_path)
-    b64 = Base64.base64encode(bytes_or_str)
-    # see if there is an old file, (handle_error=false won't error, and instead returns file with all nothing)
-    old_file = get_file(repo_path)
-    params = Dict("content"=>b64, "message"=>"update benchmark for $(file_name)")
-    if !isnothing(old_file.sha)
-        # if there was a file already, we need to supply its sha
-        params["sha"] = old_file.sha
-    end
-    GitHub.create_file(scratch_repo, repo_path; auth=auth, params=params)
-    return "https://github.com/MakieOrg/scratch/blob/main/$(repo_path)?raw=true"
-end
-
-function make_or_edit_comment(comment)
-    prev_comments, _ = GitHub.comments(repo, pr; auth=auth)
-    idx = findfirst(x-> occursin("## Compile Times compared to tagged", x.body), prev_comments)
-    if isnothing(idx)
-        GitHub.create_comment(repo, pr; auth=auth, params=Dict("body"=>comment))
-    else
-        GitHub.edit_comment(repo, prev_comments[idx], :pr; auth=auth, params=Dict("body"=>comment))
-    end
-end
-
-function run_bench(n=10)
-    results = Tuple{Float64, Float64}[]
-    for i in 1:n
-        result = read(`julia --project=. ./benchmark-ttfp.jl`, String)
-        tup = eval(Meta.parse(result))
-        @show tup
-        push!(results, tup)
-    end
-    return results
-end
-
-results = run_bench()
-data_repo_path = "benchmarks/" * file_name * ".json"
-old_file = get_file(data_repo_path)
-data = if !isnothing(old_file.content)
-    JSON.parse(String(old_file.content))
-else
-    Dict{String, Vector{Tuple{Float64, Float64}}}()
-end
-
-data[commit] = results
-
-upload_data(JSON.json(data), data_repo_path)
-
-tagged_repo_path = "benchmarks/julia_$(VERSION)_tagged.json"
-file_tagged = get_file(tagged_repo_path)
-tagged = JSON.parse(String(file_tagged.content))
-
-
-function sorted_by_date(data, branch_name)
-    with_date = map(collect(data)) do (commit_sha, data)
-        gc = GitHub.gitcommit(repo, commit_sha)
-        time = parse(DateTime, gc.author["date"][1:end-1])
-        (commit_sha, data, time)
-    end
-    sort!(with_date, by=last)
-    return map(with_date) do (commit_sha, data, time)
-        (commit_sha, branch_name, last.(data))
-    end
-end
-
-function create_plot()
-    plot_data = sorted_by_date(data, branch_name)
-    last_d = max(length(plot_data) - 6, 1)
-    plot_data = plot_data[last_d:end] # only take last 6 commits
-    last_tagged = sorted_by_date(tagged, "tagged")[end]
-
-    pushfirst!(plot_data, last_tagged)
-
-    ys = Float64[]
-    xs = Float64[]
-    colors = RGBAf[]
-    xticks_labels = String[]
-    xticks_nums = Int[]
-    wcolors = Makie.wong_colors(1.0)
-    legend_elems = PolyElement[]
-    legend_names = String[]
-    color_lookup = Dict{String, RGBAf}()
-    c_idx = 0
-    for (i, (commit, branch, times)) in enumerate(plot_data)
-        n = length(times)
-        append!(xs, fill(i, n))
-        append!(ys, times)
-        plot_col = get!(color_lookup, branch) do
-            c_idx += 1
-            col = wcolors[c_idx]
-            push!(legend_elems, PolyElement(color=col))
-            push!(legend_names, branch)
-            return col
-        end
-        append!(colors, fill(plot_col, n))
-        push!(xticks_nums, i)
-        push!(xticks_labels, commit[1:5])
-    end
-
-    f = Figure()
-    ax = Axis(f[1,1], xticks=(xticks_nums, xticks_labels), xlabel="commit", ylabel="time (s)")
-    pl = violin!(ax, xs, ys, show_median=true, color=colors)
-    Legend(f[1, 1], legend_elems, legend_names;
-        tellheight = false,
-        tellwidth = false,
-        margin = (10, 10, 10, 10),
-        halign = :right, valign = :bottom)
-
-    io = IOBuffer()
-    show(io, MIME"image/png"(), f)
-    return take!(io)
-end
-
-using CairoMakie
-image_bytes = create_plot()
-
-image_url = upload_data(image_bytes, repo_path)
+name = join(map(x-> x[1:5], commits_to_bench), "-vs-")
+image_url = upload_data(ctx, fig, "benchmarks/$(name).png")
 
 comment = """
 ## Compile Times compared to tagged
@@ -147,4 +19,6 @@ comment = """
 ![]($(image_url))
 """
 
-make_or_edit_comment(comment)
+pr = GitHub.PullRequest(benchmarks[end]["pr"])
+
+make_or_edit_comment(ctx, pr, comment)
