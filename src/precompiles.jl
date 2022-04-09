807fb6477a80a92a2ab64a5a8004f2090987c103
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-04-09 13:16:29 +0200

    add precompilation back

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -1,9 +1,8 @@
 function _precompile_()
-    @warn "Precompilation disabled temporarily"
-    # ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    # f, ax1, pl = scatter(1:4)
-    # f, ax2, pl = lines(1:4)
-    # f = Figure()
-    # Axis(f[1,1])
+    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    f, ax1, pl = scatter(1:4)
+    f, ax2, pl = lines(1:4)
+    f = Figure()
+    Axis(f[1,1])
     return
 end
