095f4ec2d61077c369d9a562ae2c986335564cc1
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-04-10 20:15:42 +0200

    change slidergrid to take NamedTuples

--- src/makielayout/blocks/slidergrid.jl
+++ src/makielayout/blocks/slidergrid.jl
@@ -3,19 +3,25 @@ function block_docs(::Type{SliderGrid})
     A grid of horizontal `Slider`s, where each slider has one name label on the left,
     and a value label on the right.
 
+    Each `NamedTuple` you pass specifies one `Slider`. You always have to pass `range`
+    and `label`, and optionally a `format` for the value label. Beyond that, you can set
+    any keyword that `Slider` takes, such as `startvalue`.
+
     ## Constructors
 
     ```julia
-    SliderGrid(fig_or_scene, pairs::Pair...; kwargs...)
+    SliderGrid(fig_or_scene, nts::NamedTuple...; kwargs...)
     ```
 
     ## Examples
 
     ```julia
     sg = SliderGrid(fig[1, 1],
-        "Amplitude" => 0:10,
-        "Frequency" => 0:0.5:50 => "{:.1f}Hz",
-        "Phase" => 0:0.01:2pi => x -> string(round(x, digits = 4)))
+        (label = "Amplitude", range = 0:0.1:10, startvalue = 5),
+        (label = "Frequency", range = 0:0.5:50, format = "{:.1f}Hz", startvalue = 10),
+        (label = "Phase", range = 0:0.01:2pi,
+            format = x -> string(round(x/pi, digits = 2), "π"))
+    )
     ```
 
     Working with slider values:
@@ -28,7 +34,7 @@ function block_docs(::Type{SliderGrid})
     """
 end
 
-function initialize_block!(sg::SliderGrid, pairs::Pair...)
+function initialize_block!(sg::SliderGrid, nts::NamedTuple...)
 
     default_format(x) = string(x)
     default_format(x::AbstractFloat) = string(round(x, sigdigits = 3))
@@ -41,10 +47,13 @@ function initialize_block!(sg::SliderGrid, pairs::Pair...)
     extract_range_format(p::Pair) = (p...,)
     extract_range_format(x) = (x, default_format)
 
-    for (i, pair) in enumerate(pairs)
-        label, range, format = extract_label_range_format(pair)
+    for (i, nt) in enumerate(nts)
+        label = nt.label
+        range = nt.range
+        format = haskey(nt, :format) ? nt.format : default_format
+        remaining_pairs = filter(pair -> pair[1] ∉ (:label, :range, :format), pairs(nt))
         l = Label(sg.layout[i, 1], label, halign = :left)
-        slider = Slider(sg.layout[i, 2], range = range)
+        slider = Slider(sg.layout[i, 2]; range = range, remaining_pairs...)
         vl = Label(sg.layout[i, 3],
             lift(x -> apply_format(x, format), slider.value), halign = :right)
         push!(sg.valuelabels, vl)
