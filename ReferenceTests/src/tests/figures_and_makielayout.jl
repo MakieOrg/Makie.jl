118ed2653e4368d1a8eb6819a75fc274abd125eb
ffreyer <frederic481994@hotmail.de>
2022-04-15 12:30:34 +0200

    fix test

--- ReferenceTests/src/tests/figures_and_makielayout.jl
+++ ReferenceTests/src/tests/figures_and_makielayout.jl
@@ -26,6 +26,7 @@ end
 end
 
 @cell "Label with text wrapping" begin
+    lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     fig = Figure(resolution = (1000, 660))
 
     lbl1 = Label(fig[1, 1:2], "HEADER "^10, textsize = 40, word_wrap = true)
