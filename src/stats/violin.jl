bd7bafa973d6fe5633630fb5b7051cae7a6b46a8
Fabian Greimel <fabgrei@gmail.com>
2022-04-22 09:26:07 +0100

    Apply suggestions from code review
    Co-authored-by: Pietro Vertechi <pietro.vertechi@protonmail.com>

--- src/stats/violin.jl
+++ src/stats/violin.jl
@@ -5,7 +5,7 @@ Draw a violin plot.
 - `x`: positions of the categories
 - `y`: variables whose density is computed
 # Keywords
-- `weights`: vector of statistical weights (length of data). By default, each observation has weight 1.
+- `weights`: vector of statistical weights (length of data). By default, each observation has weight `1`.
 - `orientation=:vertical`: orientation of the violins (`:vertical` or `:horizontal`)
 - `width=1`: width of the box before shrinking
 - `gap=0.2`: shrinking factor, `width -> width * (1 - gap)`
