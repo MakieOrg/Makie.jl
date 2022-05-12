bd9a209a19fe841a0c0d2cb357150d586ece0870
Simon <sdanisch@protonmail.com>
2022-05-12 11:21:12 +0200

    [breaking] clean up conversion pipeline (#1810)
    * convert more orderly
    
    * fix problems
    
    * ok, we can't actually recursively convert
    
    * the need for recursion
    
    * fix conversions
    
    * geometry improvements
    
    * fix surface
    
    * fix surface
    
    * remove unused files
    
    * fix heatmap with Intervals
    
    * bring back nice errors, by introducing proper per plot convert targets
    
    * fix plot
    
    * fix tests
    
    * reset runtests
    
    * address review
    
    * resurrect Grid and address last review comment

--- CairoMakie/src/primitives.jl
+++ CairoMakie/src/primitives.jl
@@ -437,28 +437,6 @@ end
 ################################################################################
 
 """
-    regularly_spaced_array_to_range(arr)
-If possible, converts `arr` to a range.
-If not, returns array unchanged.
-"""
-function regularly_spaced_array_to_range(arr)
-    diffs = unique!(sort!(diff(arr)))
-    step = sum(diffs) ./ length(diffs)
-    if all(x-> x ≈ step, diffs)
-        m, M = extrema(arr)
-        if step < zero(step)
-            m, M = M, m
-        end
-        # don't use stop=M, since that may not include M
-        return range(m; step=step, length=length(arr))
-    else
-        return arr
-    end
-end
-
-regularly_spaced_array_to_range(arr::AbstractRange) = arr
-
-"""
     interpolation_flag(is_vector, interp, wpx, hpx, w, h)
 
 * is_vector: if we're using vector backend
@@ -495,14 +473,14 @@ function draw_atomic(scene::Scene, screen::CairoScreen, @nospecialize(primitive:
         N = size(image, 1)
         xs = range(l, r, length = N+1)
     else
-        xs = regularly_spaced_array_to_range(xs)
+        xs = Makie.regularly_spaced_array_to_range(xs)
     end
     if !(ys isa AbstractVector)
         l, r = extrema(ys)
         N = size(image, 2)
         ys = range(l, r, length = N+1)
     else
-        ys = regularly_spaced_array_to_range(ys)
+        ys = Makie.regularly_spaced_array_to_range(ys)
     end
     model = primitive[:model][]
     interp = to_value(get(primitive, :interpolate, true))
