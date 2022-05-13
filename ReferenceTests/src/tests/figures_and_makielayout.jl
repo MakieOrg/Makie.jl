8f03716f18b1a28768f61b7293bcf431674867fa
ffreyer <frederic481994@hotmail.de>
2022-05-13 14:19:01 +0200

    fix @cell

--- ReferenceTests/src/tests/figures_and_makielayout.jl
+++ ReferenceTests/src/tests/figures_and_makielayout.jl
@@ -25,7 +25,7 @@ end
     fig
 end
 
-@cell "Label with text wrapping" begin
+@reference_test "Label with text wrapping" begin
     lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     fig = Figure(resolution = (1000, 660))
 
