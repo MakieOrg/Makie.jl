2b4ccd3d078cfc9876af131a69c8a5b2b034022c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-05-19 10:03:37 +0200

    remove unnecessary imports from ReferenceTests

--- ReferenceUpdater/src/image_download.jl
+++ ReferenceUpdater/src/image_download.jl
@@ -1,6 +1,5 @@
-
 function upload_release(user, repo, token, tag, path)
-    ghr() do ghr_path
+    ghr_jll.ghr() do ghr_path
         run(`$ghr_path -replace -u $(user) -r $(repo) -t $(token) $(tag) $(path)`)
     end
 end
@@ -18,21 +17,4 @@ function upload_reference_images(path=basedir("recorded"), tag=last_major_versio
         Tar.create(path, tarfile)
         upload_release("JuliaPlots", "Makie.jl", ENV["GITHUB_TOKEN"], tag, tarfile)
     end
-end
-
-function download_refimages(tag=last_major_version(); name="refimages")
-    url = "https://github.com/JuliaPlots/Makie.jl/releases/download/$(tag)/$(name).tar"
-    images_tar = basedir("$(name).tar")
-    images = basedir(name)
-    if isfile(images_tar)
-        if Bool(parse(Int, get(ENV, "REUSE_IMAGES_TAR", "0")))
-            @info "$images_tar already exists, skipping download as requested"
-        else
-            rm(images_tar)
-        end
-    end
-    !isfile(images_tar) && download(url, images_tar)
-    isdir(images) && rm(images, recursive=true, force=true)
-    Tar.extract(images_tar, images)
-    return images
 end
