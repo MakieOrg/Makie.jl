286ab6b8a9aad5821cc4233f78be2684de083d92
SimonDanisch <sdanisch@gmail.com>
2022-06-03 17:04:01 +0200

    use benchmarktools to benchmark runtime

--- metrics/ttfp/benchmark-ttfp.jl
+++ metrics/ttfp/benchmark-ttfp.jl
@@ -1,4 +1,13 @@
 t_using = (tstart = time(); using CairoMakie; time() - tstart)
-t_plot = (tstart = time(); save("test.png", scatter(1:4; color=1:4, colormap=:turbo, markersize=20, visible=true)); time() - tstart)
+
+function test()
+    screen = display(scatter(1:4; color=1:4, colormap=:turbo, markersize=20, visible=true))
+    Makie.colorbuffer(screen)
+end
+t_plot = (tstart = time(); test(); time() - tstart)
+using BenchmarkTools
+
+b = @btime test()
+
 rm("test.png")
 println("($t_using, $t_plot)")
