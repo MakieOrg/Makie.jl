7d20f81ac72dc2da1c638bb469a13cb452febdd5
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-06-05 14:52:26 +0200

    reimplement hvlines, hvspan and ablines via recipes

---
+++ src/basic_recipes/ablines.jl
@@ -0,0 +1,51 @@
+"""
+    ABLines(intercepts, slopes; attrs...)
+
+Creates a line defined by `f(x) = slope * x + intercept` crossing a whole `Scene` with 2D projection at its current limits.
+You can pass one or multiple intercepts or slopes.
+All style attributes are the same as for `LineSegments`.
+"""
+@recipe(ABLines) do scene
+    Theme(;
+        xautolimits = false,
+        yautolimits = false,
+        default_theme(LineSegments, scene)...,
+        cycle = :color,
+    )
+end
+
+function Makie.plot!(p::ABLines)
+    scene = Makie.parent_scene(p)
+    transf = transform_func(scene)
+
+    
+
+    is_identity_transform(transf) || throw(ArgumentError("ABLines is only defined for the identity transform, not $(typeof(transf))."))
+
+    limits = lift(projview_to_2d_limits, scene.camera.projectionview)
+
+    points = Observable(Point2f[])
+    
+    onany(limits, p[1], p[2]) do lims, intercept, slope
+        inv = inverse_transform(transf)
+        empty!(points[])
+        f(x) = x * b + a
+        broadcast_foreach(intercept, slope) do intercept, slope
+            f(x) = intercept + slope * x
+            xmin, xmax = first.(extrema(lims))
+            push!(points[], Point2f(xmin, f(xmin)))
+            push!(points[], Point2f(xmax, f(xmax)))
+        end
+        notify(points)
+    end
+
+    notify(p[1])
+
+    linesegments!(p, points; p.attributes...)
+    p
+end
+
+function abline!(args...; kwargs...)
+    Base.depwarn("abline! is deprecated and will be removed in the future. Use ablines / ablines! instead." , :abline!, force = true)
+    ablines!(args...; kwargs...)
+end
---
+++ src/basic_recipes/hvspan.jl
@@ -0,0 +1,82 @@
+"""
+    hspan(ys_low, ys_high; xmin = 0.0, xmax = 1.0, attrs...)
+
+Create horizontal bands spanning across a `Scene` with 2D projection.
+The bands will be placed from `ys_low` to `ys_high` in data coordinates and `xmin` to `xmax`
+in scene coordinates (0 to 1). All four of these can have single or multiple values because
+they are broadcast to calculate the final spans.
+"""
+@recipe(HSpan) do scene
+    Theme(;
+        xautolimits = false,
+        xmin = 0,
+        xmax = 1,
+        default_theme(Poly, scene)...,
+        cycle = [:color => :patchcolor],
+    )
+    end
+
+"""
+    vspan(xs_low, xs_high; ymin = 0.0, ymax = 1.0, attrs...)
+
+Create vertical bands spanning across a `Scene` with 2D projection.
+The bands will be placed from `xs_low` to `xs_high` in data coordinates and `ymin` to `ymax`
+in scene coordinates (0 to 1). All four of these can have single or multiple values because
+they are broadcast to calculate the final spans.
+"""
+@recipe(VSpan) do scene
+    Theme(;
+        yautolimits = false,
+        ymin = 0,
+        ymax = 1,
+        default_theme(Poly, scene)...,
+        cycle = [:color => :patchcolor],
+    )
+end
+
+function Makie.plot!(p::Union{HSpan, VSpan})
+    scene = Makie.parent_scene(p)
+    transf = transform_func_obs(scene)
+
+    limits = lift(projview_to_2d_limits, scene.camera.projectionview)
+
+    rects = Observable(Rect2f[])
+
+    mi = p isa HSpan ? p.xmin : p.ymin
+    ma = p isa HSpan ? p.xmax : p.ymax
+    
+    onany(limits, p[1], p[2], mi, ma, transf) do lims, lows, highs, mi, ma, transf
+        inv = inverse_transform(transf)
+        empty!(rects[])
+        min_x, min_y = minimum(lims)
+        max_x, max_y = maximum(lims)
+        broadcast_foreach(lows, highs, mi, ma) do low, high, mi, ma
+            if p isa HSpan
+                x_mi = min_x + (max_x - min_x) * mi
+                x_ma = min_x + (max_x - min_x) * ma
+                x_mi = _apply_x_transform(inv, x_mi)
+                x_ma = _apply_x_transform(inv, x_ma)
+                push!(rects[], Rect2f(Point2f(x_mi, low), Vec2f(x_ma - x_mi, high - low)))
+            elseif p isa VSpan
+                y_mi = min_y + (max_y - min_y) * mi
+                y_ma = min_y + (max_y - min_y) * ma
+                y_mi = _apply_y_transform(inv, y_mi)
+                y_ma = _apply_y_transform(inv, y_ma)
+                push!(rects[], Rect2f(Point2f(low, y_mi), Vec2f(high - low, y_ma - y_mi)))
+            end
+        end
+        notify(rects)
+    end
+
+    notify(p[1])
+
+    poly!(p, rects; p.attributes...)
+    p
+end
+
+_apply_x_transform(t::Tuple, v) = apply_transform(t[1], v)
+_apply_x_transform(::typeof(identity), v) = v
+_apply_x_transform(other, v) = error("x transform not defined for transform function $(typeof(other))")
+_apply_y_transform(t::Tuple, v) = apply_transform(t[2], v)
+_apply_y_transform(other, v) = error("y transform not defined for transform function $(typeof(other))")
+_apply_y_transform(::typeof(identity), v) = v
