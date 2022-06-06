85c85475df09dbc176e4ad03acd6144fac1e7c4f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-06-06 13:45:34 +0200

    capitalization

--- src/basic_recipes/ablines.jl
+++ src/basic_recipes/ablines.jl
@@ -1,5 +1,5 @@
 """
-    ABLines(intercepts, slopes; attrs...)
+    ablines(intercepts, slopes; attrs...)
 
 Creates a line defined by `f(x) = slope * x + intercept` crossing a whole `Scene` with 2D projection at its current limits.
 You can pass one or multiple intercepts or slopes.
