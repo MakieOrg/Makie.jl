f2d4720b64bcc7d0221ef1f4acdd6f2daa782b48
Anshul Singhvi <anshulsinghvi@gmail.com>
2022-06-08 23:28:08 +0200

    First shot at GLMakie plots in CairoMakie

---
+++ CairoMakie/src/glmakie_integration.jl
@@ -0,0 +1,106 @@
+
+function gl_alpha_colorbuffer(scene::Scene)
+    GLMakie.activate!()
+    # Makie.inline!(true)
+    bg = scene.backgroundcolor[]
+    scene.backgroundcolor[] = RGBf(0, 0, 0)
+    screen = display(scene)
+    println("Screen")
+    @show size(scene) GeometryBasics.widths(screen)
+    b1 = Makie.colorbuffer(screen)
+    # b1, screen1 = GLMakie.scene2image(scene)
+    save("hi-white.png", b1)
+    b1 = deepcopy(b1)
+    println("White")
+    @show size(scene) GeometryBasics.widths(screen) size(b1)
+    scene.backgroundcolor[] = RGBf(1, 1, 1)
+    screen = display(scene)
+    b2 = Makie.colorbuffer(screen)
+    # b2, screen2 = GLMakie.scene2image(scene)
+    save("hi-black.png", b2)
+    println("Black")
+    @show size(scene) GeometryBasics.widths(screen) size(b2)
+    scene.backgroundcolor[] = bg
+    CairoMakie.activate!()
+    # Makie.inline!(!Makie.use_display[])
+    c= infer_alphacolor.(b1, b2)
+    @show typeof(c)
+    c
+end
+
+function alpha_colorbuffer(scene)
+    GLMakie.activate!()
+    display(scene)
+    bg = scene.backgroundcolor[]
+    scene.backgroundcolor[] = RGBAf(0, 0, 0, 1)
+    b1 = copy(Makie.colorbuffer(scene))
+    FileIO.save("hi-white.png", b1)
+    scene.backgroundcolor[] = RGBAf(1, 1, 1, 1)
+    b2 = Makie.colorbuffer(scene)
+    FileIO.save("hi-black.png", b2)
+    CairoMakie.activate!()
+    scene.backgroundcolor[] = bg
+    return map(infer_alphacolor, b1, b2)
+end
+
+
+function infer_alphacolor(rgb1, rgb2)
+    rgb1 == rgb2 && return RGBAf(rgb1.r, rgb1.g, rgb1.b, 1)
+    c1 = Float64.((rgb1.r, rgb1.g, rgb1.b))
+    c2 = Float64.((rgb2.r, rgb2.g, rgb2.b))
+    alpha = @. 1 - (c1 - c2) * -1 # ( / (0 - 1))
+    meanalpha = clamp(sum(alpha) / 3, 0, 1)
+    meanalpha == 0 && return RGBAf(0, 0, 0, 0)
+    c = @. clamp((c1 / meanalpha), 0, 1)
+    return RGBAf(c..., meanalpha)
+end
+
+function plot2img(plot::Combined, scale::Real = 1; use_backgroundcolor = false)
+    scene = Makie.parent_scene(plot)
+    w, h = Int.(scene.px_area[].widths)
+
+    # We create a dummy scene to render to, which inherits its parent's
+    # transformation and camera
+
+    render_scene = Makie.Scene(camera = Makie.camera(scene), lights = scene.lights, ssao = scene.ssao, show_axis = false, backgroundcolor = :transparent,)
+    Makie.resize!(render_scene, (pixelarea(scene)[].widths .* scale)...)
+    # copy over the transofrmation and camera attributes
+    render_scene.transformation.transform_func[] = scene.transformation.transform_func[]
+    render_scene.transformation.rotation[]       = scene.transformation.rotation[]
+    render_scene.transformation.scale[]          = scene.transformation.scale[]
+    render_scene.transformation.translation[]    = scene.transformation.translation[]
+
+    # push only the relevant plot to the scene
+    push!(render_scene, plot)
+
+    img = if use_backgroundcolor
+        Makie.colorbuffer(render_scene)
+    else # render with transparency, using the alpha-colorbuffer hack
+        alpha_colorbuffer(render_scene)
+    end
+
+    return img
+end
+
+function CairoMakie.draw_atomic(scene::Scene, screen::CairoMakie.CairoScreen, primitive::Volume, scale::Real = 10)
+
+    w, h = Int.(scene.px_area[].widths)
+
+    img = plot2img(primitive, scale; use_backgroundcolor = false)
+    Makie.save("hi.png", img) # cache for debugging
+
+    surf = Cairo.CairoARGBSurface(CairoMakie.to_uint32_color.(img))
+
+    Cairo.rectangle(screen.context, 0, 0, w, h)
+    Cairo.save(screen.context)
+    Cairo.scale(screen.context, w / surf.width, h / surf.height)
+    Cairo.set_source_surface(screen.context, surf, 0, 0)
+    p = Cairo.get_source(screen.context)
+    Cairo.pattern_set_extend(p, Cairo.EXTEND_PAD) # avoid blurry edges
+    Cairo.pattern_set_filter(p, Cairo.FILTER_NEAREST)
+    Cairo.fill(screen.context)
+    Cairo.restore(screen.context)
+
+    return
+
+end
