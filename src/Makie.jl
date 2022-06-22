011c590e976d4a03e076bf0abd32698b73b0da67
SimonDanisch <sdanisch@protonmail.com>
2022-06-22 17:32:30 +0200

    move empty!(FONT_CACHE)

--- src/Makie.jl
+++ src/Makie.jl
@@ -297,8 +297,7 @@ function __init__()
             GridLayoutBase.DEFAULT_ROWGAP[]
         end
     end
-    # fonts aren't cacheable by precompilation, so we need to empty it on load!
-    empty!(FONT_CACHE)
+
     cfg_path = joinpath(homedir(), ".config", "makie", "theme.jl")
     if isfile(cfg_path)
         @warn "The global configuration file is no longer supported." *
