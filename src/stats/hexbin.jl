c27af0e059473838337ea13ac864c889c4b2c708
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-08-25 12:32:53 +0200

    run formatter

--- src/stats/hexbin.jl
+++ src/stats/hexbin.jl
@@ -13,95 +13,95 @@ Plots a heatmap with hexagonal bins for the observations `xs` and `ys`.
 * `colorrange::Tuple(<:Real,<:Real} = Makie.automatic`  sets the values representing the start and end points of `colormap`.
 """
 @recipe(Hexbin) do scene
-    Attributes(;
-    colormap = theme(scene, :colormap),
-    colorrange=Makie.automatic,
-    gridsize = 20,
-    mincnt = 0,
-    scale=identity)
+    return Attributes(;
+        colormap=theme(scene, :colormap),
+        colorrange=Makie.automatic,
+        gridsize=20,
+        mincnt=0,
+        scale=identity
+    )
 end
 
-function Makie.plot!(hb::Hexbin{<:Tuple{<:AbstractVector{<:Any}, <:AbstractVector{<:Any}}})
-    x,y =hb[1:2]
-   
+function Makie.plot!(hb::Hexbin{<:Tuple{<:AbstractVector{<:Any},<:AbstractVector{<:Any}}})
+    x, y = hb[1:2]
+
     polys = Observable(Polygon[])
-    count_hex=Observable(Float64[])
-    function calculate_grid(x,y,grid_size,mincnt,scale)
+    count_hex = Observable(Float64[])
+    function calculate_grid(x, y, grid_size, mincnt, scale)
         empty!(polys[])
         empty!(count_hex[])
 
         any(isempty, (x, y)) && return
 
-        x_diff = maximum(x)-minimum(x)
-        y_diff = maximum(y)-minimum(y)
-    
+        x_diff = maximum(x) - minimum(x)
+        y_diff = maximum(y) - minimum(y)
+
         scaling_x = 1
         scaling_y = 1
-        if x_diff>y_diff
-            scaling_y =x_diff/y_diff
+        if x_diff > y_diff
+            scaling_y = x_diff / y_diff
         else
-            scaling_x = y_diff/x_diff
+            scaling_x = y_diff / x_diff
         end
-        y_copy = y.*scaling_y
-        x_copy = x.*scaling_x
-        
+        y_copy = y .* scaling_y
+        x_copy = x .* scaling_x
+
         min_x, max_x = extrema(x_copy)
         min_y, max_y = extrema(y_copy)
 
-        r = (max_x-min_x)/((grid_size-2)*2) 
-        x_odd_grid =min_x-r:sin(pi/3)*2*r:max_x-r
-        x_even_grid = min_x+sin(pi/3)*r:sin(pi/3)*2*r:max_x-r
-        y_odd_grid = min_y:3*r:max_y
-        y_even_grid = min_y+3/2*r:3*r:max_y-3/2*r
+        r = (max_x - min_x) / ((grid_size - 2) * 2)
+        x_odd_grid = (min_x - r):(sin(pi / 3) * 2 * r):(max_x - r)
+        x_even_grid = (min_x + sin(pi / 3) * r):(sin(pi / 3) * 2 * r):(max_x - r)
+        y_odd_grid = min_y:(3 * r):max_y
+        y_even_grid = (min_y + 3 / 2 * r):(3 * r):(max_y - 3 / 2 * r)
         grid_points = Point2f[]
         for i in x_odd_grid
             for j in y_odd_grid
-                push!(grid_points,Point2f(i+r,j))
+                push!(grid_points, Point2f(i + r, j))
             end
         end
         for i in x_even_grid
             for j in y_even_grid
-                push!(grid_points,Point2f(i,j))
+                push!(grid_points, Point2f(i, j))
             end
         end
-        values = Point2f.(x_copy,y_copy)
+        values = Point2f.(x_copy, y_copy)
         tree = KDTree(grid_points)
-        ind, dist = nn(tree,values)
+        ind, dist = nn(tree, values)
         amount_hex = zeros(size(grid_points))
         for i in ind
-            amount_hex[i]+=1
+            amount_hex[i] += 1
         end
-        grid_points=grid_points[amount_hex.>=mincnt]
-        amount_hex=amount_hex[amount_hex.>=mincnt]      
+        grid_points = grid_points[amount_hex .>= mincnt]
+        amount_hex = amount_hex[amount_hex .>= mincnt]
         amount_hex = scale.(amount_hex)
 
-        xe = sin.(0:pi/3:5/3*pi)
-        ys = cos.(0:pi/3:5/3*pi)
-        hex_points=Point2f[]
-        for (a,b) in zip(xe,ys)
-            push!(hex_points, Point2f(a*r*1/scaling_x,b*r*1/scaling_y))
+        xe = sin.(0:(pi / 3):(5 / 3 * pi))
+        ys = cos.(0:(pi / 3):(5 / 3 * pi))
+        hex_points = Point2f[]
+        for (a, b) in zip(xe, ys)
+            push!(hex_points, Point2f(a * r * 1 / scaling_x, b * r * 1 / scaling_y))
         end
-        hex = [Polygon(hex_points.+p*Point2f(1/scaling_x,1/scaling_y)) for p in grid_points]
+        hex = [Polygon(hex_points .+ p * Point2f(1 / scaling_x, 1 / scaling_y)) for p in grid_points]
 
         for b in hex
-            push!(polys[],b)
+            push!(polys[], b)
         end
         for a in amount_hex
-            push!(count_hex[],a)
+            push!(count_hex[], a)
         end
-
     end
-    onany(calculate_grid,x,y,hb.gridsize, hb.mincnt,hb.scale)
-    calculate_grid(x[],y[],hb.gridsize[],hb.mincnt[],hb.scale[])
+    onany(calculate_grid, x, y, hb.gridsize, hb.mincnt, hb.scale)
+    calculate_grid(x[], y[], hb.gridsize[], hb.mincnt[], hb.scale[])
 
-    replace_automatic!(hb,:colorrange)do 
+    replace_automatic!(hb, :colorrange) do
         if isempty(count_hex[])
-            (0,1)
+            (0, 1)
         else
-            (minimum(count_hex[]),maximum(count_hex[]) )
+            (minimum(count_hex[]), maximum(count_hex[]))
         end
-    end 
-    if ! isempty(polys[])
-        poly!(hb,polys,color=count_hex,colormap=hb.colormap)
+    end
+    if !isempty(polys[])
+        poly!(hb, polys; color=count_hex, colormap=hb.colormap)
     end
 end
