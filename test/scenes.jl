0ecbf4e369752c71e85d11184f5c7c34ed3efbd5
SimonDanisch <sdanisch@protonmail.com>
2022-09-20 13:59:35 +0200

    test getproperty

--- test/scenes.jl
+++ test/scenes.jl
@@ -1,4 +1,8 @@
 @testset "Scenes" begin
     scene = Scene()
     @test propertynames(scene) == fieldnames(Scene)
+    @testset "getproperty(scene, :$field)" for field in fieldnames(Scene)
+            @test getproperty(scene, field) !== missing # well, just don't error
+        end
+    end
 end
