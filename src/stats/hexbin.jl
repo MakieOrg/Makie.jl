d9ab338e99de63defbf2fb998c2c11c9d1388936
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-09-22 11:57:14 +0200

    fix alg

--- src/stats/hexbin.jl
+++ src/stats/hexbin.jl
@@ -16,104 +16,95 @@ Plots a heatmap with hexagonal bins for the observations `xs` and `ys`.
     return Attributes(;
         colormap=theme(scene, :colormap),
         colorrange=Makie.automatic,
-        gridsize=20,
-        mincnt=0,
+        bins=20,
+        mincnt=1,
         scale=identity
     )
 end
 
-function Makie.plot!(hb::Hexbin{<:Tuple{<:AbstractVector{<:Any},<:AbstractVector{<:Any}}})
-    x, y = hb[1:2]
+function Makie.plot!(hb::Hexbin{<:Tuple{<:AbstractVector{<:Point2}}})
+    xy = hb[1]
 
     points = Observable(Point2f[])
     count_hex = Observable(Float64[])
-    markersize = Observable(1.0)
+    markersize = Observable(Vec2f(1, 1))
 
-    function calculate_grid(x, y, grid_size, mincnt, scale)
+    function calculate_grid(xy, bins, mincnt, scale)
         empty!(points[])
         empty!(count_hex[])
 
-        any(isempty, (x, y)) && return
+        isempty(xy) && return
 
-        x_diff = maximum(x) - minimum(x)
-        y_diff = maximum(y) - minimum(y)
+        sqrt3 = sqrt(3)
 
-        scaling_x = 1
-        scaling_y = 1
-        if x_diff > y_diff
-            scaling_y = x_diff / y_diff
-        else
-            scaling_x = y_diff / x_diff
+        # enclose data in limits
+        _expand((lo, hi)) = prevfloat(lo), nextfloat(hi)
+
+        xmi, xma = _expand(extrema((p[1] for p in xy)))
+        ymi, yma = _expand(extrema((p[2] for p in xy)))
+
+        x_diff = xma - xmi
+        y_diff = yma - ymi
+
+        xspacing = x_diff / bins
+        yspacing = y_diff / bins
+
+        ysize = yspacing / 3 * 4
+        ry = ysize / 2
+
+        xsize = xspacing * 2
+        rx = xsize / sqrt3
+
+        d = Dict{Tuple{Int, Int, Bool}, Int}()
+
+        # for the distance measurement, the y dimension must be weighted relative to the x
+        # dimension according to the different sizes in each, otherwise the attribution to hexagonal
+        # cells is wrong
+        yweight = xsize / ysize
+
+        for (_x, _y) in xy
+            nx, nxs, dvx = nearest_center(_x, 2 * xspacing, xmi)
+            ny, nys, dvy = nearest_center(_y, 2 * yspacing, ymi)
+
+            d1 = ((_x - nx) ^ 2 + (yweight * (_y - ny)) ^ 2)
+            d2 = ((_x - nxs) ^ 2 + (yweight * (_y - nys)) ^ 2)
+
+            is_grid1 = d1 < d2
+
+            id = (dvx, dvy, is_grid1)
+
+            d[id] = get(d, id, 0) + 1
         end
-        y_copy = y .* scaling_y
-        x_copy = x .* scaling_x
 
-        min_x, max_x = extrema(x_copy)
-        min_y, max_y = extrema(y_copy)
-
-        r = (max_x - min_x) / ((grid_size - 2) * 2)
-        xdist = sin(pi / 3) * 2 * r
-        # x_odd_grid = (min_x - r):(sin(pi / 3) * 2 * r):(max_x - r)
-        # x_even_grid = (min_x + sin(pi / 3) * r):(sin(pi / 3) * 2 * r):(max_x - r)
-        # y_odd_grid = min_y:(3 * r):max_y
-        # y_even_grid = (min_y + 3 / 2 * r):(3 * r):(max_y - 3 / 2 * r)
-        # grid_points = Point2f[]
-        # for i in x_odd_grid
-        #     for j in y_odd_grid
-        #         push!(grid_points, Point2f(i + r, j))
-        #     end
-        # end
-        # for i in x_even_grid
-        #     for j in y_even_grid
-        #         push!(grid_points, Point2f(i, j))
-        #     end
-        # end
-        # values = Point2f.(x_copy, y_copy)
-        # tree = KDTree(grid_points)
-        # ind, dist = nn(tree, values)
-        # amount_hex = zeros(size(grid_points))
-        # for i in ind
-        #     amount_hex[i] += 1
-        # end
-        # grid_points = grid_points[amount_hex .>= mincnt]
-        # amount_hex = amount_hex[amount_hex .>= mincnt]
-        # amount_hex = scale.(amount_hex)
-
-        d = Dict{Point2f, Int}()
-
-        for (_x, _y) in zip(x, y)
-            nx, nxs = nearest_center(_x, xdist)
-            ny, nys = nearest_center(_y, xdist * sqrt(3))
-
-            d1 = ((_x - nx) ^ 2 + (_y - ny) ^ 2)
-            d2 = ((_x - nxs) ^ 2 + (_y - nys) ^ 2)
-
-            bin = if d1 > d2
-                Point2f(nxs, nys)
-            else
-                Point2f(nx, ny)
+        for ix in 0:bins-1
+            for iy in 0:bins-1
+                for is_grid1 in (true, false)
+                    _x = center_value(ix, 2 * xspacing, xmi, is_grid1)
+                    _y = center_value(iy, 2 * yspacing, ymi, is_grid1)
+                    c = get(d, (ix, iy, is_grid1), 0)
+                    if c >= mincnt
+                        push!(points[], Point2f(_x, _y))
+                        push!(count_hex[], c)
+                    end
+                end
             end
-
-            d[bin] = get(d, bin, 0) + 1
         end
 
-        for (p, c) in pairs(d)
-            push!(points[], p)
-            push!(count_hex[], c)
-        end
+        @show sum(last, pairs(d))
 
-        markersize[] = r
+        markersize[] = Vec2f(rx, ry)
         notify(points)
         notify(count_hex)
     end
-    onany(calculate_grid, x, y, hb.gridsize, hb.mincnt, hb.scale)
-    calculate_grid(x[], y[], hb.gridsize[], hb.mincnt[], hb.scale[])
+    onany(calculate_grid, xy, hb.bins, hb.mincnt, hb.scale)
+    # trigger once
+    notify(hb.bins)
 
     replace_automatic!(hb, :colorrange) do
         if isempty(count_hex[])
             (0, 1)
         else
-            (minimum(count_hex[]), maximum(count_hex[]))
+            @show (minimum(count_hex[]), maximum(count_hex[]))
         end
     end
 
@@ -122,9 +113,17 @@ function Makie.plot!(hb::Hexbin{<:Tuple{<:AbstractVector{<:Any},<:AbstractVector
     scatter!(hb, points; color=count_hex, colormap=hb.colormap, marker = hexmarker, markersize = markersize, markerspace = :data)
 end
 
-function nearest_center(val, scale)
-    dv = Int(fld(val, scale / 2))
-    rounded = scale / 2 * (dv + (isodd(dv) ? 1 : 0))
-    rounded_scaled = scale / 2 * (dv + (iseven(dv) ? 1 : 0))
-    return (rounded, rounded_scaled)
+function center_value(dv, scale, offset, is_grid1)
+    if is_grid1
+        offset + scale / 2 * (dv + (isodd(dv) ? 1 : 0))
+    else
+        offset + scale / 2 * (dv + (iseven(dv) ? 1 : 0))
+    end
+end
+
+function nearest_center(val, scale, offset)
+    dv = Int(fld(val - offset, scale / 2))
+    rounded = offset + scale / 2 * (dv + isodd(dv))
+    rounded_scaled = offset + scale / 2 * (dv + iseven(dv))
+    return rounded, rounded_scaled, dv
 end
