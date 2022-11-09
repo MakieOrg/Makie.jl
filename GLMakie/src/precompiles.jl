8ff595cfb7e91912493f57abef8540fe18bee937
SimonDanisch <sdanisch@protonmail.com>
2022-11-09 15:09:43 +0100

    clean up compile

--- GLMakie/src/precompiles.jl
+++ GLMakie/src/precompiles.jl
@@ -4,7 +4,6 @@ macro compile(block)
     return quote
         let
             figlike = $(esc(block))
-            Makie.display(figlike; visible=false)
             Makie.colorbuffer(figlike)
         end
     end
@@ -13,6 +12,9 @@ end
 let
     @precompile_all_calls begin
         GLMakie.activate!()
+        screen = GLMakie.singleton_screen(false)
+        close(screen)
+        destroy!(screen)
         base_path = normpath(joinpath(dirname(pathof(Makie)), "..", "precompile"))
         shared_precompile = joinpath(base_path, "shared-precompile.jl")
         include(shared_precompile)
