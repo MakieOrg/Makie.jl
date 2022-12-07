6de41c651359338b4bc622ea6322bc31429a453d
SimonDanisch <sdanisch@protonmail.com>
2022-12-07 18:47:14 +0100

    try to get docs to work

--- docs/makedocs.jl
+++ docs/makedocs.jl
@@ -2,7 +2,7 @@ using Pkg
 cd(@__DIR__)
 Pkg.activate(".")
 pkg"dev .. ../MakieCore ../CairoMakie ../GLMakie ../WGLMakie ../RPRMakie"
-pkg"add MeshIO GeometryBasics"
+pkg"add MeshIO GeometryBasics JSServe#sd/improvements"
 Pkg.instantiate()
 Pkg.precompile()
 
