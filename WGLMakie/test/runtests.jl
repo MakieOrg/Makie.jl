d571ae929ca7badf925cd67a7a4f32d62a3ea3be
ffreyer <frederic481994@hotmail.de>
2022-12-31 18:55:57 +0100

    exclude linecap test from WGLMakie

--- WGLMakie/test/runtests.jl
+++ WGLMakie/test/runtests.jl
@@ -52,7 +52,8 @@ excludes = Set([
     "Array of Images Scatter",
     "Image Scatter different sizes",
     "scatter with stroke",
-    "scatter with glow"
+    "scatter with glow",
+    "lines with caps"
 ])
 
 @testset "refimages" begin
