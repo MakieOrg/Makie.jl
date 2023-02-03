494d229397a173afc976a2f81a5687bf1a0ba056
SimonDanisch <sdanisch@protonmail.com>
2023-02-03 19:07:23 +0100

    try new JSServe

--- metrics/ttfp/run-benchmark.jl
+++ metrics/ttfp/run-benchmark.jl
@@ -207,7 +207,7 @@ ENV["JULIA_PKG_PRECOMPILE_AUTO"] = 0
 project1 = make_project_folder("current-pr")
 Pkg.activate(project1)
 if Package == "WGLMakie"
-    Pkg.add([(; name="Electron"), (; name="JSServe")])
+    Pkg.add([(; name="Electron"), (; name="MsgPack", rev="sd/perfi"), (; name="JSServe", rev="sd/dashi")])
 end
 pkgs = NamedTuple[(; path="./MakieCore"), (; path="."), (; path="./$Package"), (;name="BenchmarkTools")]
 # cd("dev/Makie")
