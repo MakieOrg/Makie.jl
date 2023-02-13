12edbd1296a06ae13410e376f272a04101ad7f5c
Eddie Groshev <eddie_g_89@hotmail.com>
2023-02-12 18:28:26 -0800

    fix RectangleZoom selection vertices

--- src/makielayout/interactions.jl
+++ src/makielayout/interactions.jl
@@ -122,11 +122,12 @@ function _chosen_limits(rz, ax)
     return r
 end
 
-function _selection_vertices(ax_scene, outer, inner)
+function _selection_vertices(ax, outer, inner)
     _clamp(p, plow, phigh) = Point2f(clamp(p[1], plow[1], phigh[1]), clamp(p[2], plow[2], phigh[2]))
-    proj(point) = project(ax_scene, point) .+ minimum(ax_scene.px_area[])
-    outer = positivize(outer)
-    inner = positivize(inner)
+    proj(point) = project(ax.scene, point) .+ minimum(ax.scene.px_area[])
+    transf = Makie.transform_func(ax)
+    outer = positivize(Makie.apply_transform(transf, outer))
+    inner = positivize(Makie.apply_transform(transf, inner))
 
     obl = bottomleft(outer)
     obr = bottomright(outer)
