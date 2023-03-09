674e129182da4ed3bec101fb8ee9682dcb75b47f
Capri-Corn <cornelius.miller@stud.uni-hannover.de>
2023-03-09 16:04:28 +0100

    Wrong files

--- t.jl
+++
@@ -1,8 +0,0 @@
-f = Figure(resolution = (800, 600))
-ax = Axis(f[1, 1])
-x = range(0, 10, length=100)
-y = sin.(x)
-lines!(ax, x, y)
-limits!(ax,0,10,-1,1 )
-f
-save("test_fig.png", f, px_per_unit = 2)
