41b52b2e2ed314a5042c5cacd00f26d6a68c3f1d
KronosTheLate <61620837+KronosTheLate@users.noreply.github.com>
2023-03-10 16:26:37 +0100

    Update src/makielayout/blocks/axis.jl
    Co-authored-by: Anshul Singhvi <anshulsinghvi@gmail.com>

--- src/makielayout/blocks/axis.jl
+++ src/makielayout/blocks/axis.jl
@@ -918,7 +918,6 @@ end
 
 """
     autolimits!(la::Axis)
-    autolimits!()
 
 Reset manually specified limits of `la` to an automatically determined rectangle, that depends on the 
 data limits of all plot objects in the axis, as well as the autolimit margins for x and y axis.
