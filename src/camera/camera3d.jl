11313dc211608cc8a6463d56557be5dbbb0d033e
ffreyer <frederic481994@hotmail.de>
2023-03-13 00:03:05 +0100

    cleanup docstring

--- src/camera/camera3d.jl
+++ src/camera/camera3d.jl
@@ -8,7 +8,6 @@ struct Camera3D <: AbstractCamera3D
     # Interactivity
     pulser::Observable{Float64}
     selected::Observable{Bool}
-    # scroll_mod::Observable{Bool}
 
 	# view matrix
     eyeposition::Observable{Vec3f}
@@ -22,24 +21,37 @@ struct Camera3D <: AbstractCamera3D
 end
 
 """
-    Camera3D(scene[; attributes...])
+    Camera3D(scene[; kwargs...])
 
-Creates a 3d camera with a lot of controls.
+Sets up a 3D camera with mouse and keyboard controls.
 
-The 3D camera is (or can be) unrestricted in terms of rotations and translations. Both `cam3d!(scene)` and `cam3d_cad!(scene)` create this camera type. Unlike the 2D camera, settings and controls are stored in the `cam.attributes` field rather than in the struct directly, but can still be passed as keyword arguments. The general camera settings include
+The behavior of the camera can be adjusted via keyword arguments or the fields
+`settings` and `controls`.
 
-- `fov = 45f0` sets the "neutral" field of view, i.e. the fov corresponding to no zoom. This is irrelevant if the camera uses an orthographic projection.
-- `near = automatic` sets the value of the near clip. By default this will be chosen based on the scenes bounding box. The final value is in `cam.near`.
-- `far = automatic` sets the value of the far clip. By default this will be chosen based on the scenes bounding box. The final value is in `cam.far`.
-- `rotation_center = :lookat` sets the default center for camera rotations. Currently allows `:lookat` or `:eyeposition`.
+## Settings
+
+Settings include anything that isn't a mouse or keyboard button.
+
 - `projectiontype = Perspective` sets the type of the projection. Can be `Orthographic` or `Perspective`.
+- `rotation_center = :lookat` sets the default center for camera rotations. Currently allows `:lookat` or `:eyeposition`.
 - `fixed_axis = false`: If true panning uses the (world/plot) z-axis instead of the camera up direction.
 - `zoom_shift_lookat = true`: If true keeps the data under the cursor when zooming. Only applies to orthographic cameras.
 - `cad = false`: If true rotates the view around `lookat` when zooming off-center.
 
-The camera view follows from the position of the camera `eyeposition`, the point which the camera focuses `lookat` and the up direction of the camera `upvector`. These can be accessed as `cam.eyeposition` etc and adjusted via `update_cam!(scene, cameracontrols(scene), eyeposition, lookat[, upvector = Vec3f(0, 0, 1)])`. They can also be passed as keyword arguments when the camera is constructed.
+- `keyboard_rotationspeed = 1f0` sets the speed of keyboard based rotations.
+- `keyboard_translationspeed = 0.5f0` sets the speed of keyboard based translations.
+- `keyboard_zoomspeed = 1f0` sets the speed of keyboard based zooms.
 
-The camera can be controlled by keyboard and mouse. The keyboard has the following available attributes
+- `mouse_rotationspeed = 1f0` sets the speed of mouse rotations.
+- `mouse_translationspeed = 0.5f0` sets the speed of mouse translations.
+- `mouse_zoomspeed = 1f0` sets the speed of mouse zooming (mousewheel).
+
+- `update_rate = 1/30` sets the rate at which keyboard based camera updates are evaluated.
+- `circular_rotation = (true, true, true)` enables circular rotations for (fixed x, fixed y, fixed z) rotation axis. (This means drawing a circle with your mouse around the center of the scene will result in a continuous rotation.)
+
+## Controls
+
+Controls include any kind of hotkey setting.
 
 - `up_key   = Keyboard.r` sets the key for translations towards the top of the screen.
 - `down_key = Keyboard.f` sets the key for translations towards the bottom of the screen.
@@ -50,8 +62,6 @@ The camera can be controlled by keyboard and mouse. The keyboard has the followi
 
 - `zoom_in_key   = Keyboard.u` sets the key for zooming into the scene (enlarge, via fov).
 - `zoom_out_key  = Keyboard.o` sets the key for zooming out of the scene (shrink, via fov).
-- `stretch_view_key  = Keyboard.page_up` sets the key for moving `eyepostion` away from `lookat`.
-- `contract_view_key = Keyboard.page_down` sets the key for moving `eyeposition` towards `lookat`.
 
 - `pan_left_key  = Keyboard.j` sets the key for rotations around the screens vertical axis.
 - `pan_right_key = Keyboard.l` sets the key for rotations around the screens vertical axis.
@@ -60,30 +70,32 @@ The camera can be controlled by keyboard and mouse. The keyboard has the followi
 - `roll_clockwise_key        = Keyboard.e` sets the key for rotations of the screen.
 - `roll_counterclockwise_key = Keyboard.q` sets the key for rotations of the screen.
 
-- `keyboard_rotationspeed = 1f0` sets the speed of keyboard based rotations.
-- `keyboard_translationspeed = 0.5f0` sets the speed of keyboard based translations.
-- `keyboard_zoomspeed = 1f0` sets the speed of keyboard based zooms.
-- `update_rate = 1/30` sets the rate at which keyboard based camera updates are evaluated.
-
-and mouse interactions are controlled by
-
-- `translation_button = Mouse.right` sets the mouse button for drag-translations. (up/down/left/right)
-- `scroll_mod = true` sets an additional modifier button for scroll-based zoom. (true being neutral)
-- `rotation_button = Mouse.left` sets the mouse button for drag-rotations. (pan, tilt)
-
-- `mouse_rotationspeed = 1f0` sets the speed of mouse rotations.
-- `mouse_translationspeed = 0.5f0` sets the speed of mouse translations.
-- `mouse_zoomspeed = 1f0` sets the speed of mouse zooming (mousewheel).
-- `circular_rotation = (true, true, true)` enables circular rotations for (fixed x, fixed y, fixed z) rotation axis. (This means drawing a circle with your mouse around the center of the scene will result in a continuous rotation.)
-
-There are also a few generally applicable controls:
-
 - `fix_x_key = Keyboard.x` sets the key for fixing translations and rotations to the (world/plot) x-axis.
 - `fix_y_key = Keyboard.y` sets the key for fixing translations and rotations to the (world/plot) y-axis.
 - `fix_z_key = Keyboard.z` sets the key for fixing translations and rotations to the (world/plot) z-axis.
 - `reset = Keyboard.home` sets the key for fully resetting the camera. This equivalent to setting `lookat = Vec3f(0)`, `upvector = Vec3f(0, 0, 1)`, `eyeposition = Vec3f(3)` and then calling `center!(scene)`.
 
-You can also make adjustments to the camera position, rotation and zoom by calling relevant functions:
+- `translation_button = Mouse.right` sets the mouse button for drag-translations. (up/down/left/right)
+- `scroll_mod = true` sets an additional modifier button for scroll-based zoom. (true being neutral)
+- `rotation_button = Mouse.left` sets the mouse button for drag-rotations. (pan, tilt)
+
+## Other kwargs
+
+Some keyword arguments are used to initialize fields. These include
+
+- `eyeposition = Vec3f(3)`: The position of the camera.
+- `lookat = Vec3f(0)`: The point the camera is focused on.
+- `upvector = Vec3f(0, 0, 1)`: The world direction corresponding to the up direction of the screen.
+
+- `fov = 45.0` is the field of view. This is irrelevant if the camera uses an orthographic projection.
+- `near = 0.1` sets the position of the near clip plane relative to `eyeposition - lookat`. Must be greater 0. Anything between the camera and the near clip plane is hidden.
+- `far = 10.0` sets the position of the far clip plane relative to `eyeposition - lookat`. Anything further away than the far clip plane is hidden.
+
+Note that updating these observables in an active camera requires a call to `update_cam(scene)`
+for them to be applied. For updating `eyeposition`, `lookat` and/or upvector 
+`update_cam!(scene, eyeposition, lookat, upvector = Vec3f(0,0,1))` is preferred.
+
+The camera position and orientation can also be adjusted via the functions
 
 - `translate_cam!(scene, v)` will translate the camera by the given world/plot space vector `v`.
 - `rotate_cam!(scene, angles)` will rotate the camera around its axes with the corresponding angles. The first angle will rotate around the cameras "right" that is the screens horizontal axis, the second around the up vector/vertical axis or `Vec3f(0, 0, +-1)` if `fixed_axis = true`, and the third will rotate around the view direction i.e. the axis out of the screen. The rotation respects the current `rotation_center` of the camera.
