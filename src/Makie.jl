ee56751b0fb37970a163e95c89937a21e62b06c0
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2023-05-08 15:31:34 +0200

    strip linenumber info from default exprs

--- src/Makie.jl
+++ src/Makie.jl
@@ -52,6 +52,7 @@ import TriplotBase
 import MiniQhull
 import Setfield
 import REPL
+import MacroTools
 
 using IntervalSets: IntervalSets, (..), OpenInterval, ClosedInterval, AbstractInterval, Interval, endpoints
 using FixedPointNumbers: N0f8
