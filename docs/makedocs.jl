eb290a6fe281c46a08a1ba02e382519824099a18
SimonDanisch <sdanisch@protonmail.com>
2023-06-09 20:15:39 +0200

    test jsserve

--- docs/makedocs.jl
+++ docs/makedocs.jl
@@ -2,7 +2,7 @@ using Pkg
 cd(@__DIR__)
 Pkg.activate(".")
 pkg"dev .. ../MakieCore ../CairoMakie ../GLMakie ../WGLMakie ../RPRMakie"
-pkg"add MeshIO GeometryBasics JSServe"
+pkg"add MeshIO GeometryBasics JSServe#sd/static-export"
 Pkg.instantiate()
 Pkg.precompile()
 
