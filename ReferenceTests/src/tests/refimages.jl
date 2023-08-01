c82c40409178b3ec75f4f08634445930e965596c
DanielVandH <danj.vandenheuvel@gmail.com>
2023-08-01 15:58:30 +1000

    Update to the new DelaunayTriangulation release, and add customiable bounding box

--- ReferenceTests/src/tests/refimages.jl
+++ ReferenceTests/src/tests/refimages.jl
@@ -10,16 +10,7 @@ using ReferenceTests.LaTeXStrings
 using ReferenceTests.DelimitedFiles
 using ReferenceTests.Test
 using ReferenceTests.Colors: RGB, N0f8
-using ReferenceTests.DelaunayTriangulation: DelaunayTriangulation,
-                             triangulate,
-                             convert_boundary_points_to_indices,
-                             each_point,
-                             reset_representative_points!,
-                             refine!,
-                             get_total_area,
-                             voronoi,
-                             get_polygon_colors,
-                             centroidal_smooth
+using ReferenceTests.DelaunayTriangulation
 using Makie: Record, volume
 
 @testset "primitives" begin
