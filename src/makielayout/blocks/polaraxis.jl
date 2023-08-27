85bd8f7fbb1bfcf8424db8f69ff448076c3bb443
ffreyer <frederic481994@hotmail.de>
2023-08-27 17:53:07 +0200

    consider rtick pad when reserving space

--- src/makielayout/blocks/polaraxis.jl
+++ src/makielayout/blocks/polaraxis.jl
@@ -54,11 +54,13 @@ function initialize_block!(po::PolarAxis; palette=nothing)
             rticklabelplot.plots[1].text,
             rticklabelplot.plots[1].fontsize,
             rticklabelplot.plots[1].font,
+            po.rticklabelpad,
             thetaticklabelplot.plots[1].text,
             thetaticklabelplot.plots[1].fontsize,
             thetaticklabelplot.plots[1].font,
-            po.thetaticklabelpad, po.overlay.px_area
-        ) do _, _, _, _, _, _, pad, area
+            po.thetaticklabelpad,
+            po.overlay.px_area
+        ) do _, _, _, rpad, _, _, _, tpad, area
 
         # get maximum size of tick label
         # (each boundingbox represents a string without text.position applied)
@@ -75,7 +77,7 @@ function initialize_block!(po::PolarAxis; palette=nothing)
         max_width, max_height = max_widths
 
         space_from_center = 0.5 .* widths(area)
-        space_for_ticks = 2pad .+ (max_width, max_height)
+        space_for_ticks = 2max(rpad, tpad) .+ (max_width, max_height)
         space_for_axis = space_from_center .- space_for_ticks
 
         # divide by width only because aspect ratios
