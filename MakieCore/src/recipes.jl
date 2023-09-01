ac02141c4c87dbf71d06b301f6dc18f5719e6d05
Simon <sdanisch@protonmail.com>
2023-09-01 14:13:10 +0200

    Fix depwarn for cairomakie lines (#3200)
    * Fix depwarn for cairomakie lines
    
    * clean up get_attribute

--- MakieCore/src/recipes.jl
+++ MakieCore/src/recipes.jl
@@ -21,6 +21,7 @@ func2type(f::Function) = Combined{f}
 plotkey(::Type{<: AbstractPlot{Typ}}) where Typ = Symbol(lowercase(func2string(Typ)))
 plotkey(::T) where T <: AbstractPlot = plotkey(T)
 plotkey(::Nothing) = :scatter
+plotkey(any) = nothing
 
 """
      default_plot_signatures(funcname, funcname!, PlotType)
