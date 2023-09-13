7cebfcb5571c789115aeffeab496e2eac42e695a
SimonDanisch <sdanisch@protonmail.com>
2023-09-13 20:59:29 +0200

    add docstring

--- src/colorsampler.jl
+++ src/colorsampler.jl
@@ -211,6 +211,20 @@ struct ColorMapping{N,T<:AbstractArray{<:Number,N},T2<:AbstractArray{<:Number,N}
     color_scaled::Observable{T2}
 end
 
+"""
+    Categorical(colormaplike)
+
+Accepts all colormap values that the `colormap` attribute of a plot accepts.
+Will make sure to map one value to one color and create the correct Colorbar for the plot.
+
+Example:
+```julia
+fig, ax, pl = barplot(1:3; color=1:3, colormap=Makie.Categorical(:viridis))
+```
+
+!!! warning
+    This feature might change outside breaking releases, since the API is not yet finalized
+"""
 struct Categorical{T} <: AbstractVector{RGBAf}
     values::Vector{T}
 end
