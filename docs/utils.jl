9b44c2d963e7cefda066ba808ac3b4fbc7a4bf0c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2023-12-06 11:31:25 +0100

    fix string error

--- docs/utils.jl
+++ docs/utils.jl
@@ -84,14 +84,14 @@ function env_examplefigure(com, _)
 
     str = """
     ```julia:$name
-    using Makie.LaTeXStrings: @L_str                      # hide
-    __result = begin                                      # hide
+    using Makie.LaTeXStrings: @L_str                       # hide
+    __result = begin                                       # hide
         $code
-    end                                                   # hide
-    sz = size(Makie.parent_scene(__result))               # hide
-    open(joinpath(@OUTPUT, "$(name)_size.txt), "w") do io # hide
-        print(io, sz[1], " ", sz[2])                      # hide
-    end                                                   # hide
+    end                                                    # hide
+    sz = size(Makie.parent_scene(__result))                # hide
+    open(joinpath(@OUTPUT, "$(name)_size.txt"), "w") do io # hide
+        print(io, sz[1], " ", sz[2])                       # hide
+    end                                                    # hide
     save(joinpath(@OUTPUT, "$pngfile"), __result; $rest_kwargs_str, px_per_unit = 2, pt_per_unit = 0.75) # hide
     $(svg ? "save(joinpath(@OUTPUT, \"$svgfile\"), __result; $rest_kwargs_str, px_per_unit = 2, pt_per_unit = 0.75) # hide" : "")
     nothing # hide
