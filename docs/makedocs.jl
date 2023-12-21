5427e2a5b06c9bd562685cc9641374c57f3cceee
SimonDanisch <sdanisch@protonmail.com>
2023-12-21 18:34:22 +0100

    bonito got tagged

--- docs/makedocs.jl
+++ docs/makedocs.jl
@@ -1,7 +1,6 @@
 using Pkg
 cd(@__DIR__)
 Pkg.activate(".")
-Pkg.add(url="https://github.com/SimonDanisch/Bonito.jl")
 pkg"dev .. ../MakieCore ../CairoMakie ../GLMakie ../WGLMakie ../RPRMakie"
 Pkg.precompile()
 
