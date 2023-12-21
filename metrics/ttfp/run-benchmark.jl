37f3323e6fbc76d3528ab719bff1ed97ea03c8cc
SimonDanisch <sdanisch@protonmail.com>
2023-12-21 17:24:23 +0100

    Bonito got tagged

--- metrics/ttfp/run-benchmark.jl
+++ metrics/ttfp/run-benchmark.jl
@@ -214,8 +214,7 @@ ENV["JULIA_PKG_PRECOMPILE_AUTO"] = 0
 project1 = make_project_folder("current-pr")
 Pkg.activate(project1)
 if Package == "WGLMakie"
-    Pkg.add([(; name="Electron"),
-             (; url="https://github.com/SimonDanisch/Bonito.jl", name="Bonito", rev="master")])
+    Pkg.add([(; name="Electron"),])
 end
 pkgs = NamedTuple[(; path="./MakieCore"), (; path="."), (; path="./$Package")]
 # cd("dev/Makie")
