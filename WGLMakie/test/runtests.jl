c13d66619e22c1875b9c27c56300fcc6dee9ec74
ffreyer <frederic481994@hotmail.de>
2023-12-28 17:21:40 +0100

    try enabling more tests

--- WGLMakie/test/runtests.jl
+++ WGLMakie/test/runtests.jl
@@ -43,11 +43,11 @@ excludes = Set([
     "fast pixel marker",
     "Array of Images Scatter",
     "Image Scatter different sizes",
-    "scatter with stroke",
-    "scatter with glow",
+    # "scatter with stroke",
+    # "scatter with glow",
     "lines and linestyles",
     "Textured meshscatter", # not yet implemented
-    "BezierPath marker stroke", # not yet implemented
+    # "BezierPath marker stroke", # not yet implemented
 ])
 Makie.inline!(Makie.automatic)
 
