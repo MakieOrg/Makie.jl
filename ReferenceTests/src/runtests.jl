84435dbce8bcf0c0c42109dbdf8d60d5ad1e0d33
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2023-12-29 11:23:43 +0100

    use tile-based comparison of images

--- ReferenceTests/src/runtests.jl
+++ ReferenceTests/src/runtests.jl
@@ -2,7 +2,10 @@ function get_frames(a, b)
     return (get_frames(a), get_frames(b))
 end
 
-function get_frames(video)
+rgbf_convert(x::AbstractMatrix{<:RGB}) = convert(Matrix{RGBf}, x)
+rgbf_convert(x::AbstractMatrix{<:RGBA}) = convert(Matrix{RGBAf}, x)
+
+function get_frames(video::AbstractString)
     mktempdir() do folder
         afolder = joinpath(folder, "a")
         mkpath(afolder)
@@ -16,27 +19,43 @@ function get_frames(video)
             samples = 1:istep:length(aframes)
             aframes = aframes[samples]
         end
-        return load.(aframes)
+        return rgbf_convert.(load.(aframes))
     end
 end
 
-function compare_media(a::Matrix, b::Matrix; sigma=[1,1])
-    Images.test_approx_eq_sigma_eps(a, b, sigma, Inf)
+function compare_images(a::Matrix{<:Union{RGBf,RGBAf}}, b::Matrix{<:Union{RGBf,RGBAf}})
+    if size(a) != size(b)
+        @warn "images don't have the same size, difference will be Inf"
+        return Inf
+    end
+
+    approx_tile_size_px = 30
+
+    range_dim1 = round.(Int, range(0, size(a, 1), length = ceil(Int, size(a, 1) / approx_tile_size_px)))
+    range_dim2 = round.(Int, range(0, size(a, 2), length = ceil(Int, size(a, 2) / approx_tile_size_px)))
+
+    boundary_iter(boundaries) = zip(boundaries[1:end-1] .+ 1, boundaries[2:end])
+
+    _norm(rgb1::RGBf, rgb2::RGBf) = sqrt(sum(((rgb1.r - rgb2.r)^2, (rgb1.g - rgb2.g)^2, (rgb1.b - rgb2.b)^2)))
+    _norm(rgba1::RGBAf, rgba2::RGBAf) = sqrt(sum(((rgba1.r - rgba2.r)^2, (rgba1.g - rgba2.g)^2, (rgba1.b - rgba2.b)^2, (rgba1.alpha - rgba2.alpha)^2)))
+
+    # compute the difference score as the maximum of the mean squared differences over the color
+    # values of tiles over the image. using tiles is a simple way to increase the local sensitivity
+    # without directly going to pixel-based comparison
+    return maximum(Iterators.product(boundary_iter(range_dim1), boundary_iter(range_dim2))) do ((mi1, ma1), (mi2, ma2))
+        @views mean(_norm.(a[mi1:ma1, mi2:ma2], b[mi1:ma1, mi2:ma2]))
+    end
 end
 
-function compare_media(a, b; sigma=[1,1])
-    file, ext = splitext(a)
+function compare_media(a::AbstractString, b::AbstractString)
+    _, ext = splitext(a)
     if ext in (".png", ".jpg", ".jpeg", ".JPEG", ".JPG")
-        imga = load(a)
-        imgb = load(b)
-        if size(imga) != size(imgb)
-            @warn "images don't have the same size, difference will be Inf"
-            return Inf
-        end
-        conv(x) = convert(Matrix{RGBf}, x)
-        return compare_media(conv(imga), conv(imgb), sigma=sigma)
+        imga = rgbf_convert(load(a))
+        imgb = rgbf_convert(load(b))
+        return compare_images(imga, imgb)
     elseif ext in (".mp4", ".gif")
-        aframes, bframes = get_frames(a, b)
+        aframes = get_frames(a)
+        bframes = get_frames(b)
         # Frames can differ in length, which usually shouldn't be the case but can happen
         # when the implementation of record changes, or when the example changes its number of frames
         # In that case, we just return inf + warn
@@ -44,7 +63,7 @@ function compare_media(a, b; sigma=[1,1])
             @warn "not the same number of frames in video, difference will be Inf"
             return Inf
         end
-        return mean(compare_media.(aframes, bframes; sigma=sigma))
+        return maximum(compare_images.(aframes, bframes))
     else
         error("Unknown media extension: $ext")
     end
@@ -58,9 +77,14 @@ end
 
 function record_comparison(base_folder::String; record_folder_name="recorded", tag=last_major_version())
     record_folder = joinpath(base_folder, record_folder_name)
+    @info "Downloading reference images"
     reference_folder = download_refimages(tag)
     # we copy the reference images into the output folder, since we want to upload it all as an artifact, to know against what images we compared
-    cp(reference_folder, joinpath(base_folder, "reference"))
+    local_reference_copy_dir = joinpath(base_folder, "reference")
+    if isdir(local_reference_copy_dir)
+        rm(local_reference_copy_dir, recursive = true)
+    end
+    cp(reference_folder, local_reference_copy_dir)
     testimage_paths = get_all_relative_filepaths_recursively(record_folder)
     missing_refimages, scores = compare(testimage_paths, reference_folder, record_folder)
 
