bbb9b51312e3aad6533732f56e63ed175d7e6fe4
ffreyer <frederic481994@hotmail.de>
2024-01-07 15:25:34 +0100

    activate tests

--- WGLMakie/test/runtests.jl
+++ WGLMakie/test/runtests.jl
@@ -47,9 +47,6 @@ excludes = Set([
     "Textured meshscatter", # not yet implemented
     "3D Contour with 2D contour slices", # looks like a z-fighting issue
     "colorscale (lines)", # also z-fighting
-    "Voxel - texture mapping", # not yet implemented
-    "Voxel - colors and colormap", # not yet implemented
-    "Voxel - lowclip and highclip", # not yet implemented
 ])
 Makie.inline!(Makie.automatic)
 
