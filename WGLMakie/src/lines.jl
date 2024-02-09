382d37594209a25754a1c9199d100cb75dc1df6f
ffreyer <frederic481994@hotmail.de>
2024-02-09 18:33:02 +0100

    fix linelen transform

--- WGLMakie/src/lines.jl
+++ WGLMakie/src/lines.jl
@@ -90,7 +90,9 @@ function serialize_three(scene::Scene, plot::Union{Lines, LineSegments})
     # TODO: in Javascript
     if plot isa Lines && to_value(linestyle) isa Vector
         cam = Makie.parent_scene(plot).camera
-        pvm = lift(*, plot, cam.projectionview, uniforms[:model])
+        pvm = lift(plot, cam.projectionview, cam.pixel_space, plot.space, uniforms[:model]) do _, _, space, model
+            return Makie.space_to_clip(cam, space, true) * model
+        end
         attributes[:lastlen] = map(plot, points_transformed, pvm, cam.resolution) do ps, pvm, res
             output = Vector{Float32}(undef, length(ps))
 
