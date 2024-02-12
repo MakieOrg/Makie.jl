b0798f866ecb9062148ddba1f82fd24de283dce5
Max Freudenberg <maximilian.freudenberg@uni-goettingen.de>
2024-02-12 12:05:21 +0100

    fix CairoMakie heatmap cell sizes

--- CairoMakie/src/primitives.jl
+++ CairoMakie/src/primitives.jl
@@ -771,7 +771,15 @@ function draw_atomic(scene::Scene, screen::Screen, @nospecialize(primitive::Unio
     end
 end
 
-function _draw_rect_heatmap(ctx, xys, ni, nj, colors)
+function _draw_rect_heatmap(ctx, xys, ni, nj, colors, margin_factor=0.05f0)
+    # dx and dy are the margin widths that are added to the
+    # heatmap cell sizes further down
+    # adding 5% seems like a good compromise between avoiding artifacts
+    # and not changing pixel sizes too much
+    # how much is added can be controlled via margin_factor
+
+    dx = abs(xys[2,1][1] - xys[1,1][1]) * 0.5f0 * margin_factor
+    dy = abs(xys[1,2][2] - xys[1,1][2]) * 0.5f0 * margin_factor
     @inbounds for i in 1:ni, j in 1:nj
         p1 = xys[i, j]
         p2 = xys[i+1, j]
@@ -782,16 +790,16 @@ function _draw_rect_heatmap(ctx, xys, ni, nj, colors)
         # white lines between them due to anti aliasing. To avoid this we
         # increase their size slightly.
 
-        if alpha(colors[i, j]) == 1
+        if alpha(colors[i, j]) == 1 && margin_factor > 0
             # sign.(p - center) gives the direction in which we need to
             # extend the polygon. (Which may change due to rotations in the
             # model matrix.) (i!=1) etc is used to avoid increasing the
             # outer extent of the heatmap.
             center = 0.25f0 * (p1 + p2 + p3 + p4)
-            p1 += sign.(p1 - center) .* Point2f(0.5f0 * (i!=1),  0.5f0 * (j!=1))
-            p2 += sign.(p2 - center) .* Point2f(0.5f0 * (i!=ni), 0.5f0 * (j!=1))
-            p3 += sign.(p3 - center) .* Point2f(0.5f0 * (i!=ni), 0.5f0 * (j!=nj))
-            p4 += sign.(p4 - center) .* Point2f(0.5f0 * (i!=1),  0.5f0 * (j!=nj))
+            p1 += sign.(p1 - center) .* Point2f(dx * (i!=1),  dy * (j!=1))
+            p2 += sign.(p2 - center) .* Point2f(dx * (i!=ni), dy * (j!=1))
+            p3 += sign.(p3 - center) .* Point2f(dx * (i!=ni), dy * (j!=nj))
+            p4 += sign.(p4 - center) .* Point2f(dx * (i!=1),  dy * (j!=nj))
         end
 
         Cairo.set_line_width(ctx, 0)
