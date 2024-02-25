9e1bdf755fc905d74251ae8845f68497fb7c8b04
James <96961792+jamesahoward@users.noreply.github.com>
2024-02-25 23:25:14 +0000

    Update translate! documentation
    Update translate! documentation to reflect that a 'Transformable' object can be a scene or a plot.

--- src/layouting/transformation.jl
+++ src/layouting/transformation.jl
@@ -138,10 +138,10 @@ function translate!(::Type{T}, scene::Transformable, t) where T
     end
 end
 """
-    translate!(scene::Transformable, xyz::VecTypes)
-    translate!(scene::Transformable, xyz...)
+    translate!(t::Transformable, xyz::VecTypes)
+    translate!(t::Transformable, xyz...)
 
-Apply an absolute translation to the Scene, translating it to `x, y, z`.
+Apply an absolute translation to the given `Transformable` (a Scene or Plot), translating it to `x, y, z`.
 """
 translate!(scene::Transformable, xyz::VecTypes) = translate!(Absolute, scene, xyz)
 translate!(scene::Transformable, xyz...) = translate!(Absolute, scene, xyz)
@@ -149,7 +149,7 @@ translate!(scene::Transformable, xyz...) = translate!(Absolute, scene, xyz)
 """
     translate!(Accum, scene::Transformable, xyz...)
 
-Translate the scene relative to its current position.
+Translate the given `Transformable` (a Scene or Plot) relative to its current position.
 """
 translate!(::Type{T}, scene::Transformable, xyz...) where T = translate!(T, scene, xyz)
 
