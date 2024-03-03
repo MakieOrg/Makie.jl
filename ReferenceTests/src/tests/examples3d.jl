28ba53683a84e52352b1c469c6616fdb256d9ed8
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-03-03 14:11:05 +0100

    comment out arrows test

--- ReferenceTests/src/tests/examples3d.jl
+++ ReferenceTests/src/tests/examples3d.jl
@@ -199,27 +199,27 @@ end
     f, ax, p = surface(x, y, z, color=Makie.logo(), transparency=true)
 end
 
-@reference_test "Arrows on Sphere" begin
-    n = 20
-    f   = (x, y, z) -> x * exp(cos(y) * z)
-    ∇f  = (x, y, z) -> Point3f(exp(cos(y) * z), -sin(y) * z * x * exp(cos(y) * z), x * cos(y) * exp(cos(y) * z))
-    ∇ˢf = (x, y, z) -> ∇f(x, y, z) - Point3f(x, y, z) * dot(Point3f(x, y, z), ∇f(x, y, z))
+# @reference_test "Arrows on Sphere" begin
+#     n = 20
+#     f   = (x, y, z) -> x * exp(cos(y) * z)
+#     ∇f  = (x, y, z) -> Point3f(exp(cos(y) * z), -sin(y) * z * x * exp(cos(y) * z), x * cos(y) * exp(cos(y) * z))
+#     ∇ˢf = (x, y, z) -> ∇f(x, y, z) - Point3f(x, y, z) * dot(Point3f(x, y, z), ∇f(x, y, z))
 
-    θ = [0;(0.5:n - 0.5) / n;1]
-    φ = [(0:2n - 2) * 2 / (2n - 1);2]
-    x = [cospi(φ) * sinpi(θ) for θ in θ, φ in φ]
-    y = [sinpi(φ) * sinpi(θ) for θ in θ, φ in φ]
-    z = [cospi(θ) for θ in θ, φ in φ]
+#     θ = [0;(0.5:n - 0.5) / n;1]
+#     φ = [(0:2n - 2) * 2 / (2n - 1);2]
+#     x = [cospi(φ) * sinpi(θ) for θ in θ, φ in φ]
+#     y = [sinpi(φ) * sinpi(θ) for θ in θ, φ in φ]
+#     z = [cospi(θ) for θ in θ, φ in φ]
 
-    pts = vec(Point3f.(x, y, z))
-    ∇ˢF = vec(∇ˢf.(x, y, z)) .* 0.1f0
-    surface(x, y, z)
-    arrows!(
-        pts, ∇ˢF,
-        arrowsize=0.03, linecolor=(:white, 0.6), linewidth=0.03
-    )
-    current_figure()
-end
+#     pts = vec(Point3f.(x, y, z))
+#     ∇ˢF = vec(∇ˢf.(x, y, z)) .* 0.1f0
+#     surface(x, y, z)
+#     arrows!(
+#         pts, ∇ˢF,
+#         arrowsize=0.03, linecolor=(:white, 0.6), linewidth=0.03
+#     )
+#     current_figure()
+# end
 
 @reference_test "surface + contour3d" begin
     vx = -1:0.01:1
