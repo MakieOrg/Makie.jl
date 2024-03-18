6216c7427b562758bda43948e4c834833451479e
quinn <quinn@385yw.de>
2024-03-18 15:51:04 +0100

    Document WilkinsonTicks
    Fixes #3164

--- src/makielayout/ticklocators/wilkinson.jl
+++ src/makielayout/ticklocators/wilkinson.jl
@@ -1,3 +1,18 @@
+"""
+    WilkinsonTicks(k_ideal::Int; k_min = 2, k_max = 10, kwargs...)
+
+Indicates to create ticks according to Wilkinson's scoring method. `k_min`,
+`k_ideal` and `k_max` specify the minimum, ideal and maximum number of ticks.
+
+## Attributes
+
+- `Q = [(1.0,1.0), (5.0, 0.9), (2.0, 0.7), (2.5, 0.5), (3.0, 0.2)]` a distribution of nice numbers from which labellings are sampled, stored in the form (number, score)
+- `granularity_weight = 1/4` encourages returning roughly the number of labels requested
+- `simplicity_weight = 1/6` encourages nicer labeling sequences by preferring step sizes that appear earlier in Q. Also rewards labellings that include 0 as a way to ground the sequence
+- `coverage_weight = 1/3` encourages labelings that do not extend far beyond the range of the data, penalizing unnecessary whitespace
+- `niceness_weight = 1/4` encourages labelings to produce nice ranges
+- `min_px_dist = 50.0` does nothing
+"""
 function WilkinsonTicks(k_ideal::Int; k_min = 2, k_max = 10,
         Q = [(1.0,1.0), (5.0, 0.9), (2.0, 0.7), (2.5, 0.5), (3.0, 0.2)],
         granularity_weight = 1/4,
