9362a2c6f0b89e4c9018ab056b99409a559d4819
ffreyer <frederic481994@hotmail.de>
2024-05-12 15:47:07 +0200

    fix wrong change

--- src/basic_recipes/hvspan.jl
+++ src/basic_recipes/hvspan.jl
@@ -99,4 +99,6 @@ function data_limits(p::VSpan)
     return Rect3d(Point3d(xmin, NaN, 0), Vec3d(xmax - xmin, NaN, 0))
 end
 
-convert_arguments(P::Type{<:Union{HSpan, VSpan}}, x::Interval) = apply_transform_and_model(P, endpoints(x)...)
+boundingbox(p::Union{HSpan, VSpan}, space::Symbol = :data) = apply_transform_and_model(p, data_limits(p))
+
+convert_arguments(P::Type{<:Union{HSpan, VSpan}}, x::Interval) = convert_arguments(P, endpoints(x)...)
