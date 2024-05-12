57f551133872c143774ca032f4574743d3dc2f6e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-05-12 14:14:32 +0200

    remove unresolvable refs

--- src/conversions.jl
+++ src/conversions.jl
@@ -565,7 +565,7 @@ end
     convert_arguments(Mesh, vertices, indices)::GLNormalMesh
 
 Takes `vertices` and `indices`, and creates a triangle mesh out of those.
-See [`to_vertices`](@ref) and [`to_triangles`](@ref) for more information about
+See `to_vertices` and `to_triangles` for more information about
 accepted types.
 """
 function convert_arguments(
