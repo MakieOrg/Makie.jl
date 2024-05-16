0f078c1194cb36d61557db0abe7add7c2de5f557
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-05-16 12:51:06 +0200

    improve

--- src/figures.jl
+++ src/figures.jl
@@ -99,8 +99,8 @@ to_rectsides(t::Tuple{Any, Any, Any, Any}) = GridLayoutBase.RectSides{Float32}(t
     Figure(; [figure_padding,] kwargs...)
 
 Construct a `Figure` which allows to place `Block`s like [`Axis`](@ref), [`Colorbar`](@ref) and [`Legend`](@ref) inside.
-The padding of the figure can be set by passing either one number or a tuple of four numbers for
-left, right, bottom and top paddings via the `figure_padding` keyword.
+The outer padding of the figure (the distance of the content to the edges) can be set by passing either
+one number or a tuple of four numbers for left, right, bottom and top paddings via the `figure_padding` keyword.
 
 All other keyword arguments such as `size` and `backgroundcolor` are forwarded to the
 [`Scene`](@ref) owned by the figure which acts as the container for all other visual objects.
