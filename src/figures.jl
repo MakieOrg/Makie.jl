131d99e1a358f7da224d3606a7f651a4d27814c4
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-05-16 12:49:16 +0200

    improve docstring

--- src/figures.jl
+++ src/figures.jl
@@ -102,7 +102,8 @@ Construct a `Figure` which allows to place `Block`s like [`Axis`](@ref), [`Color
 The padding of the figure can be set by passing either one number or a tuple of four numbers for
 left, right, bottom and top paddings via the `figure_padding` keyword.
 
-All other keyword arguments such as `size` and `backgroundcolor` are forwarded to [`Scene`](@ref).
+All other keyword arguments such as `size` and `backgroundcolor` are forwarded to the
+[`Scene`](@ref) owned by the figure which acts as the container for all other visual objects.
 """
 function Figure(; kwargs...)
 
