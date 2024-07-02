88993006ac2b0e164a8f112fe8b2cd4f5903642a
Documenter.jl <documenter@juliadocs.github.io>
2024-07-02 15:02:46 +0000

    build based on ee3dbc4

---
+++ previews/PR4003/assets/api.md.eiPCo-dd.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as i,a6 as a}from"./chunks/framework.C_CN40KZ.js";const b=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),s={name:"api.md"},t=a("",849),r=[t];function d(n,c,l,p,h,k){return i(),o("div",null,r)}const u=e(s,[["render",d]]);export{b as __pageData,u as default};
