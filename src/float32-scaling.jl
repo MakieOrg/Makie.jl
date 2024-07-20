bc57636397e699fe414dd167d99eb7695ddc3025
ffreyer <frederic481994@hotmail.de>
2024-07-20 17:29:33 +0200

    fix output type

--- src/float32-scaling.jl
+++ src/float32-scaling.jl
@@ -318,7 +318,8 @@ function apply_transform_and_f32_conversion(
     trans, scale = decompose_translation_scale_matrix(model)
     if is_float_safe(scale, trans) && is_identity_transform(float32convert)
         # model applied on GPU, float32convert skippable
-        return apply_transform(transform_func, data, space)
+        transformed = apply_transform(transform_func, data, space)
+        return f32_convert(nothing, transformed)
 
     elseif is_translation_scale_matrix(model)
         # translation and scale of model have been moved to f32convert, so just apply that
@@ -366,7 +367,8 @@ function apply_transform_and_f32_conversion(
     trans, scale = decompose_translation_scale_matrix(model)
     if is_float_safe(scale, trans) && is_identity_transform(float32convert)
         # model applied on GPU, float32convert skippable
-        return getindex.(apply_transform(transform_func, dimpoints, space), dim)
+        transformed = apply_transform(transform_func, dimpoints, space)
+        return [Float32(p[dim]) for p in transformed]
     
     elseif is_translation_scale_matrix(model)
         # translation and scale of model have been moved to f32convert, so just apply that
