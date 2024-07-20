9238e9433eb19a0ae10beafca24a38861b11b6ed
ffreyer <frederic481994@hotmail.de>
2024-07-20 14:32:43 +0200

    update some comments, space for model

--- src/float32-scaling.jl
+++ src/float32-scaling.jl
@@ -84,10 +84,15 @@ function patch_model(@nospecialize(plot), f32c::Nothing, model::Observable)
 end
 
 function patch_model(@nospecialize(plot), f32c::Float32Convert, model::Observable) # Observable{Any} :(
-    f32c_obs = Observable{LinearScaling}()
-    model_obs = Observable{Mat4f}(Mat4f(I))
+    f32c_obs  = Observable{LinearScaling}(f32c.scaling[], ignore_equal_values = true)
+    model_obs = Observable{Mat4f}(Mat4f(I), ignore_equal_values = true)
 
     onany(plot, f32c.scaling, model, update = true) do f32c, model
+        # Neutral f32c can mean that data and model cancel each other and we 
+        # still have Float32 preicsion issues inbetween.
+        
+        # Model has no rotation so we can extract scale + translation and move 
+        # it to the f32c. This should be cheap since we're always applying f32c.
         if is_translation_scale_matrix(model)
             scale = Makie.unsafe_extract_scale(model)
             trans = Makie.unsafe_extract_translation(model)
@@ -95,8 +100,16 @@ function patch_model(@nospecialize(plot), f32c::Float32Convert, model::Observabl
                 scale * f32c.scale, f32c.scale * trans + f32c.offset
             )
         else
+            # Otherwise we fully apply model on the CPU side with the untouched
+            # f32c afterwards
             f32c_obs[] = f32c
         end
+        # model_obs[] = Mat4f(I)
+
+        # TODO: Optimization:
+        # - keep model matrix when it's Float32-safe w/o rotation.
+        # - keep it w/ rotation if f32c is neutral as well (i.e. 1 scale, 0 offset)
+        return
     end
 
     return f32c_obs, model_obs
@@ -267,18 +280,22 @@ function apply_transform_and_f32_conversion(
         float32convert::Union{Float32Convert, LinearScaling},
         transform_func, model::Mat4d, data, space::Symbol
     )
-    # avoid intermediate arrays. TODO: Is transform_func strictly per element?
+    # TODO:
+    # - Optimization: avoid intermediate arrays 
+    # - Is transform_func strictly per element?
+    # - Optimization: Skip f32_convert if conversion is neutral (1 scale, 0 offset)
+    #                 (should probably happen here to avoid checking per element)
+
     tf = space == :data ? transform_func : identity
-    if space in (:data, :transformed)
+    if space in (:data, :transformed, :world) # maybe split :world64 and :world32?
         if is_translation_scale_matrix(model)
-            # translation and scale of model have been moved to f32convert, so
-            # just apply that
+            # translation and scale of model have been moved to f32convert, so just apply that
             return [f32_convert(float32convert, apply_transform(tf, x)) for x in data]
         else
             # model contains rotation which stops us from applying f32convert 
             # before model
             return map(data) do input
-                transformed = apply_transform_and_model(model, tf, input)
+                transformed = apply_transform_and_model(model, tf, input, space)
                 return f32_convert(float32convert, transformed)
             end
         end
@@ -314,7 +331,7 @@ function apply_transform_and_f32_conversion(
     
     tf = space == :data ? transform_func : identity
     
-    if space in (:data, :transformed)
+    if space in (:data, :transformed, :world)
         if is_translation_scale_matrix(model)
             # translation and scale of model have been moved to f32convert, so
             # just apply that
@@ -330,17 +347,17 @@ function apply_transform_and_f32_conversion(
             # before model
             if dim == 1
                 return map(data) do input
-                    transformed = apply_transform_and_model(model, tf, Point2(input, 0))
+                    transformed = apply_transform_and_model(model, tf, Point2(input, 0), space)
                     return f32_convert(float32convert, transformed, 1)
                 end
             elseif dim == 2
                 return map(data) do input
-                    transformed = apply_transform_and_model(model, tf, Point2(0, input))
+                    transformed = apply_transform_and_model(model, tf, Point2(0, input), space)
                     return f32_convert(float32convert, transformed, 2)
                 end
             else
                 return map(data) do input
-                    transformed = apply_transform_and_model(model, tf, Point3(0, 0, input))
+                    transformed = apply_transform_and_model(model, tf, Point3(0, 0, input), space)
                     return f32_convert(float32convert, transformed, 3)
                 end
             end
