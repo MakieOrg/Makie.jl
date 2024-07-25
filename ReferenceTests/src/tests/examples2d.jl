63096f11ee7fbd661da43be26a487d1466fe9230
SimonDanisch <sdanisch@protonmail.com>
2024-07-25 17:31:09 +0200

    rather do a 3d example

--- ReferenceTests/src/tests/examples2d.jl
+++ ReferenceTests/src/tests/examples2d.jl
@@ -1466,18 +1466,3 @@ end
     end
     fig
 end
-
-@reference_test "Surface interpolate attribute" begin
-    r = 0:0.01:1
-    Z = broadcast((x,y)-> 0.1 * x, r, r')
-    C = zeros(size(Z)) #colour matrix
-    C[:, 5:10] .= 1
-    C[:, 40:45] .= 2
-    C[:, 70:75] .= 3
-    C[:, 90:95] .= 4
-    f, ax, s = surface(r, r, zeros(size(Z)); interpolate=true, color=C, lowclip=:blue, colorrange=(1, 4),
-                       shading=NoShading, axis=(;title="interpolate=true"))
-    surface(f[1, 2], r, r, zeros(size(Z)); interpolate=false, color=C, lowclip=:blue, colorrange=(1, 4),
-                       shading=NoShading, axis=(;title="interpolate=false"))
-    f
-end
