9669f9d36168291b2b95ab738de4867e9dc942e8
Documenter.jl <documenter@juliadocs.github.io>
2024-08-09 16:22:12 +0000

    build based on ad4032a

---
+++ previews/PR3958/assets/api.md.FYVYGvdv.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as i,a7 as s}from"./chunks/framework.D7I-Ay91.js";const u=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),a={name:"api.md"},t=s("",851),r=[t];function d(n,c,l,p,h,k){return i(),o("div",null,r)}const b=e(a,[["render",d]]);export{u as __pageData,b as default};
