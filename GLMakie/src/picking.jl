8bd19c1d99e488be5ff032c5f787e6d2c5c45425
Eddie Groshev <eddie_g_89@hotmail.com>
2024-08-16 11:33:32 -0700

    update GLMakie

--- GLMakie/src/picking.jl
+++ GLMakie/src/picking.jl
@@ -67,17 +67,26 @@ end
 # Skips one set of allocations
 function Makie.pick_closest(scene::Scene, screen::Screen, xy, range)
     isopen(screen) || return (nothing, 0)
-    w, h = size(scene)
+    w, h = size(scene) # unitless dimensions
     ((1.0 <= xy[1] <= w) && (1.0 <= xy[2] <= h)) || return (nothing, 0)
 
-    x0, y0 = max.(1, floor.(Int, xy .- range))
-    x1, y1 = min.((w, h), floor.(Int, xy .+ range))
+    ppu = screen.px_per_unit[]
+    w, h = size(screen.root_scene) # pixel dimensions
+    x0, y0 = max.(1, floor.(Int, ppu .* (xy .- range)))
+    x1, y1 = min.((w, h), ceil.(Int, ppu .* (xy .+ range)))
     dx = x1 - x0; dy = y1 - y0
-    sids = pick_native(screen, Rect2i(x0, y0, dx, dy))
+
+    ShaderAbstractions.switch_context!(screen.glscreen)
+    fb = screen.framebuffer
+    buff = fb.buffers[:objectid]
+    glBindFramebuffer(GL_FRAMEBUFFER, fb.id[1])
+    glReadBuffer(GL_COLOR_ATTACHMENT1)
+    picks = zeros(SelectionID{UInt32}, dx, dy)
+    glReadPixels(x0, y0, dx, dy, buff.format, buff.pixeltype, picks)
 
     min_dist = range^2
     id = SelectionID{Int}(0, 0)
-    x, y =  xy .+ 1 .- Vec2f(x0, y0)
+    x, y = xy .* ppu .+ 1 .- Vec2f(x0, y0)
     for i in 1:dx, j in 1:dy
         d = (x-i)^2 + (y-j)^2
         sid = sids[i, j]
@@ -97,19 +106,28 @@ end
 # Skips some allocations
 function Makie.pick_sorted(scene::Scene, screen::Screen, xy, range)
     isopen(screen) || return (nothing, 0)
-    w, h = size(scene)
+    w, h = size(scene) # unitless dimensions
     if !((1.0 <= xy[1] <= w) && (1.0 <= xy[2] <= h))
         return Tuple{AbstractPlot, Int}[]
     end
-    x0, y0 = max.(1, floor.(Int, xy .- range))
-    x1, y1 = min.([w, h], ceil.(Int, xy .+ range))
+
+    ppu = screen.px_per_unit[]
+    w, h = size(screen.root_scene) # pixel dimensions
+    x0, y0 = max.(1, floor.(Int, ppu .* (xy .- range)))
+    x1, y1 = min.((w, h), ceil.(Int, ppu .* (xy .+ range)))
     dx = x1 - x0; dy = y1 - y0
 
-    picks = pick_native(screen, Rect2i(x0, y0, dx, dy))
+    ShaderAbstractions.switch_context!(screen.glscreen)
+    fb = screen.framebuffer
+    buff = fb.buffers[:objectid]
+    glBindFramebuffer(GL_FRAMEBUFFER, fb.id[1])
+    glReadBuffer(GL_COLOR_ATTACHMENT1)
+    picks = zeros(SelectionID{UInt32}, dx, dy)
+    glReadPixels(x0, y0, dx, dy, buff.format, buff.pixeltype, picks)
 
     selected = filter(x -> x.id > 0 && haskey(screen.cache2plot, x.id), unique(vec(picks)))
     distances = Float32[range^2 for _ in selected]
-    x, y =  xy .+ 1 .- Vec2f(x0, y0)
+    x, y = xy .* ppu .+ 1 .- Vec2f(x0, y0)
     for i in 1:dx, j in 1:dy
         if picks[i, j].id > 0
             d = (x-i)^2 + (y-j)^2
