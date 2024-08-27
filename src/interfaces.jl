0eeea64c4357fd749d9d3a1a1c77b8d803695606
SimonDanisch <sdanisch@protonmail.com>
2024-08-27 17:22:38 +0200

    fix user attributes for specapi + updating

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -106,8 +106,8 @@ const atomic_functions = (
 const Atomic{Arg} = Union{map(x-> Plot{x, Arg}, atomic_functions)...}
 
 function get_kw_values(func, names, kw)
-    return NamedTuple([Pair{Symbol,Any}(k, func(kw[k]))
-            for k in names if haskey(kw, k)])
+    return [Pair{Symbol,Any}(k, func(kw[k]))
+            for k in names if haskey(kw, k)]
 end
 
 function get_kw_obs(names, kw)
@@ -177,8 +177,10 @@ function convert_observable_args(P, args_obs, kw_obs, converted, deregister)
     new_args_obs = map(Observable, converted)
     fs = onany(kw_obs, args_obs...) do kw, args...
         conv = convert_arguments(P, args...; kw...)
-        if !(conv isa Tuple)
+        if conv isa Union{PlotSpec,BlockSpec,GridLayoutSpec,AbstractVector{PlotSpec}}
             conv = (conv,) # for PlotSpec
+        elseif !(conv isa Tuple)
+            error("Returned type of convert_arguments needs to be a PlotSpec or Tuple, got: $(typeof(conv))")
         end
         for (obs, arg) in zip(new_args_obs, conv)
             obs.val = arg
@@ -209,13 +211,13 @@ The main conversion pipeline for converting arguments for a plot type.
 Applies dim_converts, expand_dimensions (in `try_dim_convert`), convert_arguments and checks if the conversion was successful.
 """
 function conversion_pipeline(
-        P::Type{<:Plot}, used_attrs::Tuple, args::Tuple,
+        P::Type{<:Plot}, used_attrs::Tuple, args::Tuple, kw_obs,
         args_obs::Tuple, user_attributes::Dict{Symbol, Any}, deregister, recursion=1)
     if recursion === 3
         error("Recursion limit reached. This should not happen, please open an issue with Makie.jl and provide a minimal working example.")
         return P, args_obs
     end
-    kw_obs = get_kw_obs(used_attrs, user_attributes)
+
     kw = to_value(kw_obs)
     PTrait = conversion_trait(P, args...)
     dim_converted = try_dim_convert(P, PTrait, user_attributes, args_obs, deregister)
@@ -269,13 +271,21 @@ function Plot{Func}(user_args::Tuple, user_attributes::Dict) where {Func}
     deregister = Observables.ObserverFunction[]
     PTrait = conversion_trait(P, args...)
     expanded_args_obs = apply_expand_dimensions(PTrait, args, args_obs, deregister)
-    converted_obs = conversion_pipeline(P, attr, args, expanded_args_obs, user_attributes, deregister)
+    kw_obs = get_kw_obs(attr, user_attributes)
+    converted_obs = conversion_pipeline(P, attr, args, kw_obs, expanded_args_obs, user_attributes, deregister)
     args2 = map(to_value, converted_obs)
     ArgTyp = MakieCore.argtypes(args2)
     FinalPlotFunc = plotfunc(plottype(P, args2...))
     foreach(x -> delete!(user_attributes, x), attr)
-    return Plot{FinalPlotFunc,ArgTyp}(user_attributes, Any[args_obs...], Observable[converted_obs...],
+    pl = Plot{FinalPlotFunc,ArgTyp}(user_attributes, Any[args_obs...], Observable[converted_obs...],
                                       deregister)
+    used_attr_obs = map(k-> get!(pl.attributes, k, Observable{Any}(nothing)), attr)
+    onany(pl, used_attr_obs...) do args...
+        zipped = filter(((k, v),) -> !isnothing(v), collect(zip(attr, args)))
+        kw_obs[] = map(x-> x[1] => x[2], zipped)
+        return
+    end
+    return pl
 end
 
 """
