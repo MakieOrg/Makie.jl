77b0b69028f50b38be82a0e91552dbca45f7fc1f
Alexander Plavin <alexander@plav.in>
2024-09-09 12:56:44 +0200

    support images with reversed axes

--- test/conversions.jl
+++ test/conversions.jl
@@ -341,9 +341,11 @@ end
 
     v1 = collect(1:10)
     v2 = collect(1:6)
+    v3 = reverse(v1)
 
     i1 = 1 .. 10
     i2 = 1 .. 6
+    i3 = 10 .. 1
 
     o3 = Float32.(m3)
 
@@ -353,6 +355,8 @@ end
         @test convert_arguments(Image, m3) == ((0.0f0, 10.0f0), (0.0f0, 6.0f0), o3)
         @test convert_arguments(Image, v1, r2, m3) == ((1.0f0, 10.0f0), (1.0f0, 6.0f0), o3)
         @test convert_arguments(Image, i1, v2, m3) == ((1.0f0, 10.0f0), (1.0f0, 6.0f0), o3)
+        @test convert_arguments(Image, v3, i1, m3) == ((10, 1), (1, 10), o3)
+        @test convert_arguments(Image, v1, i3, m3) == ((1, 10), (10, 1), o3)
         @test convert_arguments(Image, m1, m2, m3) === (m1, m2, m3)
         @test convert_arguments(Heatmap, m1, m2) === (m1, m2)
     end
