49ea03836db74781a0b44944790584efd2081ece
ffreyer <frederic481994@hotmail.de>
2024-09-16 01:19:16 +0200

    prototype `mesh(::MetaMesh)`

---
+++ src/basic_recipes/mesh.jl
@@ -0,0 +1,48 @@
+function plot!(p::Mesh{<: Tuple{<: GeometryBasics.MetaMesh}})
+    metamesh = p[1][]
+    meshes = GeometryBasics.split_mesh(metamesh.mesh)
+    names = metamesh[:material_names]
+    
+    for (name, m) in zip(names, meshes)
+        attr = Attributes()
+        material = metamesh[:materials][name]
+
+        if haskey(material, "diffuse map")
+            try 
+                x = material["diffuse map"]
+                tex = if haskey(x, "image")
+                    x["image"]
+                elseif haskey(x, "filename")
+                    FileIO.load(x["filename"])
+                else
+                    fill(RGB{N0f8}(1,0,1))
+                end
+                repeat = get(x, "clamp", false) ? (:clamp_to_edge) : (:repeat)
+                
+                attr[:color] = ShaderAbstractions.Sampler(tex; 
+                    x_repeat = repeat, mipmap = true,
+                    minfilter = :linear_mipmap_linear, magfilter = :linear
+                )
+                
+                scale = Vec2f(get(x, "scale", Vec2f(1)))
+                trans = Vec2f(get(x, "offset", Vec2f(0)))
+                attr[:uv_transform] = ((trans, scale), :mesh)
+            catch e
+                @error "Failed to load texture from material $name: " exception = e
+            end
+        end
+
+        # attr[:ambient]   = get(material, "ambient", p.attributes[:ambient])
+        attr[:diffuse]   = get(material, "diffuse", p.attributes[:diffuse])
+        attr[:specular]  = get(material, "specular", p.attributes[:specular])
+        attr[:shininess] = get(material, "shininess", p.attributes[:shininess])
+
+        for k in Makie.attribute_names(Makie.Mesh)
+            get!(attr, k, p.attributes[k])
+        end
+
+        plt = mesh!(p, m; attr...)
+    end
+
+    return p
+end
