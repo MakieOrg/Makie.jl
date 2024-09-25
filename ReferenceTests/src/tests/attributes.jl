8b0a0510192d73de31037c6847eaa1f442e7c1c9
ffreyer <frederic481994@hotmail.de>
2024-09-25 13:35:25 +0200

    merge simple refimage tests

--- ReferenceTests/src/tests/attributes.jl
+++ ReferenceTests/src/tests/attributes.jl
@@ -1,20 +1,10 @@
-@reference_test "glowcolor, glowwidth" begin
-    scatter(RNG.randn(10), color=:blue, glowcolor=:orange, glowwidth=10)
-end
-
-@reference_test "isorange, isovalue" begin
-    r = range(-1, stop=1, length=100)
-    matr = [(x.^2 + y.^2 + z.^2) for x = r, y = r, z = r]
-    volume(matr .* (matr .> 1.4), algorithm=:iso, isorange=0.05, isovalue=1.7, colorrange=(0, 1))
-end
-
+# TODO: Redundant
 @reference_test "levels" begin
     x = LinRange(-1, 1, 20)
     y = LinRange(-1, 1, 20)
     z = x .* y'
     contour(x, y, z, linewidth=3, colormap=:colorwheel, levels=50)
 end
-
 
 @reference_test "position" begin
     fig, ax, sc = scatter(RNG.rand(10), color=:red)
@@ -24,10 +14,6 @@ end
 
 @reference_test "rotation" begin
     text("Hello World", rotation=1.1)
-end
-
-@reference_test "shading" begin
-    mesh(Sphere(Point3f(0), 1f0), color=:orange, shading=NoShading)
 end
 
 @reference_test "visible" begin
