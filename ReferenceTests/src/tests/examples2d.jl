6bcfabe34f203e3d0851d1e82acbc01ffda521f1
ffreyer <frederic481994@hotmail.de>
2024-10-01 17:53:13 +0200

    add stem

--- ReferenceTests/src/tests/examples2d.jl
+++ ReferenceTests/src/tests/examples2d.jl
@@ -1691,3 +1691,31 @@ end
     
     f
 end
+
+@reference_test "stem" begin
+    f = Figure()
+
+    xs = LinRange(0, 4pi, 30)
+    stem(f[1, 1], xs, sin.(xs))
+
+    stem(f[1, 2], xs, sin,
+        offset = 0.5, trunkcolor = :blue, marker = :rect,
+        stemcolor = :red, color = :orange,
+        markersize = 15, strokecolor = :red, strokewidth = 3,
+        trunklinestyle = :dash, stemlinestyle = :dashdot)
+    
+    stem(f[2, 1], xs, sin.(xs),
+        offset = LinRange(-0.5, 0.5, 30),
+        color = LinRange(0, 1, 30), colorrange = (0, 0.5),
+        trunkcolor = LinRange(0, 1, 30), trunkwidth = 5)
+
+    ax, p = stem(f[2, 2], 0.5xs, 2 .* sin.(xs), 2 .* cos.(xs),
+        offset = Point3f.(0.5xs, sin.(xs), cos.(xs)),
+        stemcolor = LinRange(0, 1, 30), stemcolormap = :Spectral, stemcolorrange = (0, 0.5))
+
+    center!(ax.scene)
+    zoom!(ax.scene, 0.8)
+    ax.scene.camera_controls.settings[:center] = false
+
+    f
+end
