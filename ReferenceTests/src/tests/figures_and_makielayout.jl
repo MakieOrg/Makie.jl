abd06a93a75de0572136e3aefef4ccfa8bafdc5f
ffreyer <frederic481994@hotmail.de>
2024-10-02 12:59:28 +0200

    add test for Button, Slider, Toggle, IntervalSlider

--- ReferenceTests/src/tests/figures_and_makielayout.jl
+++ ReferenceTests/src/tests/figures_and_makielayout.jl
@@ -411,3 +411,27 @@ end
     Makie.Checkbox(f[2, 5], checked = true, checkboxcolor_checked = :orange)
     f
 end
+
+# @reference_test "Button - Slider - Toggle" 
+begin
+    f = Figure(size = (500, 200))
+    Button(f[1, 1:2])
+    Button(f[2, 1:2], buttoncolor = :orange, cornerradius = 20, 
+        strokecolor = :red, strokewidth = 2, # TODO: allocate space for this
+        fontsize = 16, labelcolor = :blue)
+
+    IntervalSlider(f[1, 3])
+    sl = IntervalSlider(f[2, 3], range = 0:100, linewidth = 20, color_inactive = :orange, color_active_dimmed = :lightgreen)
+    Makie.set_close_to!(sl, 30, 70)
+
+    Toggle(f[3, 1])
+    Toggle(f[4, 1], framecolor_inactive = :lightblue, rimfraction = 0.6)
+    Toggle(f[3, 2], active = true)
+    Toggle(f[4, 2], active = true, framecolor_inactive = :lightblue, framecolor_active = :yellow, rimfraction = 0.6)
+
+    Slider(f[3, 3])
+    sl = Slider(f[4, 3], range = 0:100, linewidth = 20, color_inactive = :cyan, color_active_dimmed = :lightgreen)
+    Makie.set_close_to!(sl, 30)
+
+    f
+end
