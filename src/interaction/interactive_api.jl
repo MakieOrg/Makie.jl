40fdbc3dedc976802ee850e970e6baa3d28bc7af
ffreyer <frederic481994@hotmail.de>
2024-10-08 21:45:17 +0200

    tweak docs

--- src/interaction/interactive_api.jl
+++ src/interaction/interactive_api.jl
@@ -63,7 +63,7 @@ The index returned relates to the main input of the respective primitive plot.
 - For `scatter` and `meshscatter` it is an index into the positions given to the plot.
 - For `text` it is an index into the merged character array.
 - For `lines` and `linesegments` it is the end position of the selected line segment. 
-- For `image`, `heatmap` and `surface` it is the linear index into the matrix argument of the plot (i.e. the given image, value or z-value matrix).
+- For `image`, `heatmap` and `surface` it is the linear index into the matrix argument of the plot (i.e. the given image, value or z-value matrix) that is closest to the selected position.
 - For `voxels` it is the linear index into the given 3D Array.
 - For `mesh` it is the largest vertex index of the picked triangle face.
 - For `volume` it is always 0.
