84c5c7d74081350d94ab0a0d66e5249b483ab677
ffreyer <frederic481994@hotmail.de>
2024-10-09 16:12:40 +0200

    add adapted_grid_rng conversion argument and update tests

--- ReferenceTests/src/tests/short_tests.jl
+++ ReferenceTests/src/tests/short_tests.jl
@@ -26,19 +26,19 @@ end
 @reference_test "lines inputs" begin
     f = Figure()
     lines(f[1, 1], Circle(Point2f(0), Float32(1)))
-    lines(f[1, 2], -1..1, x -> x^2)
+    lines(f[1, 2], -1..1, x -> x^2, adapted_grid_rng = RNG.STABLE_RNG)
     lines(f[2, 1], Makie.OffsetArrays.Origin(-50)(1:100))
     f
 end
 
 @reference_test "scatter inputs" begin
     f = Figure()
-    scatter(f[1, 1], 0..1, RNG.rand(10), markersize=RNG.rand(10) .* 20)
+    scatter(f[1, 1], 0..1, RNG.rand(10), markersize=RNG.rand(10) .* 20, adapted_grid_rng = RNG.STABLE_RNG)
     scatter(f[1, 2], LinRange(0, 1, 10), RNG.rand(10))
     colors = Makie.resample(to_colormap(:Spectral), 20)
     scatter!(RNG.rand(20), RNG.rand(20), markersize=RNG.rand(20) .* 20, color=colors)
     
-    scatter(f[2, 1], -1..1, x -> x^2)
+    scatter(f[2, 1], -1..1, x -> x^2, adapted_grid_rng = RNG.STABLE_RNG)
     scatter(f[2, 2], RNG.randn(10), color=:blue, glowcolor=:orange, glowwidth=10)
     f
 end
