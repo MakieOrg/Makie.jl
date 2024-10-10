2f9775a2c7867ed55396bde7fd1b440ac002159d
Julius Krumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2024-10-10 10:48:34 +0200

    Improvements to relocatability test script (#4468)
    use `run` to get printouts and `finally` to not swallow errors in `catch`

--- relocatability.jl
+++ relocatability.jl
@@ -41,15 +41,17 @@ using PackageCompiler
 
 create_app(joinpath(pwd(), "MakieApp"), "executable"; force=true, incremental=true, include_transitive_dependencies=false)
 exe = joinpath(pwd(), "executable", "bin", "MakieApp")
-@test success(`$(exe)`)
+# `run` allows to see potential informative printouts, `success` swallows those
+p = run(`$(exe)`)
+@test p.exitcode == 0
 julia_pkg_dir = joinpath(Base.DEPOT_PATH[1], "packages")
 @test isdir(julia_pkg_dir)
 mvd_julia_pkg_dir = julia_pkg_dir * ".old"
-mv(julia_pkg_dir, mvd_julia_pkg_dir)
+mv(julia_pkg_dir, mvd_julia_pkg_dir, force = true)
 # Move package dir so that we can test relocatability (hardcoded paths to package dir being invalid now)
 try
-    mv(julia_pkg_dir, mvd_julia_pkg_dir)
-    @test success(`$(exe)`)
-catch e
+    p2 = run(`$(exe)`)
+    @test p2.exitcode == 0
+finally
     mv(mvd_julia_pkg_dir, julia_pkg_dir)
 end
