fabf49ef15a32f4cfa398620ff09b17e5180a198
ffreyer <frederic481994@hotmail.de>
2024-11-25 23:29:55 +0100

    fix test error

--- ReferenceTests/src/tests/figures_and_makielayout.jl
+++ ReferenceTests/src/tests/figures_and_makielayout.jl
@@ -311,7 +311,7 @@ end
 
     protrusions = (40, 30, 20, 10)
     perspectiveness = Observable(0.0)
-    cat = load(Makie.assetpath("cat.obj"))
+    cat = GeometryBasics.expand_faceviews(load(Makie.assetpath("cat.obj")))
     cs = 1:length(Makie.coordinates(cat))
 
     for (bx, by, viewmode) in [(1,1,:fit), (1,2,:fitzoom), (2,1,:free), (2,2,:stretch)]
