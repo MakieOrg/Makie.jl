11e86ec3120b29505ec2d90c22f437808c5a0618
Simon <sdanisch@protonmail.com>
2024-12-05 12:09:30 +0100

    Show a "Did you mean:" suggestion for thrown InvalidAttributeErrors via levenshtein distance (#4645)
    * Provide suggestions
    
    * Bad test
    
    * Spacing
    
    * pr
    
    * show edits
    
    * Fix substitution highlighting
    
    ---------
    
    Co-authored-by: Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
    Co-authored-by: Frederic Freyer <frederic481994@hotmail.de>

--- MakieCore/src/recipes.jl
+++ MakieCore/src/recipes.jl
@@ -722,6 +722,134 @@ function print_columns(io::IO, v::Vector{String}; gapsize = 2, rows_first = true
     return
 end
 
+function _levenshtein_matrix(s1, s2)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L648
+    a, b = collect(s1), collect(s2)
+    m, n = length(a), length(b)
+    d = Matrix{Int}(undef, m + 1, n + 1)
+    d[1:m+1, 1] = 0:m
+    d[1, 1:n+1] = 0:n
+    for i in 1:m
+        for j in 1:n
+            d[i+1, j+1] = min(d[i, j+1] + 1, d[i+1, j] + 1, d[i, j] + (a[i] != b[j]))
+        end
+    end
+    return d
+end
+function _levenshtein(s1, s2)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L648
+    d = _levenshtein_matrix(s1, s2)
+    return d[end]
+end
+function _fuzzyscore(needle, haystack)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L631
+    score = 0.0
+    is, acro = _bestmatch(needle, haystack)
+    score += (acro ? 2 : 1) * length(is)                # Matched characters
+    score -= 2(length(needle) - length(is))             # Missing characters
+    !acro && (score -= _avgdistance(is)/10)             # Contiguous
+    !isempty(is) && (score -= sum(is)/length(is)/100)   # Closer to beginning
+end
+function _matchinds(needle, haystack; acronym::Bool = false)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L602
+    chars = collect(needle)
+    is = Int[]
+    lastc = '\0'
+    for (i, char) in enumerate(haystack)
+        while !isempty(chars) && isspace(first(chars))
+            popfirst!(chars)
+        end
+        isempty(chars) && break
+        if lowercase(char) == lowercase(chars[1]) && (!acronym || !isletter(lastc))
+            push!(is, i)
+            popfirst!(chars)
+        end
+        lastc = char
+    end
+    return is
+end
+function _longer(x, y)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L621
+    return length(x) ≥ length(y) ? (x, true) : (y, false)
+end
+function _bestmatch(needle, haystack)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L623
+    return _longer(
+        _matchinds(needle, haystack, acronym = true),
+        _matchinds(needle, haystack)
+    )
+end
+function _avgdistance(xs)
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L627
+    return isempty(xs) ? 0 : (xs[end] - xs[1] - length(xs) + 1) / length(xs)
+end
+function _levsort(search::String, candidates::Vector{String})
+    # https://github.com/JuliaLang/julia/blob/6f3fdf7b36250fb95f512a2b927ad2518c07d2b5/stdlib/REPL/src/docview.jl#L666
+    scores = map(candidates) do cand
+        lev = Float64(_levenshtein(search, cand))
+        fuz = -_fuzzyscore(search, cand)
+        return (lev, -fuz)
+    end
+    candidates = candidates[sortperm(scores)]
+    valid = _levenshtein(search, candidates[1]) < 3 # is the first close enough?
+    return candidates[1], valid # Only return one suggestion per search
+end
+function find_nearby_attributes(attributes, candidates)
+    d = Vector{Tuple{String, Bool}}(undef, length(attributes))
+    any_close = false
+    for (i, attr) in enumerate(attributes)
+        candidate, valid = _levsort(String(attr), candidates)
+        any_close = any_close || valid
+        d[i] = (candidate, valid)
+    end
+    return d, any_close
+end
+
+function textdiff(X::String, Y::String)
+    d = _levenshtein_matrix(X, Y)
+    a, b = collect(X), collect(Y)
+    m, n = length(a), length(b)
+
+    # Backtrack to print the differences with style
+    i, j = m, n
+    results = Vector{Tuple{Char, Symbol}}()
+
+    while i > 0 || j > 0
+        if i > 0 && j > 0 && a[i] == b[j]
+            # Characters match, print normally
+            push!(results, (b[j], :normal))
+            i -= 1
+            j -= 1
+        elseif i > 0 && j > 0 && d[i+1, j+1] == d[i, j] + 1
+            # Substitution (different characters between `X` and `Y`)
+            push!(results, (b[j], :orange))  # Highlighting the new character. Not showing the old one
+            i -= 1
+            j -= 1
+        elseif j > 0 && d[i+1, j+1] == d[i+1, j] + 1
+            # Insertion in `Y` (character in `Y` but not in `X`)
+            push!(results, (b[j], :red))  # Highlighting the added character
+            j -= 1
+        elseif i > 0 && d[i+1, j+1] == d[i, j+1] + 1
+            # Deletion in `X` (character in `X` but not in `Y`)
+            i -= 1  # Just move the index for X. Not showing the deletion here.
+        end
+    end
+
+    reverse!(results)
+    io = IOBuffer()
+    cio = IOContext(io, :color => true)
+
+    for (char, clr) in results
+        if clr == :normal
+            print(io, char)
+        else
+            printstyled(cio, char; color = :blue, bold = true) # Ignoring different color choices here
+        end
+    end
+
+    return String(take!(io))
+end
+
 function Base.showerror(io::IO, err::InvalidAttributeError)
     n = length(err.attributes)
     print(io, "Invalid attribute$(n > 1 ? "s" : "") ")
@@ -730,9 +858,31 @@ function Base.showerror(io::IO, err::InvalidAttributeError)
         printstyled(io, att; color = :red, bold = true)
     end
     print(io, " for $(err.object_name) type ")
-    printstyled(io, err.type; color = :blue, bold = true)
+    printstyled(io, err.type; color=:blue, bold=true)
     println(io, ".")
     nameset = sort(string.(collect(attribute_names(err.type))))
+    attrs = string.(collect(err.attributes))
+    possible_cands, any_close = find_nearby_attributes(attrs, nameset)
+    any_close && println(io)
+    if any_close && length(possible_cands) == 1
+        print(io, "Did you mean ", textdiff(attrs[1], possible_cands[1][1]), "?")
+        println(io)
+    elseif any_close
+        print(io, "Did you mean:")
+        for (id, (passed, (suggestion, close))) in enumerate(zip(attrs, possible_cands))
+            close || continue
+            any_next = any(x -> x[2], view(possible_cands, id+1:length(possible_cands)))
+            if (id == length(err.attributes)) || (id < length(err.attributes) && !any_next)
+                print(io, " and")
+            end
+            print(io, " ", textdiff(passed, suggestion))
+            if id < length(err.attributes) && any_next
+                print(io, ",")
+            end
+        end
+        println(io, "?")
+        println(io)
+    end
     println(io)
     println(io, "The available $(err.object_name) attributes for $(err.type) are:")
     println(io)
