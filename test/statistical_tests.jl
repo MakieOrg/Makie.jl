4a680bfb112168a1be96307f0f4d1f41aa6cb549
ffreyer <frederic481994@hotmail.de>
2024-12-19 00:28:39 +0100

    actually another CI run would be nice

--- test/statistical_tests.jl
+++ test/statistical_tests.jl
@@ -296,10 +296,7 @@ end
     @test p2.plots[2][:visible][] === :false
 
     # median edge case #4675
-    @test let
-        f,a,p = violin(fill(1, 1000), push!(fill(0, 999), 1), show_median=true, datalimits=(-0.001,Inf))
-        true
-    end
+    @test violin(fill(1, 1000), push!(fill(0, 999), 1), show_median=true, datalimits=(-0.001,Inf)) !== nothing
     # And some others
     @test violin(fill(1, 1000),fill(0, 1000), show_median=true) !== nothing
     @test violin([1], [1], show_median=true) !== nothing
