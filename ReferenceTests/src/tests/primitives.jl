e6b6e9641e321b0215a6ef80c526ee476af22c43
ffreyer <frederic481994@hotmail.de>
2025-02-18 21:05:33 +0100

    fix test

--- ReferenceTests/src/tests/primitives.jl
+++ ReferenceTests/src/tests/primitives.jl
@@ -775,9 +775,9 @@ end
     texture = FileIO.load(Makie.assetpath("debug_texture.png"))
     f,a,p = voxels(ones(UInt8, 3,3,3), uv_transform = [I], color = texture)
     st = Stepper(f)
-    step!(st)
+    Makie.step!(st)
     update_cam!(a.scene, 5pi/4, -pi/4)
-    step!(st)
+    Makie.step!(st)
     st
 end
 
