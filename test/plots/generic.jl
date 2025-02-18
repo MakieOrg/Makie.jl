2944a47c8d428e91b785b8cb431372a766e1bf1e
ffreyer <frederic481994@hotmail.de>
2025-02-18 23:23:49 +0100

    prototype attribute checks

--- test/plots/generic.jl
+++ test/plots/generic.jl
@@ -1,9 +1,33 @@
-@testset "plot!() to scene" begin
+@testset "core attributes" begin
+    # also tests that plot!(scene, ...) always works
+    core_attributes = [
+        :visible, :transparency, :space, :overdraw, :inspectable, :inspector_label,
+        :inspector_clear, :inspector_hover, :clip_planes, :depth_shift, #:fxaa
+    ]
+
+    # `exclude` to exclude an attribute from being checked
+    # `skip_inheritance` to exclude it after the top level plot is checked (e.g. if child plots do not inherit it)
+    # `expected` attributes to compare to (default values)
+    function validate_attributes(p; exclude = Set(), expected = Dict{Symbol, Any}(), skip_inheritance = Set())
+        attr = attributes(p)
+
+        for name in core_attributes
+            name in exclude && continue
+            name in skip_inheritance && push!(exclude, name)
+            @test haskey(attr, name) && (to_value(attr[name]) == to_value(expected[name]))
+        end
+
+        foreach(p -> validate_attributes(p, exclude = exclude, expected = expected), p.plots)
+    end
+
     scene = Scene()
+    expected = Makie.MakieCore.generic_plot_attributes!(Attributes())
+    expected[:clip_planes] = scene.theme.clip_planes
+
     for PlotType in ALL_PLOT_TYPES
         @testset "$PlotType" begin
-            testplot!(scene, PlotType)
-            @test true
+            p = testplot!(scene, PlotType)
+            # validate_attributes(p, expected = expected)
         end
     end
 end
