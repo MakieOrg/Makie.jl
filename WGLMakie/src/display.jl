79e24492003116001baa491e1afb0162a81051ca
Julius Krumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2025-02-20 13:21:13 +0100

    DPI metadata for png (#4812)
    * remove special cairomakie png method
    
    * convert px_per_unit to dpi and store in png
    
    * bump pngfiles compat to version introducing dpi keyword
    
    * add changelog
    
    * fix test by always going to rgba
    
    * retrieve px_per_unit in more robust way

--- WGLMakie/src/display.jl
+++ WGLMakie/src/display.jl
@@ -66,6 +66,10 @@ mutable struct Screen <: Makie.MakieScreen
     end
 end
 
+function Makie.px_per_unit(s::Screen)::Float64
+    return something(s.config.px_per_unit, 1.0)
+end
+
 function Screen(; config...)
     config = Makie.merge_screen_config(ScreenConfig, Dict{Symbol,Any}(config))
     return Screen(nothing, config)
