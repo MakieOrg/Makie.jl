b85f771e0c3ba37ad21429b9bbf21f7d52c152eb
ffreyer <frederic481994@hotmail.de>
2025-02-25 15:04:06 +0100

    skip message counting tests for now

--- WGLMakie/test/runtests.jl
+++ WGLMakie/test/runtests.jl
@@ -45,11 +45,6 @@ edisplay = Bonito.use_electron_display(devtools=true)
         ReferenceTests.test_comparison(scores; threshold = 0.05)
     end
 
-    @testset "Message counts / Update frequencies" begin
-        # Uses edisplay
-        include("message_counting.jl")
-    end
-
     @testset "memory leaks" begin
         Makie.CURRENT_FIGURE[] = nothing
         app = App(nothing)
