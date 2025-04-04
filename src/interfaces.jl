9d7c340f4f8f4537c2940b4d2f525e28942816ad
abhro <5664668+abhro@users.noreply.github.com>
2025-04-04 17:01:09 -0400

    Fix indent in fenced code block in interfaces.jl

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -288,18 +288,18 @@ Those attributes will not be forwarded to the backend, but only used during the
 conversion pipeline.
 Usage:
 ```julia
-    struct MyType end
-    used_attributes(::MyType) = (:attribute,)
-    function convert_arguments(x::MyType; attribute = 1)
-        ...
-    end
-    # attribute will get passed to convert_arguments
-    # without keyword_verload, this wouldn't happen
-    plot(MyType, attribute = 2)
-    #You can also use the convenience macro, to overload convert_arguments in one step:
-    @keywords convert_arguments(x::MyType; attribute = 1)
-        ...
-    end
+struct MyType end
+used_attributes(::MyType) = (:attribute,)
+function convert_arguments(x::MyType; attribute = 1)
+    ...
+end
+# attribute will get passed to convert_arguments
+# without keyword_verload, this wouldn't happen
+plot(MyType, attribute = 2)
+#You can also use the convenience macro, to overload convert_arguments in one step:
+@keywords convert_arguments(x::MyType; attribute = 1)
+    ...
+end
 ```
 """
 used_attributes(::Type{<:Plot}, args...) = used_attributes(args...)
