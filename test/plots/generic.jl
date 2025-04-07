a8953a0f79a7952f1ad9cd7cb5d04ded56994ca3
ffreyer <frederic481994@hotmail.de>
2025-04-07 22:26:22 +0200

    merge generic attribute test files, comments

--- test/plots/generic.jl
+++ test/plots/generic.jl
@@ -2,23 +2,33 @@
     # also tests that plot!(scene, ...) always works
     core_attributes = [
         :visible, :transparency, :space, :overdraw, :inspectable, :inspector_label,
-        :inspector_clear, :inspector_hover, :clip_planes, :depth_shift, #:fxaa
+        :inspector_clear, :inspector_hover, :clip_planes, :depth_shift, :fxaa
     ]
+
+    # TODO:
+    # After compute plots #4630 consider reworking attribute passthrough to be
+    # more automatic and add tests verifying attribute passthrough & completeness
+    # This could be a start:
 
     # `exclude` to exclude an attribute from being checked
     # `skip_inheritance` to exclude it after the top level plot is checked (e.g. if child plots do not inherit it)
     # `expected` attributes to compare to (default values)
-    function validate_attributes(p; exclude = Set(), expected = Dict{Symbol, Any}(), skip_inheritance = Set())
-        attr = attributes(p)
+    # function validate_attributes(p; exclude = Set(), expected = Dict{Symbol, Any}(), skip_inheritance = Set())
+    #     attr = attributes(p)
 
-        for name in core_attributes
-            name in exclude && continue
-            name in skip_inheritance && push!(exclude, name)
-            @test haskey(attr, name) && (to_value(attr[name]) == to_value(expected[name]))
-        end
+    #     failed = Symbol[]
+    #     for name in core_attributes
+    #         name in exclude && continue
+    #         name in skip_inheritance && push!(exclude, name)
+    #         if !(haskey(attr, name) && (to_value(attr[name]) == to_value(expected[name])))
+    #             push!(failed, name)
+    #         end
+    #     end
 
-        foreach(p -> validate_attributes(p, exclude = exclude, expected = expected), p.plots)
-    end
+    #     @test failed == Symbol[]
+
+    #     foreach(p -> validate_attributes(p, exclude = exclude, expected = expected), p.plots)
+    # end
 
     scene = Scene()
     expected = Makie.MakieCore.generic_plot_attributes!(Attributes())
@@ -27,7 +37,20 @@
     for PlotType in ALL_PLOT_TYPES
         @testset "$PlotType" begin
             p = testplot!(scene, PlotType)
-            # validate_attributes(p, expected = expected)
+            @test true # Just validate that there's no error for now
         end
     end
+end
+
+# TODO: Test these for all attributes
+@testset "Generic Attributes" begin
+    scene = Scene()
+    p = scatter!(scene, rand(10))
+    @test scene.theme.inspectable[]
+    @test p.inspectable[]
+
+    scene = Scene(inspectable = false)
+    p = scatter!(scene, rand(10))
+    @test !scene.theme.inspectable[]
+    @test !p.inspectable[]
 end
