34db2c11469ef1ffba92a9c7ccec495a8375ec1e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-06-12 13:57:12 +0200

    add computepipeline to comparison in benchmark

--- metrics/ttfp/run-benchmark.jl
+++ metrics/ttfp/run-benchmark.jl
@@ -68,7 +68,13 @@ Pkg.add([(; name="JSON")])
 
 project2 = make_project_folder(base_branch)
 Pkg.activate(project2)
-pkgs = [(; rev=base_branch, name="MakieCore"), (; rev=base_branch, name="Makie"), (; rev=base_branch, name="$Package"), (;name="JSON")]
+pkgs = [
+    (; rev=base_branch, name="MakieCore"),
+    (; url="https://github.com/MakieOrg/Makie.jl", subdir="ComputePipeline", rev=base_branch), # TODO: adjust once ComputePipeline is released
+    (; rev=base_branch, name="Makie"),
+    (; rev=base_branch, name="$Package"),
+    (;name="JSON")
+]
 Package == "WGLMakie" && push!(pkgs, (; name="Electron"))
 Pkg.add(pkgs)
 
