e1e9263ce6babcdcdd505a237733ee32ff0a9fec
tom rottier <tomrottier@outlook.com>
2025-06-18 00:44:18 -0400

    make get_labeled_plots work with multiple axes

--- Makie/src/makielayout/blocks/legend.jl
+++ Makie/src/makielayout/blocks/legend.jl
@@ -995,50 +995,6 @@ function Legend(
     return _block(Legend, fig_or_scene, (), legend_defaults, bbox; kwdict_complete = true)
 end
 
-"""
-    Legend(fig_or_scene, axes::Union{Vector{Axis},Vector{Axis3},Vector{LScene},Vector{Scene}}, title = nothing; merge=true, kwargs...)
-
-Create a single-group legend with all plots from `axes` that have the attribute `label` set.
-
-If `merge` is `true`, all plot objects with the same label will be layered on top of each other into one legend entry.
-If `unique` is `true`, all plot objects with the same plot type and label will be reduced to one occurrence.
-
-
-"""
-function Legend(
-    fig_or_scene,
-    axes::AbstractArray{<: Union{<: AbstractAxis, <: AbstractScene}},
-    title=nothing;
-    merge=false,
-    unique=false,
-    kwargs...,
-)
-
-    lplots = AbstractPlot[]
-    labels = AbstractString[]
-    for ax in axes
-        pl, lb = Makie.get_labeled_plots(ax, merge=false, unique=false)
-        append!(lplots, pl)
-        append!(labels, lb)
-    end
-
-    if unique
-        plots_labels = Base.unique(((p, l),) -> (typeof(p), l), zip(lplots, labels))
-        lplots = first.(plots_labels)
-        labels = last.(plots_labels)
-    end
-
-    if merge
-        ulabels = Base.unique(labels)
-        mergedplots = [[lp for (i, lp) in enumerate(lplots) if labels[i] == ul]
-            for ul in ulabels]
-
-        lplots, labels = mergedplots, ulabels
-    end
-    
-    return Legend(fig_or_scene, lplots, labels, title; kwargs...)
-end
-
 
 """
     Legend(fig_or_scene, axis::Union{Axis, Scene, LScene}, title = nothing; merge = false, unique = false, kwargs...)
@@ -1049,14 +1005,14 @@ attribute `label` set.
 If `merge` is `true`, all plot objects with the same label will be layered on top of each other into one legend entry.
 If `unique` is `true`, all plot objects with the same plot type and label will be reduced to one occurrence.
 """
-function Legend(fig_or_scene, axis::Union{Axis, Axis3, Scene, LScene}, title = nothing; merge = false, unique = false, kwargs...)
+function Legend(fig_or_scene, axis::Union{AbstractAxis, AbstractScene, AbstractArray{<:Union{<:AbstractAxis, <:AbstractScene}}}, title = nothing; merge = false, unique = false, kwargs...)
     plots, labels = get_labeled_plots(axis, merge = merge, unique = unique)
     isempty(plots) && error("There are no plots with labels in the given axis that can be put in the legend. Supply labels to plotting functions like `plot(args...; label = \"My label\")`")
     return Legend(fig_or_scene, plots, labels, title; kwargs...)
 end
 
 function get_labeled_plots(ax; merge::Bool, unique::Bool)
-    lplots = filter(get_plots(ax)) do plot
+    lplots = filter(reduce(vcat, get_plots.(ax), init=AbstractPlot[])) do plot
         haskey(plot.attributes, :label) ||
             plot isa PlotList && any(x -> haskey(x.attributes, :label), plot.plots)
     end
