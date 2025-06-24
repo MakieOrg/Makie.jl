055c1605659cd8ec76e8a3e17db1cb7c8ffff699
ffreyer <frederic481994@hotmail.de>
2025-06-24 14:54:51 +0200

    fix formatting

--- Makie/test/conversions/conversions.jl
+++ Makie/test/conversions/conversions.jl
@@ -513,6 +513,6 @@ end
 @testset "Dendrogram" begin
     xs = 1:10
     ys = rand(10)
-    merges = [(i, i+1) for i in 1:20]
+    merges = [(i, i + 1) for i in 1:20]
     @test Makie.convert_arguments(Dendrogram, xs, ys, merges) == Makie.convert_arguments(Dendrogram, Point2.(xs, ys), merges)
 end
