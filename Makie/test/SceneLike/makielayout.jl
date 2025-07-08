5fbca0c7f8ffca72a77e95d520aad4c428039951
cgarling <chris.t.garling@gmail.com>
2025-07-07 22:03:06 -0400

    Fix formatting

--- Makie/test/SceneLike/makielayout.jl
+++ Makie/test/SceneLike/makielayout.jl
@@ -552,13 +552,15 @@ end
     y = 1.0:0.1:5.0
     z = broadcast((x, y) -> x, x, y')
 
-    for scale in (Makie.Symlog10(2), 
+    for scale in (
+        Makie.Symlog10(2), 
         Makie.pseudolog10, 
         Makie.AsinhScale(0.1), 
         Makie.SinhScale(1/3), 
         Makie.LogScale(1000, ℯ), 
         Makie.LuptonAsinhScale(0.1, 0.01, 0.1), 
-        Makie.PowerScale(1))
+        Makie.PowerScale(1)
+    )
         fig, ax, hm = heatmap(x, y, z; colorscale = scale, axis = (; xscale = scale))
         Colorbar(fig[1, 2], hm)
     end
