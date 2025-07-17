e1224318c0fc0db6a57733427a039425b02c4eba
ffreyer <frederic481994@hotmail.de>
2025-07-17 12:19:57 +0200

    fix broadcast error in mesh ray cast

--- Makie/src/interaction/ray_casting.jl
+++ Makie/src/interaction/ray_casting.jl
@@ -340,8 +340,7 @@ function position_on_plot(plot::Mesh, idx, ray::Ray; apply_transform = true)
 
     for f in faces(plot.mesh[])
         if idx in f
-            p1, p2, p3 = positions[f]
-            p1, p2, p3 = Makie.apply_transform.(tf, (p1, p2, p3))
+            p1, p2, p3 = Makie.apply_transform(tf, positions[f])
             pos = ray_triangle_intersection(p1, p2, p3, ray)
             if !isnan(pos)
                 if apply_transform
