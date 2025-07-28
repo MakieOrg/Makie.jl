e6247ce9692164f1af1df48237869134b081d598
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-07-28 16:46:50 +0200

    formatting

--- docs/graphviz_block.jl
+++ docs/graphviz_block.jl
@@ -12,13 +12,13 @@ module GraphvizDocsHelpers
 
     function Base.show(io::IO, ::MIME"image/svg+xml", g::GraphvizSVG)
         # Create a temporary directory and work within it
-        mktempdir() do temp_dir
+        return mktempdir() do temp_dir
             dot_file = joinpath(temp_dir, "temp.dot")
             svg_file = joinpath(temp_dir, "temp.svg")
-            
+
             # Write dot code to temporary file
             write(dot_file, g.dot_code)
-            
+
             # Run Graphviz to generate SVG with styling options
             run(`$(Graphviz_jll.dot()) 
                 -Tsvg 
@@ -37,7 +37,7 @@ module GraphvizDocsHelpers
                 -Efontsize=10
                 -o $svg_file 
                 $dot_file`)
-            
+
             # Read and write the generated SVG content directly to io
             svg_content = read(svg_file, String)
             write(io, svg_content)
@@ -49,18 +49,18 @@ function Documenter.Selectors.runner(::Type{GraphvizBlocks}, node, page, doc)
     el = node.element
     el.info = "@example"
     el.code = transform_graphviz_code(el.code)
-    Documenter.Selectors.runner(Documenter.Expanders.ExampleBlocks, node, page, doc)
+    return Documenter.Selectors.runner(Documenter.Expanders.ExampleBlocks, node, page, doc)
 end
 
 function transform_graphviz_code(code::String)
     # Add # hide to each line of the DOT code
     hidden_code = join([line * " # hide" for line in split(code, '\n')], '\n')
-    
+
     return """
-        import ..GraphvizDocsHelpers # hide
-        dot_code = raw\"\"\" # hide
-        $hidden_code
-        \"\"\" # hide
-        GraphvizDocsHelpers.GraphvizSVG(dot_code) # hide
-        """
+    import ..GraphvizDocsHelpers # hide
+    dot_code = raw\"\"\" # hide
+    $hidden_code
+    \"\"\" # hide
+    GraphvizDocsHelpers.GraphvizSVG(dot_code) # hide
+    """
 end
