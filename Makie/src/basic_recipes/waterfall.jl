aad2df8800c8322b89bfe4d4835c78047e103055
Abhirath Anand <74202102+theabhirath@users.noreply.github.com>
2025-08-15 09:23:18 -0400

    Run Runic

--- Makie/src/basic_recipes/waterfall.jl
+++ Makie/src/basic_recipes/waterfall.jl
@@ -67,9 +67,9 @@ function Makie.plot!(p::Waterfall)
     barplot!(p, p.attributes, p.xy; fillto = p.fillto, stack = automatic)
 
     function direction_markers(
-        xy, fillto, marker_pos, marker_neg, width,
-        gap, dodge, n_dodge, dodge_gap
-    )
+            xy, fillto, marker_pos, marker_neg, width,
+            gap, dodge, n_dodge, dodge_gap
+        )
         xs = first(compute_x_and_width(first.(xy), width, gap, dodge, n_dodge, dodge_gap))
         MarkerType = promote_type(typeof(marker_pos), typeof(marker_neg))
         PointType = eltype(xy)
