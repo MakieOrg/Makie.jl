cc4ae61bbb86e4186af268d2858796cd613f3716
Documenter.jl <documenter@juliadocs.github.io>
2025-10-30 00:11:57 +0000

    delete history

---
+++ v0.15/assets/documentation/theming/predefined_themes/code/example_679238586252941432.jl
@@ -0,0 +1,9 @@
+# This file was generated, do not modify it. # hide
+__result = begin # hide
+  
+with_theme(demofigure, theme_light())
+
+  end # hide
+  save(joinpath(@OUTPUT, "example_679238586252941432.png"), __result) # hide
+  
+  nothing # hide
