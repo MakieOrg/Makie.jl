74c0bc2733ea29bfe8e10b2e535a8a83958fe0eb
Frederic Freyer <frederic481994@hotmail.de>
2025-12-03 15:24:41 +0100

    add comment [skip ci]

--- GLMakie/src/screen.jl
+++ GLMakie/src/screen.jl
@@ -995,6 +995,7 @@ refreshwindowcb(screen) = window -> refreshwindowcb(screen, window)
 
 function scalechangecb(screen, window, xscale, yscale)
     sf = min(xscale, yscale)
+    # Some window managers may trigger this before the screen config is set and the window becomes visible
     if !isnothing(screen.config)
         if isnothing(screen.config.px_per_unit) && screen.scalefactor[] == screen.px_per_unit[]
             screen.px_per_unit[] = sf
