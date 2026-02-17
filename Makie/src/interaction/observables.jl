687f314cc85b8eb606f2971a860919f938f35b8a
hzxiaxz <1806656034@qq.com>
2026-02-18 00:41:26 +0800

    update

--- Makie/src/interaction/observables.jl
+++ Makie/src/interaction/observables.jl
@@ -23,7 +23,7 @@ function on_latest(f, observable::Observable; update = false, spawn = false, thr
 end
 
 
-function _run_f(f, current_value, observable)
+function _run_f(f, current_value, observable, throttle, has_changed)
     while true
         t1 = time()
         try
@@ -60,9 +60,9 @@ function on_latest(f, to_track, observable::Observable; update = false, spawn = 
         return lock(task_lock) do
             if isnothing(last_task_ref[]) || istaskdone(last_task_ref[])
                 if spawn
-                    last_task_ref[] = Threads.@spawn _run_f(f, new_value, observable) 
+                    last_task_ref[] = Threads.@spawn _run_f(f, new_value, observable, throttle, has_changed) 
                 else
-                    last_task_ref[] = Threads.@async _run_f(f, new_value, observable)
+                    last_task_ref[] = Threads.@async _run_f(f, new_value, observable, throttle, has_changed)
                 end
             else
                 has_changed[] = true
