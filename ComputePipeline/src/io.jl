3d481ce0a93eaf7ad942d236f8d64880593a76c2
ffreyer <frederic481994@hotmail.de>
2026-03-16 15:24:58 +0100

    fix error printing

--- ComputePipeline/src/io.jl
+++ ComputePipeline/src/io.jl
@@ -276,7 +276,7 @@ function collect_dirty(computed::Computed, marked = Set{Symbol}())
     return marked
 end
 function collect_dirty(edge::ComputeEdge, marked = Set{Symbol}())
-    if edge.dirty[] || any(edge.inputs_dirty)
+    if isdirty(edge) || any(edge.inputs_dirty)
         foreach(output -> push!(marked, output.name), edge.outputs)
         foreach(input -> collect_dirty(input, marked), edge.inputs)
     end
