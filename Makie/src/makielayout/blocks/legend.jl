5ae95ec3cef07810a1393b968cbbc7ab1cc92050
ffreyer <frederic481994@hotmail.de>
2026-04-06 16:56:14 +0200

    format

--- Makie/src/makielayout/blocks/legend.jl
+++ Makie/src/makielayout/blocks/legend.jl
@@ -1010,16 +1010,17 @@ If `unique` is `true`, all plot objects with the same plot type and label will b
 To create a joint legend for multiple axes it is also possible to pass a `Vector` of axis objects.
 """
 function Legend(
-    fig_or_scene,
-    axis::Union{AbstractAxis, AbstractScene, AbstractArray{<: Union{AbstractAxis, AbstractScene}}},
-    title = nothing; merge = false, unique = false, kwargs...)
+        fig_or_scene,
+        axis::Union{AbstractAxis, AbstractScene, AbstractArray{<:Union{AbstractAxis, AbstractScene}}},
+        title = nothing; merge = false, unique = false, kwargs...
+    )
     plots, labels = get_labeled_plots(axis, merge = merge, unique = unique)
     isempty(plots) && error("There are no plots with labels in the given axis that can be put in the legend. Supply labels to plotting functions like `plot(args...; label = \"My label\")`")
     return Legend(fig_or_scene, plots, labels, title; kwargs...)
 end
 
 function get_labeled_plots(ax; merge::Bool, unique::Bool)
-    lplots_init = filter(reduce(vcat, get_plots.(ax), init=AbstractPlot[])) do plot
+    lplots_init = filter(reduce(vcat, get_plots.(ax), init = AbstractPlot[])) do plot
         haskey(plot.attributes, :label) ||
             plot isa PlotList && any(x -> haskey(x.attributes, :label), plot.plots)
     end
