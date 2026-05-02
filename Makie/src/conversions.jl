c32ce7796883cb04058b36a76638f72b05be4617
ffreyer <frederic481994@hotmail.de>
2026-05-02 19:41:05 +0200

    allow `fonts` to be a Dict

--- Makie/src/conversions.jl
+++ Makie/src/conversions.jl
@@ -1485,18 +1485,18 @@ to_font(x::Vector{String}) = to_font.(x)
 to_font(x::NativeFont) = x
 to_font(x::Vector{NativeFont}) = x
 
-function to_font(fonts::Attributes, s::Symbol)
+function to_font(fonts::Union{Dict, Attributes}, s::Symbol)
     if haskey(fonts, s)
-        f = fonts[s][]
+        f = to_value(fonts[s])
         if f isa Symbol
             error("The value for font $(repr(s)) was Symbol $(repr(f)), which is not allowed. The value for a font in the fonts collection cannot be another Symbol and must be resolvable via `to_font(x)`.")
         end
-        return to_font(fonts[s][])
+        return to_font(to_value(fonts[s]))
     end
     error("The symbol $(repr(s)) is not present in the fonts collection:\n$fonts.")
 end
 
-to_font(fonts::Attributes, x) = to_font(x)
+to_font(fonts::Union{Dict, Attributes}, x) = to_font(x)
 
 to_font(::Automatic) = defaultfont()
 
