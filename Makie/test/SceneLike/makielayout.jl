2d65d001a6a9ee35f2b24e6d6c6196fd93e557bc
Alex Wadell <awadell@gmail.com>
2026-05-29 08:19:47 -0600

    fix: make axislegend's margin themeable (#5624)
    * fix: make axislegend's margin themeable
    
    Plus add test to verify that kwargs > theme > default for the
    Legend's margin
    
    * tweak: explicitly check kwargs for margin
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Frederic Freyer <frederic481994@hotmail.de>

--- Makie/test/SceneLike/makielayout.jl
+++ Makie/test/SceneLike/makielayout.jl
@@ -867,3 +867,20 @@ end
         end
     end
 end
+
+# issue 2415
+@testset "themeable axislegend" begin
+    f = Figure()
+    ax = Axis(f[1, 1])
+    lines!(ax, 1:10, label = "A line")
+    leg = @test_nowarn axislegend(ax)
+    @test all(>=(0), leg.margin[])
+    with_theme(Theme(Legend = (; margin = (1, 2, 3, 4)))) do
+        leg = @test_nowarn axislegend(ax)
+        @test leg.margin[] == (1, 2, 3, 4)
+
+        # Kwargs override theme
+        leg = @test_nowarn axislegend(ax; margin = (4, 3, 2, 1))
+        @test leg.margin[] == (4, 3, 2, 1)
+    end
+end
