df5131edb02de7538aa60c0c81d5e84922a9b869
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2026-08-07 13:59:41 +0200

    test(glmakie): Update compute graph node counts for the px_per_unit rasterization nodes

--- GLMakie/test/runtests.jl
+++ GLMakie/test/runtests.jl
@@ -41,8 +41,8 @@ GLMakie.activate!(framerate = 1.0, scalefactor = 1.0)
         # are modified in which case the numbers should just be updated
         f, a, p = scatter(rand(10))
         colorbuffer(f)
-        @test length(p.attributes.inputs) == 37
-        @test length(p.attributes.outputs) == 83
+        @test length(p.attributes.inputs) == 38
+        @test length(p.attributes.outputs) == 85
     end
 
     @testset "Reference Tests" begin
