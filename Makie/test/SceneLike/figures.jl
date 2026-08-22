beb4410967eadf734e89dfe1ff17c7fe5d93ab3c
ffreyer <frederic481994@hotmail.de>
2026-08-22 23:01:19 +0200

    fix error deprecation in tests

--- Makie/test/SceneLike/figures.jl
+++ Makie/test/SceneLike/figures.jl
@@ -251,5 +251,5 @@ end
     fig = Figure()
     io = IOBuffer()
     # if there were no show method with backend and update kwargs then MethodError would be thrown instead
-    @test_throws ErrorException show(io, MIME"text/plain"(), fig, backend = missing, update = false)
+    @test_throws FieldError show(io, MIME"text/plain"(), fig, backend = missing, update = false)
 end
