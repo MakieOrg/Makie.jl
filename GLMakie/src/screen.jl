cf3852f40f423947c00dbf441330fe2e84f57a98
John Eismeier <42679190+jeis4wpi@users.noreply.github.com>
2026-09-09 08:18:51 -0400

    bug: fix @warn that has typo with exception (#5776)
    Signed-off-by: John E <jeis4wpi@outlook.com>

--- GLMakie/src/screen.jl
+++ GLMakie/src/screen.jl
@@ -981,7 +981,7 @@ function stop_renderloop!(screen::Screen; close_after_renderloop = screen.close_
         try
             wait(screen)  # isnothing(rendertask) handled in wait(screen)
         catch e
-            @warn "Error while waiting for render task to finish. Cleanup will continue" excetion = (e, Base.catch_backtrace())
+            @warn "Error while waiting for render task to finish. Cleanup will continue" exception = (e, Base.catch_backtrace())
         end
     end
     # after done, we can set the task to nothing
