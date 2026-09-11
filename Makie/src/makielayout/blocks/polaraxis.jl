b29b76c2d45f483da2be2aa3e2bd038e044b3d4f
ffreyer <frederic481994@hotmail.de>
2026-09-11 16:00:32 +0200

    disable shading in blockscenes Axis scenes and PolarAxis scenes by default

--- Makie/src/makielayout/blocks/polaraxis.jl
+++ Makie/src/makielayout/blocks/polaraxis.jl
@@ -10,13 +10,15 @@ function initialize_block!(po::PolarAxis; palette = nothing)
     end
 
     po.scene = Scene(
-        po.blockscene, scenearea, backgroundcolor = po.backgroundcolor, clear = true
+        po.blockscene, scenearea, backgroundcolor = po.backgroundcolor,
+        clear = true, shading = NoShading
     )
     map!(to_color, po.scene, po.scene.backgroundcolor, po.backgroundcolor)
 
     po.overlay = Scene(
         po.scene, scenearea, clear = false, backgroundcolor = :transparent,
-        transformation = Transformation(po.scene, transform_func = identity)
+        transformation = Transformation(po.scene, transform_func = identity),
+        shading = NoShading
     )
 
     if !isnothing(palette)
