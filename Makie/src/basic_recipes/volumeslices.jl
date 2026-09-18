3ab57fc727f842c5bac35623521f1d55a4f71137
ffreyer <frederic481994@hotmail.de>
2026-09-18 10:44:47 +0200

    update volumeslices (non-breaking)

--- Makie/src/basic_recipes/volumeslices.jl
+++ Makie/src/basic_recipes/volumeslices.jl
@@ -11,22 +11,25 @@ Draws heatmap slices of the volume `v`.
     bbox_color = RGBAf(0.5, 0.5, 0.5, 0.5)
 end
 
-function Makie.plot!(plot::VolumeSlices)
+function plot!(plot::VolumeSlices)
     @extract plot (x, y, z, volume)
 
     map!(plot.attributes, [:colorrange, :volume], :computed_colorrange) do colorrange, volume
         return colorrange === automatic ? extrema(volume) : colorrange
     end
 
-    bbox = lift(plot, x, y, z) do x, y, z
+    map!(plot, [:x, :y, :z], :data_limits) do x, y, z
         mx, Mx = extrema(x)
         my, My = extrema(y)
         mz, Mz = extrema(z)
-        Rect3(mx, my, mz, Mx - mx, My - my, Mz - mz)
+        return Rect3(mx, my, mz, Mx - mx, My - my, Mz - mz)
     end
 
-    axes = :x, :y, :z
+    # Swap to Observable to force visible to be an input in the heatmaps.
+    # This allows `h.visible = active` to continue working in the example
     parent_vis = ComputePipeline.get_observable!(plot.visible)
+    axes = :x, :y, :z
+
     for (ax, p, r, (X, Y)) in zip(axes, (:yz, :xz, :xy), (x, y, z), ((y, z), (x, z), (x, y)))
         hmap = heatmap!(
             plot, Attributes(plot), X, Y, zeros(length(X[]), length(Y[])),
@@ -44,7 +47,10 @@ function Makie.plot!(plot::VolumeSlices)
         add_input!(plot.attributes, Symbol(:heatmap_, p), hmap)
     end
 
-    linesegments!(plot, bbox, color = plot.bbox_color, visible = plot.bbox_visible, inspectable = false)
+    linesegments!(
+        plot, plot.data_limits, color = plot.bbox_color,
+        visible = plot.bbox_visible, inspectable = false
+    )
 
     return plot
 end
