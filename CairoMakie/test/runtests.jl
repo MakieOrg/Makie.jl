8987fc4ddc0fd4d4c7d917e5608a1ad05cb98842
Bart Janssens <bart@bartjanssens.org>
2026-09-21 17:48:46 +0200

    Allow Float64 normals in CairoMakie (#5797)
    * Allow Float64 normals in CairoMakie
    
    * Make typeassert concrete and add CHANGELOG.md line
    
    * add short test, update test dependencies
    
    * format
    
    * transform normals as Vec3f
    
    ---------
    
    Co-authored-by: ffreyer <frederic481994@hotmail.de>

--- CairoMakie/test/runtests.jl
+++ CairoMakie/test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 using CairoMakie
 using Makie.FileIO
 using ReferenceTests
+using GeometryBasics
 
 # Before changing Pkg environment, try the test in #864
 @testset "Runs without error" begin
@@ -165,28 +166,7 @@ end
     @test !isfile(tmp_path)
 end
 
-# @testset "plotlist no ambiguity (#4038)" begin
-#     f = plotlist([Makie.SpecApi.Scatter(1:10)])
-#     Makie.colorbuffer(f; backend=CairoMakie)
-#     plotlist!([Makie.SpecApi.Scatter(1:10)])
-# end
-
-@testset "multicolor line clipping (#4313)" begin
-    fig, ax, p = contour(rand(20, 20))
-    xlims!(ax, 0, 10)
-    Makie.colorbuffer(fig; backend = CairoMakie)
-end
-
-@testset "scatter with all points clipped (#XXXX)" begin
-    fig = Figure()
-    ax = Axis3(fig[1, 1])
-    # all points outside the clip volume -> unclipped_indices is empty
-    scatter!(ax, [0.5, 0.5], [0.5, 0.5], [-10.0, -10.0])
-    limits!(ax, 0, 1, 0, 1, 0, 1)
-    Makie.colorbuffer(fig; backend = CairoMakie)
-    # broadcast_foreach_index returns without error on empty indices
-    @test Makie.broadcast_foreach_index((args...) -> error("unreachable"), UInt32[], 1:3, 1:3) === nothing
-end
+include("issues.jl")
 
 excludes = Set(
     [
